d7364a3aac9c6a38d03be8108a3637b64d050927
Laurell Seville <laurellseville2000@gmail.com>
2020-05-29 15:35:36 -0700

    added sub-org tests

--- tests/organization-sub.spec.js
+++ tests/organization-sub.spec.js
@@ -1,18 +1,50 @@
 const axios = require("axios");
-const token = require("./organization.spec.js")
-
+const token = require("./organization.spec.js");
 
 describe("organization resolvers", () => {
-  test("organizationCreator", async () => {
+  test("organization-subqueries", async () => {
     const response = await axios.post("https://talawa-testing.herokuapp.com/", {
-      query: `query {
-                organizations {
-                    _id
-                    name
-                }
-            }`,
+      query: `
+      {
+        organizations {
+            _id
+            name
+            creator {
+                _id
+                firstName
+            }
+            admins{
+                _id
+                firstName
+            }
+            members {
+                _id
+                firstName
+            }
+        }
+    }
+    `,
     });
+
     const { data } = response;
-    expect(Array.isArray(data.data.organizations)).toBeTruthy();
+
+    expect(data.data.organizations).toEqual(
+      expect.objectContaining({
+        _id: expect.any(String),
+        name: expect.any(String),
+        creator: {
+            _id: expect.any(String),
+            firstName: expect.any(String)
+        },
+        admins: expect.arrayContaining[{
+            _id: expect.any(String),
+            firstName: expect.any(String)
+        }],
+        members: expect.arrayContaining[{
+            _id: expect.any(String),
+            firstName: expect.any(String)
+        }]
+      })
+    );
   });
 });
