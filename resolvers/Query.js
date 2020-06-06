4f13d3d98d25524b445b7459f3f2c7f63bd0c2ab
Laurell Seville <laurellseville2000@gmail.com>
2020-06-06 10:04:10 -0700

    added me query

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,19 +1,20 @@
 const User = require("../models/User");
 const Organization = require("../models/Organization");
 const { login } = require("./Auth");
+const authCheck = require("./functions/authCheck");
 
 const Query = {
   users: async (parent, args, context, info) => {
     try {
       if (args.id) {
-        const users = await User.find({_id:args.id});
+        const users = await User.find({ _id: args.id });
         if (!users[0]) throw new Error("User not found");
         else
-          return users.map(user=>{
+          return users.map((user) => {
             return {
-            ...user._doc,
-            password: null,
-            }
+              ...user._doc,
+              password: null,
+            };
           });
       } else {
         const users = await User.find();
@@ -21,6 +22,19 @@ const Query = {
           return { ...user._doc, password: null };
         });
       }
+    } catch (e) {
+      throw e;
+    }
+  },
+  me: async (parent, args, context, info) => {
+    authCheck(context);
+    try {
+      //Ensure user exists
+      const user = await User.findOne({ _id: context.userId });
+      if(!user) throw new Error("User does not exist")
+      console.log(user._doc)
+
+      return user._doc
     } catch (e) {
       throw e;
     }
@@ -40,7 +54,7 @@ const Query = {
     } catch (e) {
       throw e;
     }
-  }
+  },
 };
 
 module.exports = Query;
