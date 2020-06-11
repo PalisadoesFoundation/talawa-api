ae890998db4113a8c2a555a191f804867c832e52
Ranil Wallace <ranilwallace@gmail.com>
2020-06-11 16:22:27 -0500

    Config env

---
+++ models/Event.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+    name: {
+        type:String,
+        required:true
+    },
+    description: {
+        type:String,
+        required:true
+    },
+    isPublic: {
+        type:Boolean,
+        required:true
+    },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required:true
+    },
+    members: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    admins: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required:true
+        }
+    ]
+})
+
+module.exports = mongoose.model("Event", eventSchema);
