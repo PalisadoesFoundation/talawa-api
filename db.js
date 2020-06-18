e693d3dd974499d27e487acf70e3b59b2a4c5019
Ranil Wallace <ranilwallace@gmail.com>
2020-06-18 10:49:07 -0500

    Temp mongourl

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connect = mongoose
 .connect(
-  process.env.MONGO_URL,
+  "mongodb://root:janelle123@ds023428.mlab.com:23428/talawa",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
