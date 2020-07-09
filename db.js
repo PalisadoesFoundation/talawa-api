85e660ec8c08692b6c9312acbadbd2c7d86bbc55
Laurell Seville <laurellseville2000@gmail.com>
2020-07-08 20:05:22 -0700

    updated to a new db

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 
 const mongoose = require("mongoose");
 
+const mongoURL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@talawa-test-cluster-nk4oo.mongodb.net/${process.env.TEST_DB || process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
 
 
 const connect = mongoose
 .connect(
-  "mongodb://root:janelle123@ds023428.mlab.com:23428/talawa",
+  mongoURL,
+  
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
   }
 )
 
