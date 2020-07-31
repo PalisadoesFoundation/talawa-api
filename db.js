ede33d599cc3542ef8baf51fec2a0f5edf3b8e64
Laurell Seville <laurellseville2000@gmail.com>
2020-07-30 22:47:51 -0700

    hardcoded data

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 //const mongoURL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@talawa-test-cluster-nk4oo.mongodb.net/${process.env.TEST_DB || process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
-const mongoURLLocal = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1:27017/${process.env.TEST_DB || process.env.MONGO_DB}?authSource=admin`
+const mongoURLLocal = `mongodb://talawa-admin:j7B9JtK8hY6gcBPf@127.0.0.1:27017/talawa-db`
 
 const connect = mongoose
 .connect(
