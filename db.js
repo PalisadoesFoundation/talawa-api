38f3cf24497d0c0042e3ff7369c2d7d1c384328e
Laurell Seville <laurellseville2000@gmail.com>
2020-07-30 22:36:35 -0700

    changed connection url

--- db.js
+++ db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 
 //const mongoURL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@talawa-test-cluster-nk4oo.mongodb.net/${process.env.TEST_DB || process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
-const mongoURLLocal = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1/${process.env.TEST_DB || process.env.MONGO_DB}`
+//const mongoURLLocal = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1/${process.env.TEST_DB || process.env.MONGO_DB}`
+
+const mongoURLLocal =`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1:27017/${process.env.TEST_DB || process.env.MONGO_DB}?authSource=admin`
+
 
 const connect = mongoose
 .connect(
