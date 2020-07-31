0e62661f214809e52197809d039b58e0ba2dbcdc
Laurell Seville <laurellseville2000@gmail.com>
2020-07-30 22:15:57 -0700

    added username and pw to mongo url

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 //const mongoURL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@talawa-test-cluster-nk4oo.mongodb.net/${process.env.TEST_DB || process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
-const mongoURLLocal = `mongodb://127.0.0.1/${process.env.TEST_DB || process.env.MONGO_DB}`
+const mongoURLLocal = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1/${process.env.TEST_DB || process.env.MONGO_DB}`
 
 const connect = mongoose
 .connect(
