68d424dfdd406960900e2bd39f66bb8ad4e0ddfa
Laurell Seville <laurellseville2000@gmail.com>
2020-08-05 20:46:22 -0500

    fixed db problems

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
-
 const mongoose = require("mongoose");
 
-const mongoURL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/${process.env.TEST_DB || process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const mongoURL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@talawa-test-cluster-nk4oo.mongodb.net/${process.env.TEST_DB || process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
 //const mongoURL = `mongodb://talawa-admin:j7B9JtK8hY6gcBPf@127.0.0.1:27017/talawa-db`
 
@@ -15,6 +14,5 @@ const connect = mongoose
     useFindAndModify: false
   }
 )
-
 
 module.exports = connect
