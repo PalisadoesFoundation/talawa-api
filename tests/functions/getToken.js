84152cec42b42b7efbcaa31173b91f3dbd099af6
Ranil Wallace <ranilwallace@gmail.com>
2020-08-05 20:45:44 -0500

    Updated get token method for tests

--- tests/functions/getToken.js
+++ tests/functions/getToken.js
@@ -4,21 +4,25 @@ const { URL } = require("../../constants");
 //sets token before every test
 getToken = async () => {
 	const response = await axios.post(URL, {
-		query: `
-      {
-          login(data: {
-            email:"rw@gmail.com",
-            password:"test123"
-          }) {
-            userId
-            token
-          }  
+    query: `
+      mutation {
+        login(
+            data: {
+                email: "rw@gmail.com",
+                password: "test123"
+        }) {
+            user {
+                _id
+            }
+            accessToken
+            refreshToken
         }
-        `,
+      }
+    `,
 	});
 
 	const { data } = response;
-	return data.data.login.token;
+	return data.data.login.accessToken;
 };
 
 module.exports = getToken;
