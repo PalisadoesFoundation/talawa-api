97e4d44230db306b5c5010e7f8bfb36d5c503244
Laurell Seville <laurellseville2000@gmail.com>
2020-08-11 19:59:22 -0500

    added change to remove org

--- constants.js
+++ constants.js
@@ -1,10 +1,6 @@
 
 
-<<<<<<< HEAD
 const URL = "http://calico.palisadoes.org/talawa/graphql"
-=======
-const URL = "http://localhost:4000/graphql"
->>>>>>> e7efa3b6cd21a519f34b083ed440713eabff7ed9
 
 
 module.exports.URL = URL
