5c8c890c9d0b1f28a159e27e9c414dc8629c009e
Laurell Seville <laurellseville2000@gmail.com>
2020-08-20 20:18:23 -0500

    port changed

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(cors());
 
 connect
   .then(() => {
-    app.listen({ port: process.env.PORT || 80 }, () =>
+    app.listen({ port: process.env.PORT || 4000 }, () =>
       console.log(
         `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
       )
