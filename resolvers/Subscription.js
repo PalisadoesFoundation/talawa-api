e7a82689e8bb0caecb8c87d103a63ad80cd7ada0
Laurell Seville <laurellseville2000@gmail.com>
2020-09-14 16:50:47 -0500

    working on latest issue on git

--- resolvers/Subscription.js
+++ resolvers/Subscription.js
@@ -2,7 +2,19 @@ const MESSAGE_SENT_TO_DIRECT_CHAT = 'MESSAGE_SENT_TO_DIRECT_CHAT';
 
 const Subscription = {
     messageSentToDirectChat: {
-        subscribe: (parent, args, context, info) => context.pubsub.asyncIterator([MESSAGE_SENT_TO_DIRECT_CHAT]),
+        //subscribe: (parent, args, context, info) => context.pubsub.asyncIterator([MESSAGE_SENT_TO_DIRECT_CHAT]),
+
+        subscribe: withFilter(
+            (parent, args, context, info) => {
+              return context.pubsub.asyncIterator([MESSAGE_SENT_TO_DIRECT_CHAT]);
+            },
+            (payload, variables, context) => {
+      
+              //RETURN ALL THE MESSAGES THAT ARE BEING SENT TO THE CHAT
+              //console.log(payload.newMessage.receiver._id == context.context.currentUserId)
+              //return payload.messageSentToDirectChat.directChatMessageBelongsTo == context.context.currentUserId;
+            }
+          ),
     }
 }
 
