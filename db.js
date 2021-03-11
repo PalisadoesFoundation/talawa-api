82400e094d8f20627dbd6962802747ca54ccca92
Yasharth dubey <yashdubeywinner@gmail.com>
2021-03-12 04:43:44 +0530

    docker file added

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-// const mongoURL = `${process.env.MONGO_PREFIX}://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/${process.env.TEST_DB || process.env.MONGO_DB}?retryWrites=true&w=majority`;
+//const mongoURL = `${process.env.MONGO_PREFIX}://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/${process.env.TEST_DB || process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
 
-const mongoURL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1:27017/${process.env.MONGO_DB}`
+//const mongoURL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1:27017/${process.env.MONGO_DB}`
 
 //const mongoURL = `mongodb://127.0.0.1:27017/${process.env.MONGO_DB}`
 
@@ -18,4 +18,4 @@ const connect = mongoose
   }
 )
 
-module.exports = connect
+module.exports = connect;
