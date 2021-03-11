c017ce8c598b28344a08c0780fa531e8bf30a53b
Laurell Seville <laurellseville2000@gmail.com>
2021-03-11 16:03:09 -0500

    update db.js file

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-// Use this connection string if you are using a hosted instance of mongodb using atlas
 // const mongoURL = `${process.env.MONGO_PREFIX}://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/${process.env.TEST_DB || process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
+// Use this connection string if you are using a hosted instance of mongodb using atlas
 // const mongoURL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1:27017/${process.env.MONGO_DB}`
 
 // Use this connection string if you are running mongodb locally
