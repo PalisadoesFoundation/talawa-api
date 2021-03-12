928743d14b10838ac6d1c673a52cbc80e8a8fefc
Saumya Pandey <sp160899@gmail.com>
2021-03-13 03:24:33 +0530

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: ['eslint:recommended', 'plugin:jest/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', 'avoid-escape'],
+    semi: ['error', 'always'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+  plugins: ['prettier', 'jest'],
+};
