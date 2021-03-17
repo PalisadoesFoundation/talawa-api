88974dc6f33262d52ade3f02733a9d4b0d5b5c31
Sumitra Saksham <sumitra19jha@gmail.com>
2021-03-18 04:19:47 +0530

    Organizations orderBy addition

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -79,17 +79,55 @@ const Query = {
 		}
 	},
 	organizations: async (parent, args, context, info) => {
+		var sort = {}
+		var isSortingExecuted = args.orderBy != null;
+
+		//Sorting List
+		if(isSortingExecuted){
+			if(args.orderBy == "id_ASC"){
+				sort = { _id: 1 }
+			}
+
+			else if(args.orderBy == "id_DESC"){
+				sort = { _id: -1 }
+			}
+
+			else if(args.orderBy == "name_ASC"){
+				sort = { name: 1 }
+			}
+
+			else if(args.orderBy == "name_DESC"){
+				sort = { name: -1 }
+			}
+
+			else if(args.orderBy == "description_ASC"){
+				sort = { description: 1 }
+			}
+
+			else if(args.orderBy == "description_DESC"){
+				sort = { description: -1 }
+			}
+
+			else if(args.orderBy == "apiUrl_ASC"){
+				sort = { apiUrl: 1 }
+			}
+			else {
+				sort = {apiUrl: -1}
+			}
+		}
+
 		try {
 			if (args.id) {
 				const organizationFound = await Organization.find({
 					_id: args.id,
-				});
+				}).sort(sort);
 				if (!organizationFound[0]) {
 					throw new Error("Organization not found");
 				}
+
 				return organizationFound;
 			} else {
-				return await Organization.find();
+				return await Organization.find().sort(sort);
 			}
 		} catch (e) {
 			throw e;
