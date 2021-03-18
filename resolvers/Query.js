81e033f1b4bf3ac17a89ddf98841f049848fe45a
Sumitra Saksham <sumitra19jha@gmail.com>
2021-03-18 14:04:51 +0530

    taskByEvent and taskByUser orderBy addition

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -464,8 +464,46 @@ const Query = {
 		}
 	},
 	tasksByEvent: async (parent, args, context, info) => {
+		var sort = {}
+		var isSortingExecuted = args.orderBy != null;
+
+		//Sorting List
+		if(isSortingExecuted){
+			if(args.orderBy == "id_ASC"){
+				sort = { _id: 1 }
+			}
+			else if(args.orderBy == "id_DESC"){
+				sort = { _id: -1 }
+			}
+			else if(args.orderBy == "title_ASC"){
+				sort = { title: 1 }
+			}
+			else if(args.orderBy == "title_DESC"){
+				sort = { title: -1 }
+			}
+			else if(args.orderBy == "description_ASC"){
+				sort = { description: 1 }
+			}
+			else if(args.orderBy == "description_DESC"){
+				sort = { description: -1 }
+			}
+			else if(args.orderBy == "createdAt_ASC"){
+				sort = { createdAt: 1 }
+			}
+			else if(args.orderBy == "createdAt_DESC"){
+				sort = { createdAt: -1 }
+			}
+			else if(args.orderBy == "deadline_ASC"){
+				sort = { deadline: 1 }
+			}
+			else {
+				sort = { deadline: -1}
+			}
+		}
+
 		try {
 			return await Task.find({ event: args.id })
+				.sort(sort)
 				.populate("event")
 				.populate("creator", "-password");
 		} catch (e) {
@@ -473,8 +511,46 @@ const Query = {
 		}
 	},
 	tasksByUser: async (parent, args, context, info) => {
+		var sort = {}
+		var isSortingExecuted = args.orderBy != null;
+
+		//Sorting List
+		if(isSortingExecuted){
+			if(args.orderBy == "id_ASC"){
+				sort = { _id: 1 }
+			}
+			else if(args.orderBy == "id_DESC"){
+				sort = { _id: -1 }
+			}
+			else if(args.orderBy == "title_ASC"){
+				sort = { title: 1 }
+			}
+			else if(args.orderBy == "title_DESC"){
+				sort = { title: -1 }
+			}
+			else if(args.orderBy == "description_ASC"){
+				sort = { description: 1 }
+			}
+			else if(args.orderBy == "description_DESC"){
+				sort = { description: -1 }
+			}
+			else if(args.orderBy == "createdAt_ASC"){
+				sort = { createdAt: 1 }
+			}
+			else if(args.orderBy == "createdAt_DESC"){
+				sort = { createdAt: -1 }
+			}
+			else if(args.orderBy == "deadline_ASC"){
+				sort = { deadline: 1 }
+			}
+			else {
+				sort = { deadline: -1}
+			}
+		}
+
 		try {
 			return await Task.find({ creator: args.id })
+				.sort(sort)
 				.populate("event")
 				.populate("creator", "-password");
 		} catch (e) {
