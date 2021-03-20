717763efb5df4ae3b839ca90931b358cbcd17332
Sumitra Saksham <sumitra19jha@gmail.com>
2021-03-20 21:49:15 +0530

    handling existing email

--- resolvers/user_mutations/updateUserProfile.js
+++ resolvers/user_mutations/updateUserProfile.js
@@ -1,5 +1,6 @@
 const authCheck = require("../functions/authCheck");
 const userExists = require("../../helper_functions/userExists");
+const User = require("../../models/User");
 
 const updateUserProfile = async (parent, args, context, info) => {
 
@@ -7,10 +8,19 @@ const updateUserProfile = async (parent, args, context, info) => {
     authCheck(context);
 
     try {
-
         //gets user in token - to be used later on
         let userFound = await userExists(context.userId);
         if (!userFound) throw new Error("User not found");
+
+        if(args.data.email != null){
+            const emailTaken = await User.findOne({
+                email: args.data.email.toLowerCase(),
+            });
+
+            if (emailTaken) {
+                throw new Error("Email address taken.");
+            }
+        }
 
         //UPDATE USER
         userFound.overwrite({
