ba3095335a29d931357e545a45d0a6f2bf271c3a
Jordan Jones <proxima.aust@gmail.com>
2021-03-24 21:21:52 -0500

    feat, use status route

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.use(mongoSanitize());
 app.use(cors());
 app.use('/images', express.static(path.join(__dirname, './images')));
 
-app.use('/status', (req, res) =>
+app.get('/', (req, res) =>
   res.json({ 'talawa-version': 'v1', status: 'healthy' })
 );
 
