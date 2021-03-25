b36aeed930d3d926392deb4f0f0330a45e9905e7
Jordan Jones <proxima.aust@gmail.com>
2021-03-24 20:17:25 -0500

    revert, remove pm2 port setting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,9 @@ module.exports = {
       watch: '.',
       env: {
         NODE_ENV: 'development',
-        PORT: 4000,
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 80,
       },
     },
   ],
