0f9340227921673203c4395a051b266613650c6b
dsantistevan <43758544+dsantistevan@users.noreply.github.com>
2021-03-31 17:36:58 -0500

    Update creatorCheck.js
    Resolved bug where users could not delete organizations they created

--- resolvers/functions/creatorCheck.js
+++ resolvers/functions/creatorCheck.js
@@ -1,5 +1,5 @@
 const creatorCheck = (context, org) => {
-  const isCreator = org.creator === context.userId;
+  const isCreator = org.creator + '' === context.userId;  //Casted org.creator to string to have matching types with === operator
   if (!isCreator) {
     throw new Error("Users cannot delete organizations they didn't create");
   }
