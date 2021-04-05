9f32ae2bd72636d1a9a143d7d6b58546421fa698
Yasharth dubey <yashdubeywinner@gmail.com>
2021-04-05 15:38:45 +0530

    added notification webhook for the same

--- resolvers/Payment_Gateway/checkout.js
+++ resolvers/Payment_Gateway/checkout.js
@@ -23,5 +23,29 @@ router.post("/payment", async (req, res) => {
 			success: false
 		})
 	}
-})
+});
+
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
+	const event = request.body;
+  
+	// Handle the event
+	switch (event.type) {
+	  case 'payment_intent.succeeded':
+		const paymentIntent = event.data.object;
+		// Then define and call a method to handle the successful payment intent.
+		// handlePaymentIntentSucceeded(paymentIntent);
+		break;
+	  case 'payment_method.attached':
+		const paymentMethod = event.data.object;
+		// Then define and call a method to handle the successful attachment of a PaymentMethod.
+		// handlePaymentMethodAttached(paymentMethod);
+		break;
+	  // ... handle other event types
+	  default:
+		console.log(`Unhandled event type ${event.type}`);
+	}
+  
+	// Return a response to acknowledge receipt of the event
+	response.json({received: true});
+  });
 module.exports = router;
