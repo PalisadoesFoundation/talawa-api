b074187f80d04bc4bf881e8780f9fb36b03ee32c
Yasharth dubey <yashdubeywinner@gmail.com>
2021-04-08 00:39:32 +0530

    added subscription

---
+++ resolvers/Payment_Gateway/subscription.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const stripe = require('stripe')('YOUR_SECURITY_KEY')
+
+app.post('/create-checkout-session', async (req, res) => {
+  const { priceId } = req.body;
+
+  // See https://stripe.com/docs/api/checkout/sessions/create
+  // for additional parameters to pass.
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: 'subscription',
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+      // the actual Session ID is returned in the query parameter when your customer
+      // is redirected to the success page.
+      success_url: 'YOUR SUCCESS URL WITH {CHECKOUT_SESSION_ID}',
+      cancel_url: 'YOUR CANCLE PAGE URL',
+    });
+
+    res.send({
+      sessionId: session.id,
+    });
+  } catch (e) {
+    res.status(400);
+    return res.send({
+      error: {
+        message: e.message,
+      }
+    });
+  }
+});
