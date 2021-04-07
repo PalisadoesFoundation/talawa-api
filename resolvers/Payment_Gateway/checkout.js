911aa2ea3f46b3818b2f2c29c0336c597948857e
Yasharth dubey <yashdubeywinner@gmail.com>
2021-04-07 22:58:03 +0530

    added middleware to check the response before payment

--- resolvers/Payment_Gateway/checkout.js
+++ resolvers/Payment_Gateway/checkout.js
@@ -1,29 +1,34 @@
 const stripe = require('stripe')('PUT YOUR SECURITY KEY HERE');
 const express = require("express");
 const router = express.Router();
-router.post("/payment", async (req, res) => {
-	let {amount, id} = req.body //here amount is the amount for donation, and id is the way the user is going to pay like card UPI and other methods
-	try {
-		const payment = await stripe.paymentIntents.create({
-			amount,
-			currency: "USD",//Put your currency here,
-			description: "description",//put your description here,
-			payment_method: id,
-			confirm: true
-		});
-		console.log("Payment", payment)
-		res.json({
-			message: "Payment successful",
-			success: true
-		})
-	} catch (error) {
-		console.log("Error", error)
-		res.json({
-			message: "Payment failed",
-			success: false
-		})
-	}
-});
+router.post("/payment", 
+	[
+		check('amount','amount is not in the body').not().isEmpty(),
+		check('id','The method of payment is not present').not().isEmpty()
+	],
+	async (req, res) => {
+		let {amount, id} = req.body //here amount is the amount for donation, and id is the way the user is going to pay like card UPI and other methods
+		try {
+			const payment = await stripe.paymentIntents.create({
+				amount,
+				currency: "USD",//Put your currency here,
+				description: "description",//put your description here,
+				payment_method: id,
+				confirm: true
+			});
+			console.log("Payment", payment)
+			res.json({
+				message: "Payment successful",
+				success: true
+			})
+		} catch (error) {
+			console.log("Error", error)
+			res.json({
+				message: "Payment failed",
+				success: false
+			})
+		}
+	});
 
 app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
 	const event = request.body;
