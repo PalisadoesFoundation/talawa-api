86048e27138b154405bd2c4c4ffdcd36bffd8572
ShubhamBhardwaj <shubham.11901502@lpu.in>
2021-04-08 14:33:16 +0530

    appended form data with mutation

--- tests/imageTest.spec.js
+++ tests/imageTest.spec.js
@@ -12,6 +12,28 @@ const uploadBody = async (body) => {
 describe('image upload', () => {
   test('signup test', async () => {
     const body = new FormData();
+    body.append(
+      'operations',
+      JSON.stringify({
+        query: `
+          mutation ($file: Upload!) {
+            signUp (data:{
+              email:"user1011@gmail.com",
+              firstName:"user_fname",
+              lastName:"user_lname",
+              password:"Password1234",
+            }, file: $file){
+              user{
+                image
+              }
+            }
+          }
+          `,
+        variables: {
+          file: null,
+        },
+      })
+    );
 
     expect(data.body.error).toBe(null || undefined);
   });
