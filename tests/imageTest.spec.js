59b6f4d2ffe2e2627e87a8beab40e294665b3d14
ShubhamBhardwaj <shubham.11901502@lpu.in>
2021-04-12 10:58:50 +0530

    Testing for image upload done.

---
+++ tests/imageTest.spec.js
@@ -0,0 +1,52 @@
+const { URL } = require('../constants');
+const path = require('path');
+const fs = require('fs');
+const FormData = require('form-data');
+const fetch = require('node-fetch');
+
+const uploadBody = async (body) => {
+  const response = await fetch(URL, { method: 'POST', body });
+  return response;
+};
+
+describe('image upload', () => {
+  test('signup test', async () => {
+    const body = new FormData();
+    body.append(
+      'operations',
+      JSON.stringify({
+        query: `
+          mutation ($file: Upload!) {
+            signUp (data:{
+              email:"user1011@gmail.com",
+              firstName:"user_fname",
+              lastName:"user_lname",
+              password:"Password1234",
+            }, file: $file){
+              user{
+                image
+              }
+            }
+          }
+          `,
+        variables: {
+          file: null,
+        },
+      })
+    );
+    const file_name = 'test_image.png';
+    body.append(
+      'map',
+      JSON.stringify({
+        1: ['variables.file'],
+      })
+    );
+    body.append(
+      '1',
+      fs.createReadStream(path.resolve(__dirname, `./test_image/${file_name}`))
+    );
+    const data = await uploadBody(body);
+    console.log(data);
+    expect(data.body.error).toBe(null || undefined);
+  });
+});
