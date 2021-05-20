0c886c7f297fd5fbc62d978acdb5b3c6d81ec113
XshubhamX <shubham.11901502@lpu.in>
2021-05-20 15:31:16 +0530

    function to confirm otp while signing up

---
+++ helper_functions/confirmOtp.js
@@ -0,0 +1,22 @@
+const Otp = require('../models/Otp');
+
+const confirmOtp = async (email, otp) => {
+  let otp_object;
+
+  try {
+    otp_object = await Otp.findOne({
+      email,
+      text: otp,
+    });
+  } catch (e) {
+    throw new Error(e);
+  }
+
+  if (!otp_object) {
+    throw new Error('Incorrect Otp');
+  }
+
+  return true;
+};
+
+module.exports = { confirmOtp };
