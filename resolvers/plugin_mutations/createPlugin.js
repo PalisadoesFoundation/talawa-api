71596800b5c79123f7eb5463411540f57c8628e6
Sumitra Saksham <35868598+sumitra19jha@users.noreply.github.com>
2021-08-04 21:46:23 +0530

    Plugin Added

--- resolvers/plugin_mutations/createPlugin.js
+++ resolvers/plugin_mutations/createPlugin.js
@@ -1,1 +1,45 @@
-module.exports = async () => {};
+const Plugin = require('../../models/Plugins');
+const PluginField = require('../../models/PluginsField');
+const User = require('../../models/User');
+
+const { NotFoundError } = require('errors');
+const requestContext = require('talawa-request-context');
+
+module.exports = async (parent, args, context) => {
+  // gets user in token - to be used later on
+  const user = await User.findOne({
+    _id: context.userId,
+  });
+  if (!user) {
+    throw new NotFoundError(
+      requestContext.translate('user.notFound'),
+      'user.notFound',
+      'user'
+    );
+  }
+
+  let pluginAddnFields = [];
+  if (args.plugin.fields.length > 0) {
+    for (let i = 0; i < args.plugin.fields.length; i++) {
+      let pluginField = new PluginField({
+        key: args.plugin.fields[i].key,
+        value: args.plugin.fields[i].value,
+      });
+
+      pluginAddnFields.push(pluginField);
+    }
+  }
+
+  let plugin = new Plugin({
+    orgId: args.plugin.orgId,
+    pluginName: args.plugin.pluginName,
+    pluginKey: args.plugin.pluginKey,
+    additionalInfo: pluginAddnFields,
+  });
+
+  plugin = await plugin.save();
+
+  return {
+    ...plugin._doc,
+  };
+};
