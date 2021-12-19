6253835579605f8923fc446964418eac0207e204
sandeep-v1404 <valpasanisandeep@gmail.com>
2021-12-19 15:16:54 +0530

    Appended Filtering Tests for orgConnection

--- tests/organizations_connection.spec.js
+++ tests/organizations_connection.spec.js
@@ -5,17 +5,17 @@ const getToken = require('./functions/getToken');
 let token;
 
 beforeAll(async () => {
-    token = await getToken();
+  token = await getToken();
 });
 
 describe('Organizations Connection Resolvers', () => {
-    let createdOrgId;
+  let createdOrgId;
 
-    test('createOrganization', async () => {
-        const createdOrgResponse = await axios.post(
-            URL,
-            {
-                query: `
+  test('createOrganization', async () => {
+    const createdOrgResponse = await axios.post(
+      URL,
+      {
+        query: `
             mutation {
                 createOrganization(data: {
                     name:"test org"
@@ -27,28 +27,27 @@ describe('Organizations Connection Resolvers', () => {
                     }
             }
               `,
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            }
-        );
-        const { data } = createdOrgResponse;
-        createdOrgId = data?.data.createOrganization._id;
-        expect(data.data.createOrganization).toEqual(
-            expect.objectContaining({
-                _id: expect.any(String),
-            })
-        );
-    });
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const { data } = createdOrgResponse;
+    createdOrgId = data?.data.createOrganization._id;
+    expect(data.data.createOrganization).toEqual(
+      expect.objectContaining({
+        _id: expect.any(String),
+      })
+    );
+  });
 
-    test('Organizations Connection with Filter as Id', async () => {
-        const orgConnectionResponse = await axios.post(
-            URL,
-            {
-
-                query: `
+  test('Organizations Connection with Filter id', async () => {
+    const orgConnectionResponse = await axios.post(
+      URL,
+      {
+        query: `
                    query {
                         organizationsConnection(where: { id:"${createdOrgId}" }) {
                             _id
@@ -59,36 +58,142 @@ describe('Organizations Connection Resolvers', () => {
                         }
                     }
             `,
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            }
-        );
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
-        expect(
-            orgConnectionResponse.data.data.organizationsConnection.length
-        ).toEqual(1);
+    expect(
+      orgConnectionResponse.data.data.organizationsConnection
+    ).toHaveLength(1);
 
-        expect(
-            orgConnectionResponse.data.data.organizationsConnection[0]
-        ).toEqual(
-            expect.objectContaining({
-                _id: createdOrgId,
-                name: expect.any(String),
-                description: expect.any(String),
-                isPublic: expect.any(Boolean),
-                visibleInSearch: expect.any(Boolean),
-            })
-        );
+    expect(orgConnectionResponse.data.data.organizationsConnection[0]).toEqual(
+      expect.objectContaining({
+        _id: createdOrgId,
+        name: expect.any(String),
+        description: expect.any(String),
+        isPublic: expect.any(Boolean),
+        visibleInSearch: expect.any(Boolean),
+      })
+    );
+  });
+
+  test('Organizations Connection with Filter id_not', async () => {
+    const orgConnectionResponse = await axios.post(
+      URL,
+      {
+        query: `
+                  query {
+                    organizationsConnection(where:{id_not:"${createdOrgId}"}) {
+                        _id
+                        name
+                        description
+                        isPublic
+                        visibleInSearch
+                    }
+                }
+            `,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    orgConnectionResponse.data?.data.organizationsConnection.forEach((org) => {
+      expect(org).not.toEqual(
+        expect.objectContaining({
+          _id: createdOrgId,
+          name: expect.any(String),
+          description: expect.any(String),
+          isPublic: expect.any(Boolean),
+          visibleInSearch: expect.any(Boolean),
+        })
+      );
     });
+  });
 
-    test('Organizations Connection with Filters - isPublic, visibleInSearch', async () => {
-        const orgConnectionResponse = await axios.post(
-            URL,
-            {
-                query: `
+  test('Organizations Connection with Filter id_in', async () => {
+    const orgConnectionResponse = await axios.post(
+      URL,
+      {
+        query: `
+                  query {
+                    organizationsConnection(where:{id_in:"${createdOrgId}"}) {
+                        _id
+                        name
+                        description
+                        isPublic
+                        visibleInSearch
+                    }
+                }
+            `,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    orgConnectionResponse.data?.data.organizationsConnection.forEach((org) => {
+      expect(org).toEqual(
+        expect.objectContaining({
+          _id: createdOrgId,
+          name: expect.any(String),
+          description: expect.any(String),
+          isPublic: expect.any(Boolean),
+          visibleInSearch: expect.any(Boolean),
+        })
+      );
+    });
+  });
+
+  test('Organizations Connection with Filter id_not_in', async () => {
+    const orgConnectionResponse = await axios.post(
+      URL,
+      {
+        query: `
+                  query {
+                    organizationsConnection(where:{id_not_in:"${createdOrgId}"}) {
+                        _id
+                        name
+                        description
+                        isPublic
+                        visibleInSearch
+                    }
+                }
+            `,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    orgConnectionResponse.data?.data.organizationsConnection.forEach((org) => {
+      expect(org).not.toEqual(
+        expect.objectContaining({
+          _id: createdOrgId,
+          name: expect.any(String),
+          description: expect.any(String),
+          isPublic: expect.any(Boolean),
+          visibleInSearch: expect.any(Boolean),
+        })
+      );
+    });
+  });
+
+  test('Organizations Connection with Filters - isPublic, visibleInSearch', async () => {
+    const orgConnectionResponse = await axios.post(
+      URL,
+      {
+        query: `
                     query {
                         organizationsConnection(where: { isPublic: true, visibleInSearch: true }) {
                             _id
@@ -99,31 +204,31 @@ describe('Organizations Connection Resolvers', () => {
                         }
                     }
             `,
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            }
-        );
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
-        orgConnectionResponse.data.data.organizationsConnection.forEach(org => {
-            expect(org).toEqual(
-                expect.objectContaining({
-                    name: expect.any(String),
-                    description: expect.any(String),
-                    isPublic: expect.any(Boolean),
-                    visibleInSearch: expect.any(Boolean),
-                })
-            );
+    orgConnectionResponse.data.data.organizationsConnection.forEach((org) => {
+      expect(org).toEqual(
+        expect.objectContaining({
+          name: expect.any(String),
+          description: expect.any(String),
+          isPublic: expect.any(Boolean),
+          visibleInSearch: expect.any(Boolean),
         })
+      );
     });
+  });
 
-    test('Organizations Connection with Parameter first=1', async () => {
-        const orgConnectionResponse = await axios.post(
-            URL,
-            {
-                query: `
+  test('Organizations Connection with Parameter first=1', async () => {
+    const orgConnectionResponse = await axios.post(
+      URL,
+      {
+        query: `
                     query {
                         organizationsConnection(first:1) {
                             name
@@ -133,27 +238,25 @@ describe('Organizations Connection Resolvers', () => {
                         }
                     }
             `,
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            }
-        );
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
-        expect(
-            orgConnectionResponse.data.data.organizationsConnection.length
-        ).toEqual(1);
+    expect(
+      orgConnectionResponse.data.data.organizationsConnection
+    ).toHaveLength(1);
 
-        expect(
-            orgConnectionResponse.data.data.organizationsConnection[0]
-        ).toEqual(
-            expect.objectContaining({
-                name: expect.any(String),
-                description: expect.any(String),
-                isPublic: expect.any(Boolean),
-                visibleInSearch: expect.any(Boolean),
-            })
-        );
-    });
+    expect(orgConnectionResponse.data.data.organizationsConnection[0]).toEqual(
+      expect.objectContaining({
+        name: expect.any(String),
+        description: expect.any(String),
+        isPublic: expect.any(Boolean),
+        visibleInSearch: expect.any(Boolean),
+      })
+    );
+  });
 });
