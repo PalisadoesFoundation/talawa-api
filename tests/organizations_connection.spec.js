e40cd2289880b33ca48752333a81cfdde82262a9
sandeep-v1404 <valpasanisandeep@gmail.com>
2021-12-19 13:32:57 +0530

    Created Basic Tests for orgConnection

---
+++ tests/organizations_connection.spec.js
@@ -0,0 +1,159 @@
+const axios = require('axios');
+const { URL } = require('../constants');
+const getToken = require('./functions/getToken');
+
+let token;
+
+beforeAll(async () => {
+    token = await getToken();
+});
+
+describe('Organizations Connection Resolvers', () => {
+    let createdOrgId;
+
+    test('createOrganization', async () => {
+        const createdOrgResponse = await axios.post(
+            URL,
+            {
+                query: `
+            mutation {
+                createOrganization(data: {
+                    name:"test org"
+                    description:"test description"
+                    isPublic: true
+                    visibleInSearch: true
+                    }) {
+                        _id
+                    }
+            }
+              `,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        const { data } = createdOrgResponse;
+        createdOrgId = data?.data.createOrganization._id;
+        expect(data.data.createOrganization).toEqual(
+            expect.objectContaining({
+                _id: expect.any(String),
+            })
+        );
+    });
+
+    test('Organizations Connection with Filter as Id', async () => {
+        const orgConnectionResponse = await axios.post(
+            URL,
+            {
+
+                query: `
+                   query {
+                        organizationsConnection(where: { id:"${createdOrgId}" }) {
+                            _id
+                            name
+                            description
+                            isPublic
+                            visibleInSearch
+                        }
+                    }
+            `,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        expect(
+            orgConnectionResponse.data.data.organizationsConnection.length
+        ).toEqual(1);
+
+        expect(
+            orgConnectionResponse.data.data.organizationsConnection[0]
+        ).toEqual(
+            expect.objectContaining({
+                _id: createdOrgId,
+                name: expect.any(String),
+                description: expect.any(String),
+                isPublic: expect.any(Boolean),
+                visibleInSearch: expect.any(Boolean),
+            })
+        );
+    });
+
+    test('Organizations Connection with Filters - isPublic, visibleInSearch', async () => {
+        const orgConnectionResponse = await axios.post(
+            URL,
+            {
+                query: `
+                    query {
+                        organizationsConnection(where: { isPublic: true, visibleInSearch: true }) {
+                            _id
+                            name
+                            description
+                            isPublic
+                            visibleInSearch
+                        }
+                    }
+            `,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        orgConnectionResponse.data.data.organizationsConnection.forEach(org => {
+            expect(org).toEqual(
+                expect.objectContaining({
+                    name: expect.any(String),
+                    description: expect.any(String),
+                    isPublic: expect.any(Boolean),
+                    visibleInSearch: expect.any(Boolean),
+                })
+            );
+        })
+    });
+
+    test('Organizations Connection with Parameter first=1', async () => {
+        const orgConnectionResponse = await axios.post(
+            URL,
+            {
+                query: `
+                    query {
+                        organizationsConnection(first:1) {
+                            name
+                            description
+                            isPublic
+                            visibleInSearch
+                        }
+                    }
+            `,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        expect(
+            orgConnectionResponse.data.data.organizationsConnection.length
+        ).toEqual(1);
+
+        expect(
+            orgConnectionResponse.data.data.organizationsConnection[0]
+        ).toEqual(
+            expect.objectContaining({
+                name: expect.any(String),
+                description: expect.any(String),
+                isPublic: expect.any(Boolean),
+                visibleInSearch: expect.any(Boolean),
+            })
+        );
+    });
+});
