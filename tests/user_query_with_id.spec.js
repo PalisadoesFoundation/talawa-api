9dd6e5bddb4e751d6f6554289826a3128f5e99bf
sandeep-v1404 <valpasanisandeep@gmail.com>
2022-01-05 03:46:35 +0530

    Test: Created TestCase for me, user Queries

---
+++ tests/user_query_with_id.spec.js
@@ -0,0 +1,190 @@
+const axios = require('axios');
+const { URL } = require('../constants');
+const getToken = require('./functions/getToken');
+
+let token;
+let userId;
+
+beforeAll(async () => {
+  token = await getToken();
+});
+
+describe('user query', () => {
+  test('Get User Details with ID', async () => {
+    const loggedInUserResponse = await axios.post(
+      URL,
+      {
+        query: `
+      {
+        me {
+          _id
+        }
+      }
+      `,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    userId = loggedInUserResponse.data.data.me._id;
+
+    const response = await axios.post(
+      URL,
+      {
+        query: `
+          {
+          user(id: "${userId}") {
+            _id
+            firstName
+            lastName
+            email
+            userType
+            appLanguageCode
+
+            createdOrganizations {
+              _id
+              name
+              description
+              isPublic
+              visibleInSearch
+              apiUrl
+              creator {
+                _id
+                firstName
+              }
+              members {
+                _id
+                ficorstName
+              }
+              admins {
+                _id
+                firstName
+              }
+              membershipRequests {
+                _id
+              }
+              blockedUsers {
+                _id
+              }
+            }
+          }
+        }
+      `,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const { data } = response;
+    expect(data.data.user).toEqual(
+      expect.objectContaining({
+        _id: expect.any(String),
+        firstName: expect.any(String),
+        lastName: expect.any(String),
+        email: expect.any(String),
+        userType: expect.any(String),
+        appLanguageCode: expect.any(String),
+      })
+    );
+
+    //Array of Organizations
+    const userCreatedOrganizationsData = data.data.user.createdOrganizations;
+
+    // Asserting each Organization by mapping
+    userCreatedOrganizationsData.map((org) => {
+      expect(org).toEqual(
+        expect.objectContaining({
+          _id: expect.any(String),
+          name: expect.any(String),
+          description: expect.any(String),
+          isPublic: expect.any(Boolean),
+          visibleInSearch: expect.any(Boolean),
+          apiUrl: expect.any(String),
+          creator: expect.objectContaining({
+            _id: expect.any(String),
+            firstName: expect.any(String),
+          }),
+        })
+      );
+
+      // Asserting each Organization's Members Array
+      org.members.map((member) => {
+        expect(member).toEqual(
+          expect.objectContaining({
+            _id: expect.any(String),
+            firstName: expect.any(String),
+          })
+        );
+      });
+
+      // Asserting each Organization's Admins Array
+      org.admins.map((admin) => {
+        expect(admin).toEqual(
+          expect.objectContaining({
+            _id: expect.any(String),
+            firstName: expect.any(String),
+          })
+        );
+      });
+
+      // Asserting each Organization's membershipRequests Array
+      org.membershipRequests.map((membershipRequest) => {
+        expect(membershipRequest).toEqual(
+          expect.objectContaining({
+            _id: expect.any(String),
+          })
+        );
+      });
+
+      // Asserting each Organization's blockedUsers Array
+      org.blockedUsers.map((blockedUser) => {
+        expect(blockedUser).toEqual(
+          expect.objectContaining({
+            _id: expect.any(String),
+          })
+        );
+      });
+    });
+  });
+
+  test('User Query without Authorization', async () => {
+    const response = await axios.post(URL, {
+      query: `
+        {
+          user(id: "61d4676865721d2dde1f0b6c") {
+            _id
+            firstName
+            lastName
+            email
+            userType
+            appLanguageCode
+          }
+        }
+      `,
+    });
+
+    expect(response.data.errors[0]).toEqual(
+      expect.objectContaining({
+        message: 'User is not authenticated',
+        status: 422,
+      })
+    );
+
+    expect(response.data.errors[0].data[0]).toEqual(
+      expect.objectContaining({
+        message: 'User is not authenticated',
+        code: 'user.notAuthenticated',
+        param: 'userAuthentication',
+        metadata: {},
+      })
+    );
+
+    expect(response.data.data).toEqual(null);
+  });
+});
