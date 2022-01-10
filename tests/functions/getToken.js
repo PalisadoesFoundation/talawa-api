ac410f011ab3dc76ca4385df742a4fa513ab052a
sandeep-v1404 <valpasanisandeep@gmail.com>
2022-01-10 16:59:51 +0530

    Updated Workflow

--- tests/functions/getToken.js
+++ tests/functions/getToken.js
@@ -22,5 +22,33 @@ module.exports = async () => {
   });
 
   const { data } = response;
+
+  if (data.errors && data.errors[0].message === 'User not found') {
+    const signUpResponse = await axios.post(URL, {
+      query: `
+            mutation {
+                signUp(data: {
+                  firstName:"testdb2",
+                  lastName:"testdb2"
+                  email: "testdb2@test.com"
+                  password:"password"
+                }) {
+                    user {
+                      _id
+                      firstName
+                      lastName
+                      email
+                      userType
+                      appLanguageCode
+                      image
+                    }
+                    accessToken
+                  }
+                }
+                `,
+    });
+    const { data } = signUpResponse;
+    return data.data.signUp.accessToken;
+  }
   return data.data.login.accessToken;
 };
--- tests/user.spec.js
+++ tests/user.spec.js
@@ -1,72 +1,54 @@
 const axios = require('axios');
-const shortid = require('shortid');
-const { URL } = require('../constants');
 
-describe('user resolvers', () => {
-  test('allUsers', async () => {
-    const response = await axios.post(URL, {
-      query: `query {
-                users {
-                  _id
-                  firstName
-                  lastName
-                  email
-                }
-              }`,
-    });
-    const { data } = response;
-    expect(Array.isArray(data.data.users)).toBeTruthy();
+const { URL } = require('../../constants');
+
+// sets token before every test
+module.exports = async () => {
+  const response = await axios.post(URL, {
+    query: `
+    mutation{
+      login(data:{
+                email:"testdb2@test.com",
+                password:"password"
+      }){
+        user{
+        _id
+        }
+        accessToken
+        refreshToken
+      }
+    }
+    `,
   });
 
-  test('signUp', async () => {
-    var id = shortid.generate();
-    var email = `${id}@test.com`;
-    const response = await axios.post(URL, {
+  const { data } = response;
+
+  if (data.errors && data.errors[0].message === 'User not found') {
+    const signUpResponse = await axios.post(URL, {
       query: `
             mutation {
                 signUp(data: {
                   firstName:"testdb2",
                   lastName:"testdb2"
-                  email: "${email}"
+                  email: "testdb2@test.com"
                   password:"password"
                 }) {
-                  user{
-                    _id
+                    user {
+                      _id
+                      firstName
+                      lastName
+                      email
+                      userType
+                      appLanguageCode
+                      image
+                    }
+                    accessToken
                   }
-                  accessToken
                 }
-              }
-              `,
+                `,
     });
-    const { data } = response;
-    expect(data.data.signUp).toEqual(
-      expect.objectContaining({
-        accessToken: expect.any(String),
-      })
-    );
-  });
-
-  test('login', async () => {
-    const response = await axios.post(URL, {
-      query: `
-            mutation{
-                login(data: {
-                  email:"testdb2@test.com",
-                  password:"password"
-                }) {
-                  user{
-                    _id
-                  }
-                  accessToken
-                }  
-              }
-              `,
-    });
-    const { data } = response;
-    expect(data.data.login).toEqual(
-      expect.objectContaining({
-        accessToken: expect.any(String),
-      })
-    );
-  });
-});
+    const { data } = signUpResponse;
+    return data.data.signUp.accessToken;
+  }
+  return data.data.login.accessToken;
+};
