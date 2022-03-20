bd8df7ce57f013a1c2561e6fde129315ac4543a2
asmitsirohi <asmitsirohi9761@gmail.com>
2022-03-20 13:27:15 +0530

    file for users tests added

---
+++ tests/resolvers/user_query/users.spec.js
@@ -0,0 +1,20 @@
+const shortid = require('shortid');
+
+const database = require('../../../db');
+const getUserId = require('../../functions/getUserIdFromSignup');
+const users = require('../../../lib/resolvers/user_query/users');
+
+beforeAll(async () => {
+  require('dotenv').config(); // pull env variables from .env file
+  await database.connect();
+  let generatedEmail = `${shortid.generate().toLowerCase()}@test.com`;
+  userId = await getUserId(generatedEmail);
+});
+
+afterAll(() => {
+  database.disconnect();
+});
+
+describe('Testing users resolver', () => {
+  test('Testing basic info of any user', async () => {});
+});
