8f50f901cf028ccbe5d03d85ad1752b7862c7c69
asmitsirohi <asmitsirohi9761@gmail.com>
2022-03-20 15:31:23 +0530

    lint fixes

--- tests/resolvers/user_query/users.spec.js
+++ tests/resolvers/user_query/users.spec.js
@@ -4,6 +4,8 @@ const database = require('../../../db');
 const getUserId = require('../../functions/getUserIdFromSignup');
 const users = require('../../../lib/resolvers/user_query/users');
 
+let userId;
+
 beforeAll(async () => {
   require('dotenv').config(); // pull env variables from .env file
   await database.connect();
