dd7c96bb9cec1523295acb30b4e8585803328923
sandeep-v1404 <valpasanisandeep@gmail.com>
2022-04-19 19:01:41 +0530

    rafactor:Linting

--- tests/resolvers/admin_mutations/createAdmin.spec.js
+++ tests/resolvers/admin_mutations/createAdmin.spec.js
@@ -135,7 +135,9 @@ describe('Unit testing', () => {
 
     await expect(async () => {
       await createAdmin({}, args, context);
-    }).rejects.toEqual(Error('User is not authorized for performing this operation'));
+    }).rejects.toEqual(
+      Error('User is not authorized for performing this operation')
+    );
   });
 
   test('Create Admin Mutation when user is not a member of the organization', async () => {
@@ -240,7 +242,9 @@ describe('Unit testing', () => {
 
     await expect(async () => {
       await createAdmin({}, args, context);
-    }).rejects.toEqual(Error('User is not authorized for performing this operation'));
+    }).rejects.toEqual(
+      Error('User is not authorized for performing this operation')
+    );
   });
 
   test('Create Admin Mutation', async () => {
