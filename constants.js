847a6babf09f8bf0b902eed67c9738dc41f57cc1
Ebrahim <Ebrahim.elhabashy354@gmail.com>
2022-04-20 04:07:41 +0200

    added constant file

--- constants.js
+++ constants.js
@@ -1,7 +1,16 @@
 let URL = 'http://calico.palisadoes.org/talawa/graphql';
 
+const USER_NOT_AUTHORIZED = 'User not authorized';
+const USER_NOT_FOUND = 'User not found';
+const ORGANIZATION_NOT_FOUND = 'Organization not found';
+
 if (process.env.NODE_ENV === 'test') {
   URL = 'http://localhost:4000/graphql';
 }
 
-module.exports.URL = URL;
+module.exports = {
+  URL,
+  USER_NOT_AUTHORIZED,
+  USER_NOT_FOUND,
+  ORGANIZATION_NOT_FOUND,
+};
