64dd129c3c0d769f0ed18ea828f0e7094f516778
sandeep-v1404 <valpasanisandeep@gmail.com>
2022-04-22 16:36:42 +0530

    test: events query tests using orderby dynamically

--- tests/resolvers/event_query/events.spec.js
+++ tests/resolvers/event_query/events.spec.js
@@ -11,28 +11,53 @@ afterAll(() => {
 });
 
 describe('Unit testing', () => {
-  test('Events Query', async () => {
-    const args = {
-      orderBy: 'id_ASC',
-    };
-    const response = await eventsQuery({}, args);
-    response.map((event) => {
-      expect(typeof event.status === 'string').toBeTruthy();
-      expect(typeof event.title === 'string').toBeTruthy();
-      expect(typeof event.description === 'string').toBeTruthy();
-      expect(typeof event.isPublic === 'boolean').toBeTruthy();
-      expect(typeof event.isRegisterable === 'boolean').toBeTruthy();
-      expect(typeof event.recurring === 'boolean').toBeTruthy();
-      expect(typeof event.recurrance === 'string').toBeTruthy();
-      expect(
-        typeof event.location === 'string' ||
-          typeof event.location === 'object' ||
-          event.location === null
-      ).toBeTruthy();
-      expect(typeof event.startDate === 'string').toBeTruthy();
-      expect(typeof event.allDay === 'boolean').toBeTruthy();
-      expect(typeof event.startTime === 'string').toBeTruthy();
-      expect(typeof event.endTime === 'string').toBeTruthy();
+  const orderByArgs = [
+    'id_ASC',
+    'id_DESC',
+    'title_ASC',
+    'title_DESC',
+    'description_ASC',
+    'description_DESC',
+    'startDate_ASC',
+    'startDate_DESC',
+    'endDate_ASC',
+    'endDate_DESC',
+    'allDay_ASC',
+    'allDay_DESC',
+    'startTime_ASC',
+    'startTime_DESC',
+    'endTime_ASC',
+    'endTime_DESC',
+    'recurrance_ASC',
+    'recurrance_DESC',
+    'location_ASC',
+    'location_DESC',
+  ];
+
+  orderByArgs.map((arg) => {
+    test(`Events Query with orderBy ${arg}`, async () => {
+      const args = {
+        orderBy: arg,
+      };
+      const response = await eventsQuery({}, args);
+      response.map((event) => {
+        expect(typeof event.status === 'string').toBeTruthy();
+        expect(typeof event.title === 'string').toBeTruthy();
+        expect(typeof event.description === 'string').toBeTruthy();
+        expect(typeof event.isPublic === 'boolean').toBeTruthy();
+        expect(typeof event.isRegisterable === 'boolean').toBeTruthy();
+        expect(typeof event.recurring === 'boolean').toBeTruthy();
+        expect(typeof event.recurrance === 'string').toBeTruthy();
+        expect(
+          typeof event.location === 'string' ||
+            typeof event.location === 'object' ||
+            event.location === null
+        ).toBeTruthy();
+        expect(typeof event.startDate === 'string').toBeTruthy();
+        expect(typeof event.allDay === 'boolean').toBeTruthy();
+        expect(typeof event.startTime === 'string').toBeTruthy();
+        expect(typeof event.endTime === 'string').toBeTruthy();
+      });
     });
   });
 });
