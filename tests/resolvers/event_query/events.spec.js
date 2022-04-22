d340fd4b797eaeb0fc4da6f513adb30e8b74b065
sandeep-v1404 <valpasanisandeep@gmail.com>
2022-04-23 00:48:47 +0530

    refactor: cleaned up logs

--- tests/resolvers/event_query/events.spec.js
+++ tests/resolvers/event_query/events.spec.js
@@ -41,7 +41,6 @@ describe('Unit testing', () => {
       };
       const response = await eventsQuery({}, args);
       response.map((event) => {
-        console.log(typeof event.startTime, typeof event.endTime)
         expect(typeof event.status === 'string').toBeTruthy();
         expect(typeof event.title === 'string').toBeTruthy();
         expect(typeof event.description === 'string').toBeTruthy();
@@ -51,17 +50,21 @@ describe('Unit testing', () => {
         expect(typeof event.recurrance === 'string').toBeTruthy();
         expect(
           typeof event.location === 'string' ||
-          event.location === null ||
-          event.location === undefined
+            event.location === null ||
+            event.location === undefined
         ).toBeTruthy();
         expect(typeof event.startDate === 'string').toBeTruthy();
         expect(typeof event.allDay === 'boolean').toBeTruthy();
-        expect(typeof event.startTime === 'string' ||
-          event.startTime === null ||
-          event.startTime === undefined).toBeTruthy();
-        expect(typeof event.endTime === 'string' ||
-          event.endTime === null ||
-          event.endTime === undefined).toBeTruthy();
+        expect(
+          typeof event.startTime === 'string' ||
+            event.startTime === null ||
+            event.startTime === undefined
+        ).toBeTruthy();
+        expect(
+          typeof event.endTime === 'string' ||
+            event.endTime === null ||
+            event.endTime === undefined
+        ).toBeTruthy();
       });
     });
   });
