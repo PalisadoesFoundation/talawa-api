32b5132a934af53c6278f3644409d88241c2bbe0
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-06-01 12:01:12 +0000

    Add Test  : Added Valid JSON Check

--- tests/resolvers/group_chat_query/groupChatMessages.spec.js
+++ tests/resolvers/group_chat_query/groupChatMessages.spec.js
@@ -13,4 +13,12 @@ describe('tests for lib/resolvers/group_chat_query/groupChatMessages.js', () => 
   test('Should return  a JSON response (getting the group chat messages from the database)', async () => {
     expect(await groupChatMessageFindMethod()).toBeTruthy();
   });
+  test('groupChatMessasges Produces metadata object that can be parsed to valid JSON', async () => {
+    const temp = await groupChatMessageFindMethod();
+    const parseJson = () => {
+      const json = JSON.stringify(temp);
+      JSON.parse(json);
+    };
+    expect(parseJson).not.toThrow();
+  });
 });
