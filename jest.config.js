19be7a0de2a70a54577cd784e82c77a9f5ba6437
xoldyckk <xoldyckk@gmail.com>
2022-06-14 18:29:55 +0530

    added configuration for jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
-// in jest.config.js
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   // extends time taken before the request times out to 30000ms
   testTimeout: 30000,
   collectCoverage: true,
   testEnvironment: 'node',
   coverageDirectory: './coverage/',
-  collectCoverageFrom: ['lib/**/*.js*'],
-  coveragePathIgnorePatterns: ['/node_modules/', 'lib/helper_lib'],
+  collectCoverageFrom: ['./src/lib/**/*.ts*', './src/lib/**/*.js*'],
+  coveragePathIgnorePatterns: ['/node_modules/', './src/lib/helper_lib'],
+  preset: 'ts-jest',
 };
