3f18b52db28b85b0260b2bfd2e01db467107608d
xoldyckk <xoldyckk@gmail.com>
2022-06-16 03:06:12 +0530

    migrated src/lib/config/app.js to typescript

--- src/lib/config/app.ts
+++ src/lib/config/app.ts
@@ -1,7 +1,10 @@
-module.exports = {
+// Types are automatically inferred by typescript
+export const appConfig = {
   env: process.env.NODE_ENV,
   colorize_logs: process.env.COLORIZE_LOGS,
   log_level: process.env.LOG_LEVEL,
   defaultLocale: 'en',
   supportedLocales: ['hi', 'en', 'zh', 'fr', 'sp'],
 };
+
+export default appConfig;
