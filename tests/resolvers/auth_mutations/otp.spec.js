85edc2023b0c67b2985344955200a17b9ac3f012
asmitsirohi <asmitsirohi9761@gmail.com>
2022-06-16 22:03:18 +0530

    tests fixes

--- tests/resolvers/auth_mutations/otp.spec.js
+++ tests/resolvers/auth_mutations/otp.spec.js
@@ -1,36 +1,42 @@
+const axios = require('axios');
 const shortid = require('shortid');
 
-const database = require('../../../db');
+const { URL } = require('../../../constants');
 const getToken = require('../../functions/getToken');
-const otp = require('../../../lib/resolvers/auth_mutations/otp');
 
 let generatedEmail;
 let token;
 let otpToken;
 
 beforeAll(async () => {
-  require('dotenv').config();
-  await database.connect();
-
   generatedEmail = `${shortid.generate().toLowerCase()}@test.com`;
   token = await getToken(generatedEmail);
 });
 
-afterAll(() => {
-  database.disconnect();
-});
-
 describe('Testing otp resolver', () => {
   test('otp', async () => {
-    console.log(otpToken, token);
-
-    const args = {
-      data: {
-        email: generatedEmail,
+    console.log(otpToken);
+    const response = await axios.post(
+      URL,
+      {
+        query: `
+            mutation {
+                otp(data: {
+                    email: "${generatedEmail}"
+                }) {
+                    otpToken
+                }
+            }
+            `,
       },
-    };
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
-    const response = await otp({}, args);
-    expect(response).toBeFalsy();
+    const { data } = response;
+    expect(data.data).toBeFalsy();
   });
 });
