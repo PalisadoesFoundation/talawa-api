3e2ac8808f68eab433ae1e0ddc7c7ea1d2dabe07
xoldyckk <xoldyckk@gmail.com>
2022-06-29 23:28:51 +0530

    Improved types for 'src/lib/directives/authDirective.ts' and 'src/lib/directives/roleDirective.ts'

--- src/lib/directives/authDirective.ts
+++ src/lib/directives/authDirective.ts
@@ -1,10 +1,21 @@
 import { SchemaDirectiveVisitor } from 'apollo-server-express';
+import { GraphQLInterfaceType, GraphQLObjectType } from 'graphql';
 import { defaultFieldResolver, GraphQLField } from 'graphql';
 import { UnauthenticatedError } from '../helper_lib/errors';
 import requestContext from '../helper_lib/request-context';
 
 export class AuthenticationDirective extends SchemaDirectiveVisitor {
-  visitFieldDefinition(field: GraphQLField<any, any>) {
+  visitFieldDefinition(
+    field: GraphQLField<any, any>,
+    /*
+    In typescript '_' as prefix of a function argument means that argument is
+    never used in the function definition. When the argument finds it's use
+    in the function definition '_' should be removed from the argument.
+    */
+    _details: {
+      objectType: GraphQLObjectType | GraphQLInterfaceType;
+    }
+  ): GraphQLField<any, any> | void | null {
     const resolver = field.resolve || defaultFieldResolver;
 
     field.resolve = async (root, args, context, info) => {
