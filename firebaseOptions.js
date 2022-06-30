8757ed138717ae6e5a26b337a4d1a624bfe28f7e
Priyanshu Gupta <guptapriyanshu71@gmail.com>
2022-06-30 17:39:02 +0530

    send api keys from backend

---
+++ firebaseOptions.js
@@ -0,0 +1,18 @@
+module.exports.androidFirebaseOptions = {
+  apiKey: 'AIzaSyBkqgTkg2yNRsl7jIx_EtCyF9YqiCJX7sz',
+  appId: '1:803598513727:android:20d58bd8c9aae2102c2a8a',
+  messagingSenderId: '803598513727',
+  projectId: 'talawa-352607',
+  storageBucket: 'talawa-352607.appspot.com',
+}
+
+module.exports.iosFirebaseOptions = {
+  apiKey: 'AIzaSyCw_AKsWnW6oqUq4CC41aDmFcPuLkLO0gc',
+  appId: '1:803598513727:ios:1f06f9c8eef964b42c2a8a',
+  messagingSenderId: '803598513727',
+  projectId: 'talawa-352607',
+  storageBucket: 'talawa-352607.appspot.com',
+  iosClientId:
+      '803598513727-2gt176dpe0ljn5ie967o4d0rm0vo8sm0.apps.googleusercontent.com',
+  iosBundleId: 'com.talawa.app',
+}
