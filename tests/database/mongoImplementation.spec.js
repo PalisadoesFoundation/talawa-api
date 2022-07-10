ae9d1d0f1d77bbb9a628212e857595f0a77fa0f6
Ebrahim <74679570+ebrahim354@users.noreply.github.com>
2022-07-10 16:51:20 +0200

    Initial setup (#726)
    * Initial setup
    
    * simplified creation
    
    * added readme.md file

---
+++ tests/database/mongoImplementation.spec.js
@@ -0,0 +1,67 @@
+const Database = require('../../lib/Database/index');
+const User = require('../../lib/Database/MongoImplementation/schema/User');
+
+const firstUrl = 'mongodb://localhost:27017/db1?retryWrites=true&w=majority';
+const secondUrl = 'mongodb://localhost:27017/db2?retryWrites=true&w=majority';
+
+// not using an options.schema would result in creating a database
+// with all valid schema parts.
+const firstDB = new Database(firstUrl);
+const secondDB = new Database(secondUrl, {
+  schema: {
+    User,
+  },
+});
+
+const FirstUserObject = firstDB.User;
+const SecondUserObject = secondDB.User;
+
+beforeAll(async () => {
+  await firstDB.connect();
+  await secondDB.connect();
+});
+
+afterAll(async () => {
+  await firstDB.User.deleteMany({});
+  await secondDB.User.deleteMany({});
+  await firstDB.disconnect();
+  await secondDB.disconnect();
+});
+
+describe('testing multiple databases functionality', () => {
+  test('first db is working', async () => {
+    const firstDbUser = new FirstUserObject({
+      firstName: 'test',
+      lastName: 'test',
+      email: 'test@gmail.com',
+      password: 'testpassword',
+    });
+
+    await firstDbUser.save();
+    let usr1 = await FirstUserObject.findOne({ firstName: 'test' });
+    let usr2 = await SecondUserObject.findOne({ firstName: 'test' });
+    expect(usr1.firstName).toBe('test');
+    expect(usr2).toBeFalsy();
+    await FirstUserObject.deleteMany({ firstName: 'test' });
+    usr1 = await FirstUserObject.findOne({ firstName: 'test' });
+    expect(usr1).toBeFalsy();
+  });
+
+  test('second db is working', async () => {
+    const secondDbUser = new SecondUserObject({
+      firstName: 'test',
+      lastName: 'test',
+      email: 'test@gmail.com',
+      password: 'testpassword',
+    });
+
+    await secondDbUser.save();
+    let usr1 = await FirstUserObject.findOne({ firstName: 'test' });
+    let usr2 = await SecondUserObject.findOne({ firstName: 'test' });
+    expect(usr2.firstName).toBe('test');
+    expect(usr1).toBeFalsy();
+    await SecondUserObject.deleteMany({ firstName: 'test' });
+    usr2 = await SecondUserObject.findOne({ firstName: 'test' });
+    expect(usr2).toBeFalsy();
+  });
+});
