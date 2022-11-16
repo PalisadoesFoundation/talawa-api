a006a729dd764944aed90c9cb19df8ad0cc1ac3a
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-11-16 11:49:52 +0530

    fix: type errors

--- src/lib/resolvers/Subscription/directMessageChat.ts
+++ src/lib/resolvers/Subscription/directMessageChat.ts
@@ -4,10 +4,10 @@ import { SubscriptionResolvers } from "../../../generated/graphqlCodegen";
 const CHAT_CHANNEL = "CHAT_CHANNEL";
 
 /**
- * This property contained a `subscribe` field, which is used to subscribe the user to get updates for the `CHAT_CHANNEL` event.
- * 
+ * This property contained a `subscribe` field, which is used to subscribe 
+ * the user to get updates for the `CHAT_CHANNEL` event.
  * @remarks To control updates on a per-client basis, the function uses the `withFilter` 
- * method imported from `apollo-server-express` module. 
+ * method imported from `apollo-server-express` module.
  * You can learn about `subscription` {@link https://www.apollographql.com/docs/apollo-server/data/subscriptions/ | here }.
  */
 export const directMessageChat: SubscriptionResolvers["directMessageChat"] = {
