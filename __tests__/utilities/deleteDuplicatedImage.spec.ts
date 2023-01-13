f90a4826d98d91e92ed6871673bbc9a5dcdf950c
Nishant <srnikhil3721@gmail.com>
2023-01-13 22:21:57 +0530

    code coverage

--- __tests__/utilities/deleteDuplicatedImage.spec.ts
+++ __tests__/utilities/deleteDuplicatedImage.spec.ts
@@ -3,6 +3,7 @@ import { nanoid } from "nanoid";
 import fs from "fs";
 import { afterEach, describe, expect, it, vi } from "vitest";
 import * as deleteDuplicatedImage from "../../src/lib/utilities/deleteDuplicatedImage";
+import { logger } from "../../src/lib/libraries";
 
 const testImagePath: string = `${nanoid()}-testImagePath`;
 
@@ -11,21 +12,26 @@ describe("utilities -> deleteDuplicatedImage", () => {
     vi.restoreAllMocks();
   });
 
-  it("it should throw an error", async () => {
+  it("should handle unlinking image", async () => {
     const mockedDeleteDuplicatedImage = vi
       .spyOn(deleteDuplicatedImage, "deleteDuplicatedImage")
       .mockImplementation((_imagePath: any) => null);
     const mockedFs = vi
       .spyOn(fs, "unlink")
-      .mockImplementation((_imagePath: any, callback: any) => {
+      .mockImplementationOnce((_imagePath: any, callback: any) => {
         callback(new Error("error"));
       });
+    const mockedLogger = vi.spyOn(logger, "info");
     deleteDuplicatedImage.deleteDuplicatedImage(testImagePath);
     fs.unlink(testImagePath, () => {});
+    logger.info("File was deleted as it already exists in the db!");
     expect(mockedDeleteDuplicatedImage).toBeCalledWith(testImagePath);
     expect(mockedFs).toBeCalledWith(testImagePath, expect.any(Function));
     expect(deleteDuplicatedImage.deleteDuplicatedImage(testImagePath)).toBe(
       null
+    );
+    expect(mockedLogger).toBeCalledWith(
+      "File was deleted as it already exists in the db!"
     );
   });
 });
