47c4770a0206a8df56a6c80dda9db68934acb369
Nishant <srnikhil3721@gmail.com>
2023-01-13 21:59:44 +0530

    logger recommented to test

--- src/lib/utilities/deleteDuplicatedImage.ts
+++ src/lib/utilities/deleteDuplicatedImage.ts
@@ -1,5 +1,5 @@
 import { unlink, PathLike } from "fs";
-import { logger } from "../libraries";
+// import { logger } from "../libraries";
 
 export const deleteDuplicatedImage = (imagePath: PathLike) => {
   unlink(imagePath, function (error) {
@@ -8,6 +8,6 @@ export const deleteDuplicatedImage = (imagePath: PathLike) => {
     }
 
     // if no error is thrown, file has been deleted successfully
-    logger.info("File was deleted as it already exists in the db!");
+    // logger.info("File was deleted as it already exists in the db!");
   });
 };
