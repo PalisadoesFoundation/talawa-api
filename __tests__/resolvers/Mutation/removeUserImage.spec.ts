ac81d500cac97b7002153033c4adf8af7da98239
Noble Mittal <noblemittal@outlook.com>
2023-01-16 13:50:29 +0530

    increases code coverage for removeUserImage

--- __tests__/resolvers/Mutation/removeUserImage.spec.ts
+++ __tests__/resolvers/Mutation/removeUserImage.spec.ts
@@ -2,12 +2,20 @@ import "dotenv/config";
 import { Document, Types } from "mongoose";
 import { Interface_User, User } from "../../../src/lib/models";
 import { connect, disconnect } from "../../../src/db";
-import { removeUserImage as removeUserImageResolver } from "../../../src/lib/resolvers/Mutation/removeUserImage";
-import { USER_NOT_FOUND } from "../../../src/constants";
+import { USER_NOT_FOUND, USER_NOT_FOUND_MESSAGE } from "../../../src/constants";
 import { nanoid } from "nanoid";
-import { beforeAll, afterAll, describe, it, expect } from "vitest";
+import {
+  beforeAll,
+  afterAll,
+  describe,
+  it,
+  expect,
+  afterEach,
+  vi,
+} from "vitest";
 
 let testUser: Interface_User & Document<any, any, Interface_User>;
+let testImage: string = "testImage";
 
 beforeAll(async () => {
   await connect();
@@ -26,11 +34,30 @@ afterAll(async () => {
 });
 
 describe("resolvers -> Mutation -> removeUserImage", () => {
-  it(`throws NotFoundError if no user exists with _id === context.userId`, async () => {
+  afterEach(() => {
+    vi.doUnmock("../../../src/constants");
+    vi.resetModules();
+  });
+
+  it(`throws NotFoundError if no user exists with _id === context.userId and IN_PRODUCTION === false`, async () => {
     try {
       const context = {
         userId: Types.ObjectId().toString(),
       };
+
+      vi.doMock("../../../src/constants", async () => {
+        const actualConstants: object = await vi.importActual(
+          "../../../src/constants"
+        );
+        return {
+          ...actualConstants,
+          IN_PRODUCTION: false,
+        };
+      });
+
+      const { removeUserImage: removeUserImageResolver } = await import(
+        "../../../src/lib/resolvers/Mutation/removeUserImage"
+      );
 
       await removeUserImageResolver?.({}, {}, context);
     } catch (error: any) {
@@ -38,12 +65,58 @@ describe("resolvers -> Mutation -> removeUserImage", () => {
     }
   });
 
+  it(`throws NotFoundError if no user exists with _id === context.userId and IN_PRODUCTION === true`, async () => {
+    const { requestContext } = await import("../../../src/lib/libraries");
+    const spy = vi
+      .spyOn(requestContext, "translate")
+      .mockImplementationOnce((message) => `Translated ${message}`);
+
+    try {
+      const context = {
+        userId: Types.ObjectId().toString(),
+      };
+
+      vi.doMock("../../../src/constants", async () => {
+        const actualConstants: object = await vi.importActual(
+          "../../../src/constants"
+        );
+        return {
+          ...actualConstants,
+          IN_PRODUCTION: true,
+        };
+      });
+
+      const { removeUserImage: removeUserImageResolver } = await import(
+        "../../../src/lib/resolvers/Mutation/removeUserImage"
+      );
+
+      await removeUserImageResolver?.({}, {}, context);
+    } catch (error: any) {
+      expect(spy).toBeCalledWith(USER_NOT_FOUND_MESSAGE);
+      expect(error.message).toEqual(`Translated ${USER_NOT_FOUND_MESSAGE}`);
+    }
+  });
+
   it(`throws NotFoundError if no user.image exists for currentUser
-  with _id === context.userId`, async () => {
+  with _id === context.userId and IN_PRODUCTION === false`, async () => {
     try {
       const context = {
         userId: testUser.id,
       };
+
+      vi.doMock("../../../src/constants", async () => {
+        const actualConstants: object = await vi.importActual(
+          "../../../src/constants"
+        );
+        return {
+          ...actualConstants,
+          IN_PRODUCTION: false,
+        };
+      });
+
+      const { removeUserImage: removeUserImageResolver } = await import(
+        "../../../src/lib/resolvers/Mutation/removeUserImage"
+      );
 
       await removeUserImageResolver?.({}, {}, context);
     } catch (error: any) {
@@ -51,35 +124,80 @@ describe("resolvers -> Mutation -> removeUserImage", () => {
     }
   });
 
-  // it(`sets image field to null for organization with _id === args.organizationId
-  // and returns the updated organization`, async () => {
-  //   await User.updateOne(
-  //     {
-  //       _id: testUser._id,
-  //     },
-  //     {
-  //       $set: {
-  //         image: 'image',
-  //       },
-  //     }
-  //   );
+  it(`throws NotFoundError if no user.image exists for currentUser
+  with _id === context.userId and IN_PRODUCTION === true`, async () => {
+    const { requestContext } = await import("../../../src/lib/libraries");
+    const spy = vi
+      .spyOn(requestContext, "translate")
+      .mockImplementationOnce((message) => `Translated ${message}`);
 
-  //   const context = {
-  //     userId: testUser._id,
-  //   };
+    try {
+      const context = {
+        userId: testUser.id,
+      };
 
-  //   const removeUserImagePayload = await removeUserImageResolver?.(
-  //     {},
-  //     {},
-  //     context
-  //   );
+      vi.doMock("../../../src/constants", async () => {
+        const actualConstants: object = await vi.importActual(
+          "../../../src/constants"
+        );
+        return {
+          ...actualConstants,
+          IN_PRODUCTION: true,
+        };
+      });
 
-  //   const updatedTestUser = await User.findOne({
-  //     _id: testUser._id,
-  //   }).lean();
+      const { removeUserImage: removeUserImageResolver } = await import(
+        "../../../src/lib/resolvers/Mutation/removeUserImage"
+      );
 
-  //   expect(removeUserImagePayload).toEqual(updatedTestUser);
+      await removeUserImageResolver?.({}, {}, context);
+    } catch (error: any) {
+      expect(spy).toBeCalledWith("user.profileImage.notFound");
+      expect(error.message).toEqual(`Translated user.profileImage.notFound`);
+    }
+  });
 
-  //   expect(removeUserImagePayload?.image).toEqual(null);
-  // });
+  it(`sets image field to null for organization with _id === args.organizationId
+  and returns the updated user`, async () => {
+    const utilities = await import("../../../src/lib/utilities");
+
+    const deleteImageSpy = vi
+      .spyOn(utilities, "deleteImage")
+      .mockImplementation((_imageToBeDeleted: string) => {
+        return Promise.resolve();
+      });
+
+    await User.updateOne(
+      {
+        _id: testUser._id,
+      },
+      {
+        $set: {
+          image: testImage,
+        },
+      }
+    );
+
+    const context = {
+      userId: testUser._id,
+    };
+
+    const { removeUserImage: removeUserImageResolver } = await import(
+      "../../../src/lib/resolvers/Mutation/removeUserImage"
+    );
+
+    const removeUserImagePayload = await removeUserImageResolver?.(
+      {},
+      {},
+      context
+    );
+
+    const updatedTestUser = await User.findOne({
+      _id: testUser._id,
+    }).lean();
+
+    expect(removeUserImagePayload).toEqual(updatedTestUser);
+    expect(deleteImageSpy).toBeCalledWith(testImage);
+    expect(removeUserImagePayload?.image).toEqual(null);
+  });
 });
