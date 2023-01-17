27d4028fa1409df86b6c94b151c38173973c1bc5
Noble Mittal <62551163+beingnoble03@users.noreply.github.com>
2023-01-18 01:35:35 +0530

    adds test for utilities/adminCheck (#864)

---
+++ __tests__/utilities/adminCheck.spec.ts
@@ -0,0 +1,107 @@
+import "dotenv/config";
+import { Document } from "mongoose";
+import { nanoid } from "nanoid";
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
+import { connect, disconnect } from "../../src/db";
+import {
+  Interface_Organization,
+  Interface_User,
+  Organization,
+  User,
+} from "../../src/lib/models";
+import {
+  USER_NOT_AUTHORIZED,
+  USER_NOT_AUTHORIZED_MESSAGE,
+} from "../../src/constants";
+
+let testUser: Interface_User & Document<any, any, Interface_User>;
+let testOrganization: Interface_Organization &
+  Document<any, any, Interface_Organization>;
+let testUserEmail: string = `email${nanoid().toLowerCase()}@gmail.com`;
+
+beforeAll(async () => {
+  connect();
+
+  testUser = await User.create({
+    email: testUserEmail,
+    password: "testPassword",
+    firstName: "testFirstName",
+    lastName: "testLastName",
+    appLanguageCode: "en",
+  });
+
+  testOrganization = await Organization.create({
+    name: "testName",
+    description: "testDescription",
+    isPublic: true,
+    creator: testUser._id,
+    admins: [],
+    members: [testUser._id],
+  });
+});
+
+afterAll(async () => {
+  disconnect();
+});
+
+describe("utilities -> adminCheck", () => {
+  afterEach(() => {
+    vi.doUnmock("../../src/constants");
+    vi.resetModules();
+  });
+
+  it("throws error if userIsOrganizationAdmin === false and IN_PRODUCTION === false", async () => {
+    vi.doMock("../../src/constants", async () => {
+      const actualConstants: object = await vi.importActual(
+        "../../src/constants"
+      );
+      return {
+        ...actualConstants,
+        IN_PRODUCTION: false,
+      };
+    });
+
+    try {
+      const { adminCheck } = await import("../../src/lib/utilities");
+      adminCheck(testUser._id, testOrganization);
+    } catch (error: any) {
+      expect(error.message).toEqual(USER_NOT_AUTHORIZED);
+    }
+  });
+
+  it("throws error if userIsOrganizationAdmin === false and IN_PRODUCTION === true", async () => {
+    vi.doMock("../../src/constants", async () => {
+      const actualConstants: object = await vi.importActual(
+        "../../src/constants"
+      );
+      return {
+        ...actualConstants,
+        IN_PRODUCTION: true,
+      };
+    });
+
+    const { requestContext } = await import("../../src/lib/libraries");
+
+    const spy = vi
+      .spyOn(requestContext, "translate")
+      .mockImplementationOnce((message) => `Translated ${message}`);
+
+    try {
+      const { adminCheck } = await import("../../src/lib/utilities");
+      adminCheck(testUser._id, testOrganization);
+    } catch (error: any) {
+      expect(error.message).toEqual(
+        `Translated ${USER_NOT_AUTHORIZED_MESSAGE}`
+      );
+      expect(spy).toBeCalledWith(USER_NOT_AUTHORIZED_MESSAGE);
+    }
+  });
+});
