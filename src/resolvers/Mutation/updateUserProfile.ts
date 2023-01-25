90f8580984dcf1474440dd03ba6c34fa91471cdb
Kanishka <kanishka.bansode@gmail.com>
2023-01-25 17:36:32 +0530

    additional linting removal

--- src/resolvers/Mutation/updateUserProfile.ts
+++ src/resolvers/Mutation/updateUserProfile.ts
@@ -44,19 +44,14 @@ export const updateUserProfile: MutationResolvers["updateUserProfile"] = async (
       );
     }
   }
-
   // Upload file
   let uploadImageObj;
   if (args.file) {
     uploadImageObj = await uploadImage(args.file, null);
   }
-
-
   const currentUser = await User.findById({
     _id: context.userId,
   });
-
-
   // Update User
   if (uploadImageObj) {
     return await User.findOneAndUpdate(
@@ -65,7 +60,6 @@ export const updateUserProfile: MutationResolvers["updateUserProfile"] = async (
       },
       {
         $set: {
-
           email: args.data?.email ? args.data.email : currentUser?.email,
           firstName: args.data?.firstName ? args.data.firstName : currentUser?.firstName,
           lastName: args.data?.lastName ? args.data.lastName : currentUser?.lastName,
