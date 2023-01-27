2ef3716156a710f205a476d6cc15f9d256ae9300
Syed Aman Ali <aliamaan119@gmail.com>
2023-01-28 00:26:18 +0530

    fix:tags can be added while creating organization (#875)
    * fix:tags can be added while creating organization
    
    * fix:null value cannot be added in tags
    
    * new:added tags field in test file

--- src/types/generatedGraphQLTypes.ts
+++ src/types/generatedGraphQLTypes.ts
@@ -790,6 +790,7 @@ export type OrganizationInput = {
   isPublic: Scalars['Boolean'];
   location?: InputMaybe<Scalars['String']>;
   name: Scalars['String'];
+  tags: Array<Scalars['String']>;
   visibleInSearch: Scalars['Boolean'];
 };
 
