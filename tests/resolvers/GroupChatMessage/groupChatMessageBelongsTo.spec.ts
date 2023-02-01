86ce34ac1534f14b9f38c88ddc046e57e8855c90
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-02-01 23:24:59 +0530

    Migrate tests for GroupChatMessage

--- tests/resolvers/GroupChatMessage/groupChatMessageBelongsTo.spec.ts
+++ tests/resolvers/GroupChatMessage/groupChatMessageBelongsTo.spec.ts
@@ -1,66 +1,19 @@
 import "dotenv/config";
 import { groupChatMessageBelongsTo as groupChatMessageBelongsToResolver } from "../../../src/resolvers/GroupChatMessage/groupChatMessageBelongsTo";
 import { connect, disconnect } from "../../../src/db";
+import { GroupChat } from "../../../src/models";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import {
-  User,
-  Organization,
-  Interface_GroupChatMessage,
-  GroupChat,
-  GroupChatMessage,
-} from "../../../src/models";
-import { Document } from "mongoose";
-import { nanoid } from "nanoid";
-import { beforeAll, afterAll, describe, it, expect } from "vitest";
+  createTestGroupChatMessage,
+  testGroupChatMessageType,
+} from "../../helpers/groupChat";
 
-let testGroupChatMessage: Interface_GroupChatMessage &
-  Document<any, any, Interface_GroupChatMessage>;
+let testGroupChatMessage: testGroupChatMessageType;
 
 beforeAll(async () => {
   await connect();
-
-  const testUser = await User.create({
-    email: `email${nanoid().toLowerCase()}@gmail.com`,
-    password: "password",
-    firstName: "firstName",
-    lastName: "lastName",
-    appLanguageCode: "en",
-  });
-
-  const testOrganization = await Organization.create({
-    name: "name",
-    description: "description",
-    isPublic: true,
-    creator: testUser._id,
-    admins: [testUser._id],
-    members: [testUser._id],
-  });
-
-  await User.updateOne(
-    {
-      _id: testUser._id,
-    },
-    {
-      $push: {
-        createdOrganizations: testOrganization._id,
-        adminFor: testOrganization._id,
-        joinedOrganizations: testOrganization._id,
-      },
-    }
-  );
-
-  const testGroupChat = await GroupChat.create({
-    creator: testUser._id,
-    users: [testUser._id],
-    organization: testOrganization._id,
-    title: "title",
-  });
-
-  testGroupChatMessage = await GroupChatMessage.create({
-    groupChatMessageBelongsTo: testGroupChat._id,
-    sender: testUser._id,
-    createdAt: new Date(),
-    messageContent: "messageContent",
-  });
+  const resultArray = await createTestGroupChatMessage();
+  testGroupChatMessage = resultArray[3];
 });
 
 afterAll(async () => {
@@ -69,13 +22,13 @@ afterAll(async () => {
 
 describe("resolvers -> GroupChatMessage -> groupChatMessageBelongsTo", () => {
   it(`returns groupChatMessageBelongsTo object for parent.groupChatMessageBelongsTo`, async () => {
-    const parent = testGroupChatMessage.toObject();
+    const parent = testGroupChatMessage!.toObject();
 
     const groupChatMessageBelongsToPayload =
       await groupChatMessageBelongsToResolver?.(parent, {}, {});
 
     const groupChatMessageBelongsTo = await GroupChat.findOne({
-      _id: testGroupChatMessage.groupChatMessageBelongsTo,
+      _id: testGroupChatMessage!.groupChatMessageBelongsTo,
     }).lean();
 
     expect(groupChatMessageBelongsToPayload).toEqual(groupChatMessageBelongsTo);
