df9862a49bfadbb43f1d9813bb77d0d6a5b096a5
Adarsh Tiwari <xoldyckk@gmail.com>
2023-02-02 23:48:28 +0530

    Vitest config update (#983)
    * added index.ts to vite.config.ts to ignore it recursively inside src directory
    
    * removed index.spec.ts files

--- tests/middleware/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import * as exports from "../../src/middleware/index";
-import { isAuth } from "../../src/middleware/isAuth";
-import {describe,it,expect} from "vitest";
-
-describe("index", () => {
-    it("should export isAuth", () => {
-        expect(exports.isAuth).toBe(isAuth);
-    });
-});
