0399c92f12fbdaeb8dc9712586319dfcd634bc40
Aviral Yadav <121368112+aviraldevv@users.noreply.github.com>
2023-02-05 17:30:16 +0530

    Restored creatorCheck.ts

--- src/utilities/creatorCheck.ts
+++ src/utilities/creatorCheck.ts
@@ -1,10 +1,12 @@
-import { Types } from "mongoose";
 import { errors, requestContext } from "../libraries";
 import {
+  USER_NOT_AUTHORIZED,
   USER_NOT_AUTHORIZED_MESSAGE,
   USER_NOT_AUTHORIZED_CODE,
   USER_NOT_AUTHORIZED_PARAM,
+  IN_PRODUCTION,
 } from "../constants";
+import { Types } from "mongoose";
 import { Interface_Organization } from "../models";
 
 export const creatorCheck = (
@@ -15,9 +17,12 @@ export const creatorCheck = (
 
   if (userIsCreator === false) {
     throw new errors.UnauthorizedError(
-      requestContext.translate(USER_NOT_AUTHORIZED_MESSAGE),
+      IN_PRODUCTION !== true
+        ? USER_NOT_AUTHORIZED
+        : requestContext.translate(USER_NOT_AUTHORIZED_MESSAGE),
       USER_NOT_AUTHORIZED_CODE,
       USER_NOT_AUTHORIZED_PARAM
     );
   }
 };
+
