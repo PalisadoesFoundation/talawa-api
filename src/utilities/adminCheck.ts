d6e157c495bf8cd7d0ab61cbfdadda2cd75ff575
Aviral Yadav <121368112+aviraldevv@users.noreply.github.com>
2023-02-05 12:32:25 +0530

    Update adminCheck.ts

--- src/utilities/adminCheck.ts
+++ src/utilities/adminCheck.ts
@@ -1,8 +1,6 @@
 import { Types } from "mongoose";
 import { errors, requestContext } from "../libraries";
 import {
-  IN_PRODUCTION,
-  USER_NOT_AUTHORIZED,
   USER_NOT_AUTHORIZED_MESSAGE,
   USER_NOT_AUTHORIZED_CODE,
   USER_NOT_AUTHORIZED_PARAM,
@@ -19,9 +17,7 @@ export const adminCheck = (
 
   if (userIsOrganizationAdmin === false) {
     throw new errors.UnauthorizedError(
-      IN_PRODUCTION !== true
-        ? USER_NOT_AUTHORIZED
-        : requestContext.translate(USER_NOT_AUTHORIZED_MESSAGE),
+      requestContext.translate(USER_NOT_AUTHORIZED_MESSAGE),
       USER_NOT_AUTHORIZED_CODE,
       USER_NOT_AUTHORIZED_PARAM
     );
