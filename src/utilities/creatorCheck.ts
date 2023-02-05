8ccfb5f6bab762be99619a47ccccfb3faaf71e29
Aviral Yadav <121368112+aviraldevv@users.noreply.github.com>
2023-02-05 16:58:20 +0530

    Refactor creatorCheck.ts

--- src/utilities/creatorCheck.ts
+++ src/utilities/creatorCheck.ts
@@ -1,12 +1,10 @@
+import { Types } from "mongoose";
 import { errors, requestContext } from "../libraries";
 import {
-  USER_NOT_AUTHORIZED,
   USER_NOT_AUTHORIZED_MESSAGE,
   USER_NOT_AUTHORIZED_CODE,
   USER_NOT_AUTHORIZED_PARAM,
-  IN_PRODUCTION,
 } from "../constants";
-import { Types } from "mongoose";
 import { Interface_Organization } from "../models";
 
 export const creatorCheck = (
@@ -17,9 +15,7 @@ export const creatorCheck = (
 
   if (userIsCreator === false) {
     throw new errors.UnauthorizedError(
-      IN_PRODUCTION !== true
-        ? USER_NOT_AUTHORIZED
-        : requestContext.translate(USER_NOT_AUTHORIZED_MESSAGE),
+      requestContext.translate(USER_NOT_AUTHORIZED_MESSAGE),
       USER_NOT_AUTHORIZED_CODE,
       USER_NOT_AUTHORIZED_PARAM
     );
