da16710b292d85c37036233827f8011d8c523f2e
Adarsh Tiwari <xoldyckk@gmail.com>
2023-02-12 03:55:11 +0530

    Added README.md for typeDefs directory and restructured the files into corresponding SDL fields. (#1051)
    * restructured graphql type definitions and added README.md
    
    * updated README.md
    
    * updated README.md
    
    * updated README.md

--- src/typeDefs/queries.ts
+++ src/typeDefs/queries.ts
@@ -1,6 +1,7 @@
 import { gql } from "apollo-server-core";
 
-export const query = gql`
+// Place fields alphabetically to ensure easier lookup and navigation.
+export const queries = gql`
   type Query {
     adminPlugin(orgId: ID!): [Plugin]
 
