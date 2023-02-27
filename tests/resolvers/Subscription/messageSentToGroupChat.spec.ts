358e0def0021e0479ff8dee04dead159f997a056
KrutikaBhatt <krutikabhatt222@gmail.com>
2023-02-27 16:30:55 +0530

    refactoring and testing for messageSentToGroupChat completed

---
+++ tests/resolvers/Subscription/messageSentToGroupChat.spec.ts
@@ -0,0 +1,59 @@
+import "dotenv/config";
+import { describe, it, expect, beforeAll, afterAll } from "vitest";
+import { connect, disconnect } from "../../helpers/db";
+import mongoose from "mongoose";
+import {
+  testGroupChatType,
+  createTestGroupChatMessage,
+} from "../../helpers/groupChat";
+import { filterFunction } from "../../../src/resolvers/Subscription/messageSentToGroupChat";
+
+let MONGOOSE_INSTANCE: typeof mongoose | null;
+let testGroupChat: testGroupChatType;
+
+beforeAll(async () => {
+  MONGOOSE_INSTANCE = await connect();
+  testGroupChat = (await createTestGroupChatMessage())[2];
+});
+afterAll(async () => {
+  await disconnect(MONGOOSE_INSTANCE!);
+});
+
+describe("src -> resolvers -> Subscription -> messageSentToGroupChat", () => {
+  it("subscription filter function returns true", async () => {
+    const { messageSentToGroupChat: messageSentToGroupChatPayload } =
+      await import(
+        "../../../src/resolvers/Subscription/messageSentToGroupChat"
+      );
+
+    console.log("Before :", messageSentToGroupChatPayload);
+    console.log("gROUPChat", testGroupChat);
+    const _args = {};
+    const _parent = {};
+    const context = {
+      pubsub: {
+        asyncInterator: (_action: "MESSAGE_SENT_TO_GROUP_CHAT") => {
+          return;
+        },
+      },
+      context: { currentUserId: testGroupChat!.users[0] },
+    };
+    const payload = {
+      messageSentToGroupChat: {
+        groupChatMessageBelongsTo: testGroupChat!._id,
+      },
+    };
+    // @ts-ignore
+    messageSentToGroupChatPayload._parent = _parent;
+    // @ts-ignore
+    messageSentToGroupChatPayload._args = _args;
+    // @ts-ignore
+    messageSentToGroupChatPayload.context = context;
+    // @ts-ignore
+    messageSentToGroupChatPayload.payload = payload;
+    // @ts-ignore
+    const x = messageSentToGroupChatPayload?.subscribe;
+    expect(x()).not.toBe(null);
+    expect(await filterFunction(payload, context)).toBe(true);
+  });
+});
