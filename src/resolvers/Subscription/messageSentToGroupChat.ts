5e6df04df3dfa2deafa53866eaff993106d07e1a
KrutikaBhatt <65107474+KrutikaBhatt@users.noreply.github.com>
2023-03-05 08:25:00 +0530

    tests for `src/lib/resolvers/Subscription/messageSentToGroupChat.ts` (#1113)
    * tests for messageSentToGroupChat.ts
    
    * fixed lint error

--- src/resolvers/Subscription/messageSentToGroupChat.ts
+++ src/resolvers/Subscription/messageSentToGroupChat.ts
@@ -4,6 +4,23 @@ import { GroupChat } from "../../models";
 
 const MESSAGE_SENT_TO_GROUP_CHAT = "MESSAGE_SENT_TO_GROUP_CHAT";
 
+export const filterFunction = async function (
+  payload: any,
+  context: any
+): Promise<boolean> {
+  const { currentUserId } = context.context;
+  const groupChatId = payload.messageSentToGroupChat.groupChatMessageBelongsTo;
+
+  const groupChat = await GroupChat.findOne({
+    _id: groupChatId,
+  }).lean();
+
+  const currentUserIsGroupChatMember = groupChat!.users.some(
+    (user) => user.toString() === currentUserId.toString()
+  );
+  return currentUserIsGroupChatMember;
+};
+
 export const messageSentToGroupChat: SubscriptionResolvers["messageSentToGroupChat"] =
   {
     // @ts-ignore
@@ -11,21 +28,6 @@ export const messageSentToGroupChat: SubscriptionResolvers["messageSentToGroupCh
       (_parent, _args, context) =>
         context.pubsub.asyncIterator([MESSAGE_SENT_TO_GROUP_CHAT]),
 
-      async (payload, _variables, context) => {
-        const { currentUserId } = context.context;
-
-        const groupChatId =
-          payload.messageSentToGroupChat.groupChatMessageBelongsTo;
-
-        const groupChat = await GroupChat.findOne({
-          _id: groupChatId,
-        }).lean();
-
-        const currentUserIsGroupChatMember = groupChat!.users.some(
-          (user) => user.toString() === currentUserId.toString()
-        );
-
-        return currentUserIsGroupChatMember;
-      }
+      async (payload, _variables, context) => filterFunction(payload, context)
     ),
   };
