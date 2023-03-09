f54c0cbb31e85a789df70a329e3eabc0bbd01e9e
aashimawadhwa <ashimawadhwa1208@gmail.com>
2023-03-09 16:08:28 +0530

    express.urlencoded

--- src/server.ts
+++ src/server.ts
@@ -68,7 +68,7 @@ app.use(
 app.use(mongoSanitize());
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Fix added to stream
 app.use(
