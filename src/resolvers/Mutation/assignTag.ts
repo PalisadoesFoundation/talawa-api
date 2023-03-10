438fd6be99934bf432e96ad88e4b666313a149ba
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-03-10 14:59:36 +0530

    Migrate all error objects to the new style

--- src/resolvers/Mutation/assignTag.ts
+++ src/resolvers/Mutation/assignTag.ts
@@ -2,14 +2,10 @@ import { MutationResolvers } from "../../types/generatedGraphQLTypes";
 import { errors, requestContext } from "../../libraries";
 import { User, Tag, UserTag } from "../../models";
 import {
-  USER_NOT_FOUND_MESSAGE,
-  USER_NOT_FOUND_CODE,
-  USER_NOT_FOUND_PARAM,
-  USER_NOT_AUTHORIZED_MESSAGE,
-  USER_NOT_AUTHORIZED_CODE,
-  USER_NOT_AUTHORIZED_PARAM,
+  USER_NOT_FOUND_ERROR,
+  USER_NOT_AUTHORIZED_ERROR,
+  TAG_NOT_FOUND,
 } from "../../constants";
-import { TAG_NOT_FOUND } from "../../constants";
 
 export const assignTag: MutationResolvers["assignTag"] = async (
   _parent,
@@ -27,9 +23,9 @@ export const assignTag: MutationResolvers["assignTag"] = async (
   // Checks whether currentUser and the requestUser both exist.
   if (!currentUser || !requestUser) {
     throw new errors.NotFoundError(
-      requestContext.translate(USER_NOT_FOUND_MESSAGE),
-      USER_NOT_FOUND_CODE,
-      USER_NOT_FOUND_PARAM
+      requestContext.translate(USER_NOT_FOUND_ERROR.MESSAGE),
+      USER_NOT_FOUND_ERROR.CODE,
+      USER_NOT_FOUND_ERROR.PARAM
     );
   }
 
@@ -40,9 +36,9 @@ export const assignTag: MutationResolvers["assignTag"] = async (
 
   if (!tag) {
     throw new errors.NotFoundError(
-      TAG_NOT_FOUND.message,
-      TAG_NOT_FOUND.code,
-      TAG_NOT_FOUND.param
+      TAG_NOT_FOUND.MESSAGE,
+      TAG_NOT_FOUND.CODE,
+      TAG_NOT_FOUND.PARAM
     );
   }
   // Check that the user shouldn't already be assigned the tag
@@ -64,9 +60,9 @@ export const assignTag: MutationResolvers["assignTag"] = async (
     !(currentUser.userType === "SUPERADMIN")
   ) {
     throw new errors.UnauthorizedError(
-      requestContext.translate(USER_NOT_AUTHORIZED_MESSAGE),
-      USER_NOT_AUTHORIZED_CODE,
-      USER_NOT_AUTHORIZED_PARAM
+      requestContext.translate(USER_NOT_AUTHORIZED_ERROR.MESSAGE),
+      USER_NOT_AUTHORIZED_ERROR.CODE,
+      USER_NOT_AUTHORIZED_ERROR.PARAM
     );
   }
 
