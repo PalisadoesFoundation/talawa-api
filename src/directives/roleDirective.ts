6ca586e301eaede4546daa2f138e02ef38f49745
Eva <20bcs043@nith.ac.in>
2023-03-10 11:36:50 +0530

    fixing eslint errors

--- src/directives/roleDirective.ts
+++ src/directives/roleDirective.ts
@@ -17,7 +17,7 @@ import { errors, requestContext } from "../libraries";
 import { User } from "../models";
 
 export class RoleAuthorizationDirective extends SchemaDirectiveVisitor {
-    /**
+  /**
    * This function authenticates the role of the user and if not validated, then throws an Unauthenticated Error.
    * @param field - GraphQLField
    * @param  _details - Object
