50f636743fc3c2a4a3cdbc897d92c53a463e23dd
Eva <20bcs043@nith.ac.in>
2023-03-11 09:28:05 +0530

    [doc:models] TSDoc Comments Migration

--- src/models/GroupChatMessage.ts
+++ src/models/GroupChatMessage.ts
@@ -1,7 +1,9 @@
 import { Schema, Types, model, PopulatedDoc, Document, models } from "mongoose";
 import { Interface_GroupChat } from "./GroupChat";
 import { Interface_User } from "./User";
-
+/**
+ * This is an interface that represents a database(MongoDB) document for Group Chat Message.
+ */
 export interface Interface_GroupChatMessage {
   _id: Types.ObjectId;
   groupChatMessageBelongsTo: PopulatedDoc<Interface_GroupChat & Document>;
@@ -10,7 +12,14 @@ export interface Interface_GroupChatMessage {
   messageContent: string;
   status: string;
 }
-
+/**
+ * This represents the schema for a `GroupChatMessage`.
+ * @param groupChatMessageBelongsTo - This is the association referring to the `GroupChat` model.
+ * @param sender - Sender of the message.
+ * @param createdAt - Time stamp of data creation.
+ * @param messageContent - Content of the message.
+ * @param status - Status.
+ */
 const groupChatMessageSchema = new Schema({
   groupChatMessageBelongsTo: {
     type: Schema.Types.ObjectId,
