6d673c6c25dc9c4994606d3e390060b184f77a98
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-03-18 14:49:06 +0530

    Add conditional

--- src/resolvers/UserTag/parentTag.ts
+++ src/resolvers/UserTag/parentTag.ts
@@ -2,6 +2,10 @@ import { UserTagResolvers } from "../../types/generatedGraphQLTypes";
 import { OrganizationTagUser } from "../../models";
 
 export const parentTag: UserTagResolvers["parentTag"] = async (parent) => {
+  // Check if the parentTag is null
+  if (parent.parentTagId === null) return null;
+
+  // If the parentTag is not null, make a database request to fetch the same
   return await OrganizationTagUser.findOne({
     _id: parent!.parentTagId,
   }).lean();
