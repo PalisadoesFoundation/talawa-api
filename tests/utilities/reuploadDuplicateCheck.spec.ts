56c7918c4c83ecffc23dec900e419f39711e9aa7
Ujwal Kumar <ujwalkumar95@gmail.com>
2023-03-21 19:13:27 +0530

    Fix typescript-eslint/no-inferrable-types warning in reuploadDuplicateCheck.spec.ts

--- tests/utilities/reuploadDuplicateCheck.spec.ts
+++ tests/utilities/reuploadDuplicateCheck.spec.ts
@@ -16,7 +16,7 @@ import { Type_ImagePath } from "../../src/utilities/reuploadDuplicateCheck";
 
 const testNewImagePath: Type_ImagePath = `${nanoid()}-testNewImagePath`;
 const testOldImagePath: Type_ImagePath = `${nanoid()}-testOldImagePath`;
-const testNewImageHash: string = `${nanoid()}-testHash`;
+const testNewImageHash = `${nanoid()}-testHash`;
 
 const testErrors = [
   {
