11296432584f8d961b64ac26bfd7d2b9167eb44a
Ujwal Kumar <ujwalkumar95@gmail.com>
2023-03-21 19:30:56 +0530

    Fix typescript-eslint/no-inferrable-types warning in vaidationError.ts

--- src/libraries/errors/validationError.ts
+++ src/libraries/errors/validationError.ts
@@ -5,7 +5,7 @@ import { ApplicationError, Interface_Error } from "./applicationError";
 export class ValidationError extends ApplicationError {
   constructor(
     errors: Interface_Error[] = [],
-    message: string = "Validation error"
+    message = "Validation error"
   ) {
     super(errors, 422, message);
   }
