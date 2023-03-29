aa79b84b9d28f5e91ebc81dbe137e2d215bd2ddf
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2023-03-29 06:54:44 +0530

    Fixes 1559 of Talawa-Mobile, The user detail fetch populated by membershipRequest

--- src/resolvers/User/index.ts
+++ src/resolvers/User/index.ts
@@ -1,6 +1,8 @@
 import { UserResolvers } from "../../types/generatedGraphQLTypes";
+import { membershipRequests } from "./membershipRequests";
 // import { tagsAssignedWith } from "./tagsAssignedWith";
 
 export const User: UserResolvers = {
   // tagsAssignedWith,
+  membershipRequests
 };
---
+++ src/resolvers/User/membershipRequests.ts
@@ -0,0 +1,15 @@
+import { MembershipRequest } from "../../models";
+import { UserResolvers } from "../../types/generatedGraphQLTypes";
+/**
+ * This resolver function will fetch and return the list of Membership requests for the Organization from database.
+ * @param parent - An object that is the return value of the resolver for this field's parent.
+ * @returns An object that contains the list of membership requests for the organization.
+ */
+export const membershipRequests: UserResolvers["membershipRequests"] =
+  async (parent) => {
+    return await MembershipRequest.find({
+      _id: {
+        $in: parent.membershipRequests,
+      },
+    }).lean();
+  };
