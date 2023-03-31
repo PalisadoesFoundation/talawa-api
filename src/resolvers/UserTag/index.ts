cfe5e658f565fda935ddf16a804da83666052e22
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-03-31 11:03:53 +0530

    Register child tags in the index

--- src/resolvers/UserTag/index.ts
+++ src/resolvers/UserTag/index.ts
@@ -1,10 +1,11 @@
 import { UserTagResolvers } from "../../types/generatedGraphQLTypes";
-
+import { childTags } from "./childTags";
 import { organization } from "./organization";
 import { parentTag } from "./parentTag";
 import { usersAssignedTo } from "./usersAssignedTo";
 
 export const UserTag: UserTagResolvers = {
+  childTags,
   organization,
   parentTag,
   usersAssignedTo,
