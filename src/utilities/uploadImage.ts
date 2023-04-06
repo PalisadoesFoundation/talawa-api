b158e971e444e79cd9cfebe4423a1b64e94c9dcb
Nishant Singh <srnikhil3721@gmail.com>
2023-04-07 02:29:45 +0530

    error handling during upload file process

--- src/utilities/uploadImage.ts
+++ src/utilities/uploadImage.ts
@@ -5,6 +5,12 @@ import { logger } from "../libraries";
 import { imageAlreadyInDbCheck } from "./imageAlreadyInDbCheck";
 import { deleteImage } from "./deleteImage";
 import { imageExtensionCheck } from "./imageExtensionCheck";
+
+interface Interface_NewImage {
+  createReadStream: () => any;
+  filename: string;
+}
+
 /**
  * This function uploads the new image and deletes the previously uploaded image if exists.
  * @remarks
@@ -14,51 +20,58 @@ import { imageExtensionCheck } from "./imageExtensionCheck";
  * @returns Path of an uploaded image.
  */
 export const uploadImage = async (
-  newImageFile: any,
+  newImageFile: Interface_NewImage | null,
   oldImagePath: string | null
 ) => {
+  if (!newImageFile) {
+    throw new Error("newImageFile is empty or null");
+  }
   const id = shortid.generate();
 
   const { createReadStream, filename } = await newImageFile;
 
   // throw an error if file is not png or jpg
   await imageExtensionCheck(filename);
+  try {
+    const writeStream = createWriteStream(
+      path.join(__dirname, "../../images", `/${id}-${filename}`)
+    );
 
-  // upload new image
-  await new Promise((resolve, reject) =>
-    createReadStream()
-      .pipe(
-        createWriteStream(
-          path.join(__dirname, "../../images", `/${id}-${filename}`)
-        )
-      )
-      .on("close", resolve)
-      .on("error", (error: any) => reject(error))
-      .on("finish", () =>
-        resolve({
-          path,
-        })
-      )
-  );
+    // handle errors during the upload process
+    writeStream.on("error", (error: Error) => {
+      throw new Error(`Error uploading files: ${error.message}`);
+    });
 
-  const newImagePath = `images/${id}-${filename}`;
+    // upload new image
+    await new Promise((resolve, reject) => {
+      createReadStream()
+        .pipe(writeStream)
+        .on("close", resolve)
+        .on("finish", resolve)
+        .on("error", (error: Error) => {
+          reject(`Error uploading file: ${error.message}`);
+        });
+    });
+    const newImagePath = `images/${id}-${filename}`;
 
-  if (oldImagePath !== null) {
-    console.log("oldImagePath is not null");
+    if (oldImagePath !== null) {
+      console.log("oldImagePath is not null");
+      logger.info("Deleting old image");
 
-    logger.info("old image should be deleted");
+      // If user/organization already has an image delete it from the API
+      await deleteImage(oldImagePath, newImagePath);
+    }
 
-    // If user/organization already has an image delete it from the API
-    await deleteImage(oldImagePath, newImagePath);
+    const imageAlreadyInDbPath = await imageAlreadyInDbCheck(
+      oldImagePath,
+      newImagePath
+    );
+
+    return {
+      newImagePath,
+      imageAlreadyInDbPath,
+    };
+  } catch (error) {
+    logger.error("Error uploading file ", error);
   }
-
-  const imageAlreadyInDbPath = await imageAlreadyInDbCheck(
-    oldImagePath,
-    newImagePath
-  );
-
-  return {
-    newImagePath,
-    imageAlreadyInDbPath,
-  };
 };
