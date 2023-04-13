69666ae100400fa67975147f2b242fd12a54631c
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-04-13 21:49:45 +0530

    Added comments

--- src/resolvers/Query/helperFunctions/getWhere.ts
+++ src/resolvers/Query/helperFunctions/getWhere.ts
@@ -636,6 +636,7 @@ export const getWhere = <T = unknown>(
     };
   }
 
+  // Return users who are member of provider organizationId
   if (where.member_of) {
     wherePayload = {
       ...wherePayload,
