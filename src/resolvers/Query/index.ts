9f5e67e7dd5985f2aba95ce43b23f8ac12bec7e7
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-04-13 12:51:20 +0530

    Remove commentsByPost

--- src/resolvers/Query/index.ts
+++ src/resolvers/Query/index.ts
@@ -1,7 +1,6 @@
 import { QueryResolvers } from "../../types/generatedGraphQLTypes";
 import { checkAuth } from "./checkAuth";
 import { comments } from "./comments";
-import { commentsByPost } from "./commentsByPost";
 import { directChatMessages } from "./directChatMessages";
 import { directChats } from "./directChats";
 import { directChatsByUserID } from "./directChatsByUserID";
@@ -41,7 +40,6 @@ import { usersConnection } from "./usersConnection";
 export const Query: QueryResolvers = {
   checkAuth,
   comments,
-  commentsByPost,
   directChatMessages,
   directChats,
   directChatsByUserID,
