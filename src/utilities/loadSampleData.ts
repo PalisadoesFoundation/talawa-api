03d37154ae018a5569c15ac8d04f868fc1c1d025
Ansh Goyal <anshgoyal1704@gmail.com>
2023-04-14 10:10:54 +0530

    add db loader
    Signed-off-by: Ansh Goyal <anshgoyal1704@gmail.com>

---
+++ src/utilities/loadSampleData.ts
@@ -0,0 +1,94 @@
+import "dotenv/config";
+import yargs from "yargs";
+import { User, Organization, Event, Post } from "../models";
+import { connect } from "../db";
+import fs from "fs";
+import path from "path";
+
+// Required arguments that need to be parsed
+yargs.option("i", {
+  alias: "items",
+  describe: "Comma-separated list of collections to load sample data into",
+  type: "string",
+});
+
+yargs.option("f", {
+  alias: "format",
+  describe: "Whether to format the database before insertion",
+  type: "boolean",
+});
+
+// Parse arguments in synchronization 
+const argv: {
+  items?: string;
+  format?: boolean;
+  _: unknown;
+} = yargs.parseSync();
+
+// Default collections available to insert
+let collections = ["users", "organizations", "posts", "events"];
+const argvItems = argv.items;
+
+// Check if specific connections need to be inserted
+if(argvItems){
+  collections = argvItems.split(",");
+}
+
+// Format database function
+async function formatDatabase() {
+  await Promise.all([
+    User.deleteMany({}),
+    Organization.deleteMany({}),
+    Event.deleteMany({}),
+    Post.deleteMany({}),
+  ]);
+  console.log("Cleared all collections\n");
+  //process.exit(1);
+}
+
+// Insert collections function
+async function insertCollections(collections: Array<string>) {
+
+  // Connect to MongoDB database
+  await connect();
+
+  // Check if database format is required
+  if(argv.format){
+    await formatDatabase();
+  }
+
+  // Iterate over arguments and add to database
+  for (const collection of collections) {
+    const data = fs.readFileSync(path.join(__dirname, "sample_data", `${collection}.json`));
+    const docs = JSON.parse(data.toString());
+
+    try {
+      switch (collection) {
+        case 'users':
+          await User.insertMany(docs);
+          break;
+        case 'organizations':
+          await Organization.insertMany(docs);
+          break;
+        case 'events':
+          await Event.insertMany(docs);
+          break;
+        case 'posts':
+          await Post.insertMany(docs);
+          break;
+        default:
+          console.log("\x1b[31m", `Invalid collection name: ${collection}`);
+          break;
+      }
+      console.log("\x1b[35m", `Added ${collection} collection`);
+    } catch (err) {
+      console.error("\x1b[31m", `Error adding ${collection} collection: ${err}`);
+    }
+  }
+
+  console.log("\nCollections added successfully");
+  process.exit(1);
+}
+
+// Calling the insert collections function
+insertCollections(collections);
