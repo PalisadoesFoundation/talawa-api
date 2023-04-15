76df0bc0042bd703d7f13a6a7025f1fdc0d14ec9
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-04-15 18:41:14 +0530

    Remove all the mentioned queries (#1304)

--- src/resolvers/Query/groupChats.ts
+++
@@ -1,9 +0,0 @@
-import { QueryResolvers } from "../../types/generatedGraphQLTypes";
-import { GroupChat } from "../../models";
-/**
- * This query fetch all the group chat from the database.
- * @returns An object containing a list of all group chat.
- */
-export const groupChats: QueryResolvers["groupChats"] = async () => {
-  return await GroupChat.find().lean();
-};
