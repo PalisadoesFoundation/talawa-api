30156f07d6cdd0ef77c3c3771833bc5b15729316
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-06-04 13:55:50 +0530

    Add field level resolver for organization on event model

--- src/resolvers/Event/index.ts
+++ src/resolvers/Event/index.ts
@@ -1,8 +1,10 @@
 import type { EventResolvers } from "../../types/generatedGraphQLTypes";
 import { attendees } from "./attendees";
+import { organization } from "./organization";
 import { projects } from "./projects";
 
 export const Event: EventResolvers = {
   attendees,
+  organization,
   projects,
 };
