50266aea58f77cf5d7c848cc8007d505151f96ae
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-06-12 15:32:37 +0530

    Add tests for userTag resolvers

---
+++ tests/resolvers/UserTag/childTags.spec.ts
@@ -0,0 +1,72 @@
+import "dotenv/config";
+import { childTags as childTagsResolver } from "../../../src/resolvers/UserTag/childTags";
+import type {
+  UserTagsConnectionResult,
+  UserTagChildTagsArgs,
+} from "../../../src/types/generatedGraphQLTypes";
+import { connect, disconnect } from "../../helpers/db";
+import type mongoose from "mongoose";
+import { beforeAll, afterAll, describe, it, expect } from "vitest";
+import type { TestUserTagType } from "../../helpers/tags";
+import { createTwoLevelTagsWithOrg } from "../../helpers/tags";
+import { MAXIMUM_FETCH_LIMIT } from "../../../src/constants";
+
+let MONGOOSE_INSTANCE: typeof mongoose;
+let testChildTag1: TestUserTagType,
+  testChildTag2: TestUserTagType,
+  testParentTag: TestUserTagType;
+
+beforeAll(async () => {
+  MONGOOSE_INSTANCE = await connect();
+  [, , [testParentTag, testChildTag1, testChildTag2]] =
+    await createTwoLevelTagsWithOrg();
+});
+
+afterAll(async () => {
+  await disconnect(MONGOOSE_INSTANCE);
+});
+
+describe("resolvers -> Tag -> childTags", () => {
+  it(`returns error object when the arguments are incorrect`, async () => {
+    const parent = testParentTag!;
+
+    const args: UserTagChildTagsArgs = {
+      input: {
+        limit: MAXIMUM_FETCH_LIMIT + 1,
+        direction: "FORWARD",
+      },
+    };
+
+    const payload = await childTagsResolver?.(parent, args, {});
+
+    expect(payload!.errors.length).toEqual(1);
+    expect(payload!.data).toBeNull();
+  });
+
+  it(`returns correct connection object when the arguments are correct`, async () => {
+    const parent = testParentTag!;
+
+    const args: UserTagChildTagsArgs = {
+      input: {
+        limit: MAXIMUM_FETCH_LIMIT,
+        direction: "FORWARD",
+      },
+    };
+
+    const payload = (await childTagsResolver?.(
+      parent,
+      args,
+      {}
+    )) as UserTagsConnectionResult;
+
+    expect(payload.errors.length).toEqual(0);
+    expect(payload.data).not.toBeNull();
+    expect(payload.data!.pageInfo.startCursor).toEqual(
+      testChildTag1!._id.toString()
+    );
+    expect(payload.data!.pageInfo.endCursor).toEqual(
+      testChildTag2!._id.toString()
+    );
+    expect(payload.data!.edges[0].node).toEqual(testChildTag1);
+  });
+});
---
+++ tests/resolvers/UserTag/usersAssignedTo.spec.ts
@@ -0,0 +1,76 @@
+import "dotenv/config";
+import { usersAssignedTo as usersAssignedToResolver } from "../../../src/resolvers/UserTag/usersAssignedTo";
+import type {
+  UsersConnectionResult,
+  UserTagUsersAssignedToArgs,
+} from "../../../src/types/generatedGraphQLTypes";
+import { connect, disconnect } from "../../helpers/db";
+import type mongoose from "mongoose";
+import { beforeAll, afterAll, describe, it, expect } from "vitest";
+import type { TestUserTagType } from "../../helpers/tags";
+import type { TestUserType } from "../../helpers/userAndOrg";
+import { createTagsAndAssignToUser } from "../../helpers/tags";
+import { MAXIMUM_FETCH_LIMIT } from "../../../src/constants";
+import { TagUser } from "../../../src/models";
+
+let MONGOOSE_INSTANCE: typeof mongoose;
+let testTag: TestUserTagType, testUser: TestUserType;
+
+beforeAll(async () => {
+  MONGOOSE_INSTANCE = await connect();
+  [testUser, , [testTag]] = await createTagsAndAssignToUser();
+});
+
+afterAll(async () => {
+  await disconnect(MONGOOSE_INSTANCE);
+});
+
+describe("resolvers -> Tag -> usersAssignedTo", () => {
+  it(`returns error object when the arguments are incorrect`, async () => {
+    const parent = testTag!;
+
+    const args: UserTagUsersAssignedToArgs = {
+      input: {
+        limit: MAXIMUM_FETCH_LIMIT + 1,
+        direction: "FORWARD",
+      },
+    };
+
+    const payload = await usersAssignedToResolver?.(parent, args, {});
+
+    expect(payload!.errors.length).toEqual(1);
+    expect(payload!.data).toBeNull();
+  });
+
+  it(`returns correct connection object when the arguments are correct`, async () => {
+    const parent = testTag!;
+
+    const args: UserTagUsersAssignedToArgs = {
+      input: {
+        limit: MAXIMUM_FETCH_LIMIT,
+        direction: "FORWARD",
+      },
+    };
+
+    const payload = (await usersAssignedToResolver?.(
+      parent,
+      args,
+      {}
+    )) as UsersConnectionResult;
+
+    expect(payload.errors.length).toEqual(0);
+    expect(payload.data).not.toBeNull();
+
+    const userTagObject = await TagUser.findOne({
+      tagId: testTag!._id,
+    });
+
+    expect(payload.data!.pageInfo.startCursor).toEqual(
+      userTagObject!._id.toString()
+    );
+    expect(payload.data!.pageInfo.endCursor).toEqual(
+      userTagObject!._id.toString()
+    );
+    expect(payload.data!.edges[0].node._id).toEqual(testUser!._id);
+  });
+});
