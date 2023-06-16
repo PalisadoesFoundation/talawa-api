72e8b1bc5aca553efa09778ff045b9682e50219e
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-06-16 21:05:16 +0530

    Add subscription server and apollo-server-express integration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import { ApolloServer } from "apollo-server";
 import {
   ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageDisabled,
+  ApolloServerPluginDrainHttpServer,
 } from "apollo-server-core";
-import "dotenv/config"; // pull env variables from .env file
+import { ApolloServer } from "apollo-server-express";
+
+import "dotenv/config"; // Pull all the environment variables from .env file
 import { typeDefs } from "./typeDefs";
 import { resolvers } from "./resolvers";
 import {
@@ -10,58 +14,71 @@ import {
   RoleAuthorizationDirective,
 } from "./directives";
 import { isAuth } from "./middleware";
-import { PubSub } from "apollo-server-express";
 import * as database from "./db";
-import { makeExecutableSchema } from "@graphql-tools/schema";
 import { MapperKind, getDirective, mapSchema } from "@graphql-tools/utils";
-import { GraphQLField, GraphQLFieldConfig } from "graphql";
+import type { GraphQLField, GraphQLFieldConfig } from "graphql";
 import { errors, requestContext } from "./libraries";
+import express from "express";
+import http from "http";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { execute, subscribe } from "graphql";
+import { SubscriptionServer } from "subscriptions-transport-ws";
 
-const pubsub = new PubSub();
+// const pubsub = new PubSub();
 
 //@ts-ignore
-function directiveTransformer(schema , directiveName) {
-  return mapSchema(schema, {
+// function directiveTransformer(schema, directiveName) {
+//   return mapSchema(schema, {
+//     [MapperKind.OBJECT_FIELD]: (
+//       fieldConfig: GraphQLFieldConfig<any, any>
+//     ): any => {
+//       // Check whether this field has the specified directive
+//       const authDirective = getDirective(
+//         schema,
+//         fieldConfig,
+//         directiveName
+//       )?.[0];
+//       if (authDirective) {
+//         //@ts-ignore
+//         const { resolve = defaultFieldResolver } = fieldConfig;
 
-    [MapperKind.OBJECT_FIELD]:(fieldConfig:GraphQLFieldConfig<any, any>):any => {
+//         fieldConfig.resolve = (root, args, context, info): string => {
+//           if (context.expired || !context.isAuth)
+//             throw new errors.UnauthenticatedError(
+//               requestContext.translate("user.notAuthenticated"),
+//               "user.notAuthenticated",
+//               "userAuthentication"
+//             );
+//           return resolve(root, args, context, info);
+//         };
+//         return fieldConfig;
+//       }
+//     },
+//   });
+// }
 
-      // Check whether this field has the specified directive
-      const authDirective = getDirective(schema, fieldConfig, directiveName)?.[0];
-      if (authDirective) {
-
-        //@ts-ignore
-        const { resolve = defaultFieldResolver } = fieldConfig;
-
-        fieldConfig.resolve = (root, args, context, info): string => {
-          if (context.expired || !context.isAuth)
-            throw new errors.UnauthenticatedError(
-              requestContext.translate("user.notAuthenticated"),
-              "user.notAuthenticated",
-              "userAuthentication"
-            );
-          return resolve(root, args, context, info);
-        };
-        return fieldConfig;
-      }
-    }
-
-  })
-}
-
-let schema = makeExecutableSchema({
+const schema = makeExecutableSchema({
   typeDefs,
-  resolvers
-});
-
-schema = directiveTransformer(schema  , 'auth');
-
-const server = new ApolloServer({
-  schema,
-
+  resolvers,
   // schemaDirectives: {
   //   auth: AuthenticationDirective,
   //   role: RoleAuthorizationDirective,
   // },
+});
+// schema = directiveTransformer(schema, "auth");
+
+const app = express();
+
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer, enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
+
+// Intializing a variable to store the subscription server
+// TODO: Figure out typescript and ESLint (Prefer const) error
+let subscriptionServer: SubscriptionServer | undefined;
+
+const server = new ApolloServer({
+  schema,
   context: ({
     req,
     res,
@@ -75,7 +92,7 @@ const server = new ApolloServer({
     if (connection) {
       return {
         ...connection,
-        pubsub,
+        // pubsub,
         res,
         req,
         apiRootUrl,
@@ -83,7 +100,7 @@ const server = new ApolloServer({
     } else {
       return {
         ...isAuth(req),
-        pubsub,
+        // pubsub,
         res,
         req,
         apiRootUrl,
@@ -92,15 +109,55 @@ const server = new ApolloServer({
   },
   csrfPrevention: true,
   cache: "bounded",
-  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    process.env.NODE_ENV === "production"
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageGraphQLPlayground(),
+    {
+      // TODO: Fix typescript error
+      async serverWillStart() {
+        return {
+          async drainServer(): Promise<void> {
+            subscriptionServer!.close();
+          },
+        };
+      },
+    },
+  ],
 });
 
-async function startServer():Promise<void> {
+subscriptionServer = SubscriptionServer.create(
+  {
+    schema,
+    execute,
+    subscribe,
+    // Return the context object for the subscription server
+    onConnect() {
+      // lookup userId by token, etc.
+      // return { userId };
+    },
+  },
+  {
+    server: httpServer,
+    path: server.graphqlPath,
+  }
+);
+
+async function startServer(): Promise<void> {
   await database.connect();
 
-  server.listen().then(({ url }: { url: string }) => {
-    console.log(`🚀  Server ready at ${url}`);
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: "/",
   });
+
+  // Modified server startup
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
 startServer();
