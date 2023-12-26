1949629c58933e6fc1cca5acc52d97373cbfe575
vasujain275 <vasujain275@gmail.com>
2023-12-26 23:26:54 +0530

    Added MongoDB Reset Script

---
+++ scripts/reset_mongo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the name of your MongoDB container
+MONGO_CONTAINER_NAME="talawa-api-mongodb-1"
+
+# Set the name of your MongoDB database
+MONGO_DATABASE_NAME="talawa-api"
+
+# Use a single docker exec command to drop the database
+docker exec -it "$MONGO_CONTAINER_NAME" mongosh --eval "db.getSiblingDB('$MONGO_DATABASE_NAME').dropDatabase()"
