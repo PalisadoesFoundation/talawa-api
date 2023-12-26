40626312a0b969670e0e0d2c43e610ac6cbe6e90
vasujain275 <vasujain275@gmail.com>
2023-12-27 01:06:43 +0530

    refactor: import sample data after dropping the database in reset_mongo.sh

--- scripts/reset_mongo.sh
+++ scripts/reset_mongo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Set the name of your MongoDB container
 MONGO_CONTAINER_NAME="talawa-api-mongodb-1"
 
-# Set the name of your MongoDB database
 MONGO_DATABASE_NAME="talawa-api"
 
 # Uses docker exec command to drop the database
 docker exec -it "$MONGO_CONTAINER_NAME" mongosh --eval "db.getSiblingDB('$MONGO_DATABASE_NAME').dropDatabase()"
+
+npm run import:sample-data
 
 chmod +x "$0"
