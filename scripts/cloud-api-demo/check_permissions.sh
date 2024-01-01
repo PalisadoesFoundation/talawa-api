1acc455ec07415a00de0b9ad1d07689158dedf5d
vasujain275 <vasujain275@gmail.com>
2024-01-01 16:38:20 +0530

    refactor: Added detailed comments in scripts

--- scripts/cloud-api-demo/check_permissions.sh
+++ scripts/cloud-api-demo/check_permissions.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Purpose: Check and correct permissions for script files
+# This script will be used in a cron job to ensure proper permissions for various scripts on the cloud instance.
+# This ensures that all scripts have correct permissions and ownership for the talawa-api user on the cloud instance.
+
 # Check and correct permissions for cert_renew.sh
 chown talawa-api /usr/local/bin/cert_renew.sh
 chmod 700 /usr/local/bin/cert_renew.sh
--- scripts/cloud-api-demo/reset_mongo.sh
+++ scripts/cloud-api-demo/reset_mongo.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# Purpose: Required for a cron job to reset the database on a cloud instance every 24 hours
+# This script uses docker exec command to drop the specified MongoDB database,
+# then runs a command to import sample data using npm.
+
+# Specify the MongoDB container name
 MONGO_CONTAINER_NAME="talawa-api-mongodb-1"
 
+# Specify the MongoDB database name
 MONGO_DATABASE_NAME="talawa-api"
 
-# Uses docker exec command to drop the database
+# Use docker exec command to drop the specified MongoDB database
 docker exec -it "$MONGO_CONTAINER_NAME" mongosh --eval "db.getSiblingDB('$MONGO_DATABASE_NAME').dropDatabase()"
 
+# Run a command to import sample data using npm
 npm run import:sample-data
 
 # Set permissions to 700 for the script
