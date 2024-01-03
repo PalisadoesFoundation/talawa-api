9501efc33469642c23029a3c0feabba743a2abcb
Akhilender Bongirwar <112749383+akhilender-bongirwar@users.noreply.github.com>
2024-01-03 22:27:05 +0530

    fix: Removed depricated xss-clean package (#1536)
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
 import express from "express";
 import { rateLimit } from "express-rate-limit";
-// No type defintions available for package 'xss-clean'
-// @ts-ignore
-import xss from "xss-clean";
 import helmet from "helmet";
 import mongoSanitize from "express-mongo-sanitize";
 import cors from "cors";
