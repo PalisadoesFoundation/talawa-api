17d7675a6a6cc746a05540f90bc9a3f469066a13
Ankit Varshney <lyfflame45@gmail.com>
2024-01-26 14:36:46 +0530

    fixed failing test

--- src/utilities/validateConnectionArgs.ts
+++ src/utilities/validateConnectionArgs.ts
@@ -3,11 +3,11 @@ import type { UserPostsArgs } from "../types/generatedGraphQLTypes";
 export const validateConnectionArgs = (args: UserPostsArgs): void => {
   const { first, last, after, before } = args;
 
-  if (!((first && !last) || (!first && last))) {
+  if (!((first && !last) || (!first && last) || (!first && !last))) {
     throw new Error("Provide either 'first' or 'last', not both");
   }
 
-  if (!((after && !before) || (!after && before))) {
+  if (!((after && !before) || (!after && before) || (!after && !before))) {
     throw new Error("Provide either 'after' or 'before', not both");
   }
 
