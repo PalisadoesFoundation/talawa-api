540025b0cbd26709f291ea9741d17c1074a523c9
Ankit Varshney <lyfflame45@gmail.com>
2024-01-26 13:52:26 +0530

    implemented validation for connection argument

--- src/resolvers/User/post.ts
+++ src/resolvers/User/post.ts
@@ -1,44 +1,11 @@
 import { Post } from "../../models";
-import type {
-  UserPostsArgs,
-  UserResolvers,
-} from "../../types/generatedGraphQLTypes";
+import type { UserResolvers } from "../../types/generatedGraphQLTypes";
+import { validateConnectionArgs } from "../../utilities/validateConnectionArgs";
 /**
  * This resolver function will fetch and return the post created by the user from database.
  * @param parent - An object that is the return value of the resolver for this field's parent.
  * @returns An array which conatins the post created by the user.
  */
-const validateConnectionArgs = (args: UserPostsArgs): void => {
-  const { first, last, after, before } = args;
-
-  if (
-    !(
-      (first !== undefined && last === undefined) ||
-      (first === undefined && last !== undefined)
-    )
-  ) {
-    throw new Error("Provide either 'first' or 'last', not both");
-  }
-
-  if (
-    !(
-      (after !== undefined && before === undefined) ||
-      (after === undefined && before !== undefined)
-    )
-  ) {
-    throw new Error("Provide either 'after' or 'before', not both");
-  }
-
-  if (
-    (first !== undefined && after === undefined) ||
-    (last !== undefined && before === undefined)
-  ) {
-    throw new Error(
-      "When using 'first', provide 'after'; when using 'last', provide 'before'"
-    );
-  }
-};
-
 export const posts: UserResolvers["posts"] = async (parent, args) => {
   validateConnectionArgs(args);
   const pageSize: number = args.first || args.last || 10;
---
+++ src/utilities/validateConnectionArgs.ts
@@ -0,0 +1,19 @@
+import type { UserPostsArgs } from "../types/generatedGraphQLTypes";
+
+export const validateConnectionArgs = (args: UserPostsArgs): void => {
+  const { first, last, after, before } = args;
+
+  if (!((first && !last) || (!first && last))) {
+    throw new Error("Provide either 'first' or 'last', not both");
+  }
+
+  if (!((after && !before) || (!after && before))) {
+    throw new Error("Provide either 'after' or 'before', not both");
+  }
+
+  if ((first && !after) || (last && !before)) {
+    throw new Error(
+      "When using 'first', provide 'after'; when using 'last', provide 'before'"
+    );
+  }
+};
