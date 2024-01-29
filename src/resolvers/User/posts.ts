244a5cc32f07e5f493366412e179fcea9372ea0c
Ankit Varshney <lyfflame45@gmail.com>
2024-01-29 19:32:31 +0530

    fix code logic

--- src/resolvers/User/posts.ts
+++ src/resolvers/User/posts.ts
@@ -1,15 +1,32 @@
 import { Post } from "../../models";
-import type { UserResolvers } from "../../types/generatedGraphQLTypes";
+import type {
+  ConnectionPageInfo,
+  UserResolvers,
+} from "../../types/generatedGraphQLTypes";
 import { parseRelayConnectionArguments } from "../../utilities/validateConnectionArgs";
 /**
  * This resolver function will fetch and return the post created by the user from database.
  * @param parent - An object that is the return value of the resolver for this field's parent.
  * @returns An array which conatins the post created by the user.
  */
-
+const isValidCursor = async (cursor: string | null): Promise<boolean> => {
+  const result = await Post.findById(cursor);
+  return result ? true : false;
+};
 export const posts: UserResolvers["posts"] = async (parent, args) => {
   const paginationArgs = parseRelayConnectionArguments(args);
-
+  if (!isValidCursor(paginationArgs.cursor)) {
+    return {
+      edges: [],
+      pageInfo: {
+        hasNextPage: false,
+        hasPreviousPage: false,
+        startCursor: null,
+        endCursor: null,
+      },
+      totalCount: 0,
+    };
+  }
   const query: Record<string, unknown> = {
     creatorId: parent._id,
   };
@@ -23,19 +40,31 @@ export const posts: UserResolvers["posts"] = async (parent, args) => {
     .limit(paginationArgs.limit + 1)
     .lean();
 
+  const pageInfo: ConnectionPageInfo = {
+    hasNextPage: false,
+    hasPreviousPage: false,
+    startCursor: null,
+    endCursor: null,
+  };
+  if (posts.length > paginationArgs.limit) {
+    if (paginationArgs.direction == "FORWARD") {
+      pageInfo.hasNextPage = true;
+    } else {
+      pageInfo.hasPreviousPage = true;
+    }
+    posts.pop();
+  }
   if (paginationArgs.direction == "BACKWARD") {
     posts.reverse();
   }
-  const pageInfo = {
-    hasNextPage: paginationArgs.direction == "FORWARD" ? true : false,
-    hasPreviousPage: paginationArgs.direction == "BACKWARD" ? true : false,
-    startCursor: posts[0]?._id.toString(),
-    endCursor: posts[posts.length - 1]?._id.toString(),
-  };
+
   const edges = posts.map((post) => ({
     node: post,
     cursor: post._id.toString(),
   }));
+  pageInfo.startCursor = edges[0].cursor;
+  1;
+  pageInfo.endCursor = edges[edges.length - 1].cursor;
   return {
     edges,
     pageInfo,
