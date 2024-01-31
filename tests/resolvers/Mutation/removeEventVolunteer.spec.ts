272f5516866d52669648d850996457aee1545184
Lakshya Satpal <lakshyasatpal372@gmail.com>
2024-02-01 03:06:38 +0530

    write tests for eventVolunteersByEvent and removeEventVolunteers

---
+++ tests/resolvers/Mutation/removeEventVolunteer.spec.ts
@@ -0,0 +1,124 @@
+import type mongoose from "mongoose";
+import { Types } from "mongoose";
+import type { MutationUpdateEventVolunteerArgs } from "../../../src/types/generatedGraphQLTypes";
+import { connect, disconnect } from "../../helpers/db";
+import {
+  USER_NOT_FOUND_ERROR,
+  EventVolunteerResponse,
+  EVENT_VOLUNTEER_NOT_FOUND_ERROR,
+  EVENT_VOLUNTEER_INVITE_USER_MISTMATCH,
+} from "../../../src/constants";
+import {
+  beforeAll,
+  afterAll,
+  describe,
+  it,
+  expect,
+  vi,
+  afterEach,
+} from "vitest";
+import type { TestUserType } from "../../helpers/userAndOrg";
+import type { TestEventVolunteerType } from "../../helpers/events";
+import { createTestEventAndVolunteer } from "../../helpers/events";
+
+let MONGOOSE_INSTANCE: typeof mongoose;
+let testUser: TestUserType;
+let testEventVolunteer: TestEventVolunteerType;
+
+beforeAll(async () => {
+  MONGOOSE_INSTANCE = await connect();
+  const temp = await createTestEventAndVolunteer();
+  testUser = temp[0];
+  testEventVolunteer = temp[2];
+});
+
+afterAll(async () => {
+  await disconnect(MONGOOSE_INSTANCE);
+});
+
+describe("resolvers -> Mutation -> removeEventVolunteer", () => {
+  afterEach(() => {
+    vi.doUnmock("../../../src/constants");
+    vi.resetModules();
+  });
+  it(`throws NotFoundError if no user exists with _id === context.userId `, async () => {
+    const { requestContext } = await import("../../../src/libraries");
+
+    const spy = vi
+      .spyOn(requestContext, "translate")
+      .mockImplementationOnce((message) => `Translated ${message}`);
+
+    try {
+      const args: MutationUpdateEventVolunteerArgs = {
+        id: testEventVolunteer?._id,
+      };
+
+      const context = { userId: Types.ObjectId().toString() };
+
+      const { updateEventVolunteer: updateEventVolunteerResolver } =
+        await import("../../../src/resolvers/Mutation/updateEventVolunteer");
+
+      await updateEventVolunteerResolver?.({}, args, context);
+    } catch (error: unknown) {
+      expect(spy).toHaveBeenLastCalledWith(USER_NOT_FOUND_ERROR.MESSAGE);
+      expect((error as Error).message).toEqual(
+        `Translated ${USER_NOT_FOUND_ERROR.MESSAGE}`
+      );
+    }
+  });
+
+  it(`throws NotFoundError if no event volunteer exists with _id === args.id`, async () => {
+    const { requestContext } = await import("../../../src/libraries");
+
+    const spy = vi
+      .spyOn(requestContext, "translate")
+      .mockImplementationOnce((message) => `Translated ${message}`);
+
+    try {
+      const args: MutationUpdateEventVolunteerArgs = {
+        id: Types.ObjectId().toString(),
+      };
+
+      const context = { userId: testUser?._id };
+
+      const { removeEventVolunteer: removeEventVolunteerResolver } =
+        await import("../../../src/resolvers/Mutation/removeEventVolunteer");
+
+      await removeEventVolunteerResolver?.({}, args, context);
+    } catch (error: unknown) {
+      expect(spy).toHaveBeenLastCalledWith(
+        EVENT_VOLUNTEER_NOT_FOUND_ERROR.MESSAGE
+      );
+      expect((error as Error).message).toEqual(
+        `Translated ${EVENT_VOLUNTEER_NOT_FOUND_ERROR.MESSAGE}`
+      );
+    }
+  });
+
+  it(`removes event volunteer with _id === args.id and returns it`, async () => {
+    const args: MutationUpdateEventVolunteerArgs = {
+      id: testEventVolunteer?._id,
+    };
+
+    const context = { userId: testUser?._id };
+    const { removeEventVolunteer: removeEventVolunteerResolver } = await import(
+      "../../../src/resolvers/Mutation/removeEventVolunteer"
+    );
+
+    const deletedVolunteer = await removeEventVolunteerResolver?.(
+      {},
+      args,
+      context
+    );
+
+    expect(deletedVolunteer).toEqual(
+      expect.objectContaining({
+        _id: testEventVolunteer?._id,
+        userId: testEventVolunteer?.userId,
+        isInvited: testEventVolunteer?.isInvited,
+        isAssigned: testEventVolunteer?.isAssigned,
+        response: testEventVolunteer?.response,
+      })
+    );
+  });
+});
