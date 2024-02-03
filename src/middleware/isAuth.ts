0698f8cdfab2e3b1731d975a6ea21792bfa2b98f
aditya <adiraiar022@gmail.com>
2024-02-03 22:47:08 +0530

    new commit

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -27,58 +27,58 @@ export const isAuth = (request: Request): InterfaceAuthData => {
   };
 
   // This checks to see if there is an authorization field within the incoming request
-  // const authHeader = request.headers.authorization;
+  const authHeader = request.headers.authorization;
 
-  // // If no authorization header was sent from the client
-  // if (!authHeader) {
-  //   return authData;
-  // }
+  // If no authorization header was sent from the client
+  if (!authHeader) {
+    return authData;
+  }
 
-  // // format of request sent will be Bearer tokenvalue
-  // // this splits it into two values bearer and the token
-  // const token = authHeader.split(" ")[1];
+  // format of request sent will be Bearer tokenvalue
+  // this splits it into two values bearer and the token
+  const token = authHeader.split(" ")[1];
 
-  // // if the token is null or an empty string
-  // if (!token || token === "") {
-  //   return authData;
-  // }
+  // if the token is null or an empty string
+  if (!token || token === "") {
+    return authData;
+  }
 
-  // // uses key created in the auth resolver
-  // // to be changed in production
-  // // only tokens created with this key will be valid tokens
-  // let decodedToken: any;
-  // try {
-  //   decodedToken = jwt.verify(
-  //     token,
-  //     ACCESS_TOKEN_SECRET as string,
-  //     (err, decoded) => {
-  //       if (err) {
-  //         return err;
-  //       }
-  //       return decoded;
-  //     }
-  //   ); // If there is an error decoded token would contain it
+  // uses key created in the auth resolver
+  // to be changed in production
+  // only tokens created with this key will be valid tokens
+  let decodedToken: any;
+  try {
+    decodedToken = jwt.verify(
+      token,
+      ACCESS_TOKEN_SECRET as string,
+      (err, decoded) => {
+        if (err) {
+          return err;
+        }
+        return decoded;
+      }
+    ); // If there is an error decoded token would contain it
 
-  //   if (decodedToken.name === "TokenExpiredError") {
-  //     // If the token has expired set the expired field of authData to true and return it
-  //     authData.expired = true;
-  //     return authData;
-  //   }
-  // } catch (e) {
-  //   authData.expired = true;
-  //   return authData;
-  // }
+    if (decodedToken.name === "TokenExpiredError") {
+      // If the token has expired set the expired field of authData to true and return it
+      authData.expired = true;
+      return authData;
+    }
+  } catch (e) {
+    authData.expired = true;
+    return authData;
+  }
 
-  // // if the decoded token is not set
-  // if (!decodedToken) {
-  //   logger.info("decoded token is not present");
-  //   return authData;
-  // }
+  // if the decoded token is not set
+  if (!decodedToken) {
+    logger.info("decoded token is not present");
+    return authData;
+  }
 
   // shows the user is an authenticated user
   authData.isAuth = true;
   // pulls user data(userId) out of the token and attaches it to userId field of authData object
-  authData.userId = `64378abd85008f171cf2990d`;
+  authData.userId = decodedToken.userId;
 
   return authData;
 };
