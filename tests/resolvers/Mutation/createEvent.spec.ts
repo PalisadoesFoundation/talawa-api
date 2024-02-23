9a5d4181eada52390ac8414e1d95b1f267d912de
Shekhar Patel <90516956+duplixx@users.noreply.github.com>
2024-02-24 00:15:35 +0530

    covered tests

--- tests/resolvers/Mutation/createEvent.spec.ts
+++ tests/resolvers/Mutation/createEvent.spec.ts
@@ -257,6 +257,10 @@ describe("resolvers -> Mutation -> createEvent", () => {
         creatorId: testUser?._id,
         admins: expect.arrayContaining([testUser?._id]),
         organization: testOrganization?._id,
+        startDate: new Date("2023-01-01T00:00:00Z"),
+        startTime: new Date().toUTCString(),
+        images: ["image_url_1", "image_url_2", "image_url_3", "image_url_4"],
+        recurrance: "ONCE",
       }),
     );
 
