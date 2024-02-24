2df25115463c1e7234ad683a039f90d39e64d9d6
Ankit Varshney <lyfflame45@gmail.com>
2024-02-24 12:48:34 +0530

    fix graphqlConnection

--- src/resolvers/User/posts.ts
+++ src/resolvers/User/posts.ts
@@ -1,14 +1,20 @@
 // import  {  Post } from "../../models";
+import type { Types } from "mongoose";
 import type { InterfacePost } from "../../models";
 import { Post } from "../../models";
 import type { UserResolvers } from "../../types/generatedGraphQLTypes";
 import {
-  generateConnectionObject,
-  getFilterObject,
-  getLimit,
-  getSortingObject,
-} from "../../utilities/graphqlConnectionFactory";
-import { parseRelayConnectionArguments } from "../../utilities/parseRelayConnectionArguments";
+  getCommonGraphQLConnectionFilter,
+  getCommonGraphQLConnectionSort,
+  parseGraphQLConnectionArguments,
+  transformToDefaultGraphQLConnection,
+  type DefaultGraphQLArgumentError,
+  type ParseGraphQLConnectionCursorArguments,
+  type ParseGraphQLConnectionCursorResult,
+} from "../../utilities/graphQLConnection";
+
+import { GraphQLError } from "graphql";
+import { MAXIMUM_FETCH_LIMIT } from "../../constants";
 
 /**
  * Resolver function to fetch and return posts created by a user from the database.
@@ -17,32 +23,101 @@ import { parseRelayConnectionArguments } from "../../utilities/parseRelayConnect
  * @returns An object containing an array of posts,totalCount of post and pagination information.
  */
 export const posts: UserResolvers["posts"] = async (parent, args) => {
-  const paginationArgs = parseRelayConnectionArguments(args, 10);
-
-  let query: Record<string, unknown> = {
-    creatorId: parent._id,
-  };
-  const filterObject = getFilterObject(paginationArgs);
-  if (filterObject) {
-    query = { ...query, ...filterObject };
+  const parseGraphQLConnectionArgumentsResult =
+    await parseGraphQLConnectionArguments({
+      args,
+      parseCursor: (args) =>
+        parseCursor({
+          ...args,
+          creatorId: parent._id,
+        }),
+      maximumLimit: MAXIMUM_FETCH_LIMIT,
+    });
+  if (!parseGraphQLConnectionArgumentsResult.isSuccessful) {
+    throw new GraphQLError("Invalid arguments provided.", {
+      extensions: {
+        code: "INVALID_ARGUMENTS",
+        errors: parseGraphQLConnectionArgumentsResult.errors,
+      },
+    });
   }
+  const { parsedArgs } = parseGraphQLConnectionArgumentsResult;
+
+  const filter = getCommonGraphQLConnectionFilter({
+    cursor: parsedArgs.cursor,
+    direction: parsedArgs.direction,
+  });
 
   //get the sorting object
-  const sortingObject = { createdAt: -1 };
-  const sortedField = getSortingObject(paginationArgs.direction, sortingObject);
 
-  // Fetch posts from the database.
-  const posts = await Post.find(query)
-    .sort(sortedField)
-    .limit(getLimit(paginationArgs.limit))
-    .lean();
+  const sort = getCommonGraphQLConnectionSort({
+    direction: parsedArgs.direction,
+  });
 
-  const getNodeFromResult = (object: InterfacePost): InterfacePost => object;
+  const [objectList, totalCount] = await Promise.all([
+    Post.find({
+      ...filter,
+      creatorId: parent._id,
+    })
+      .sort(sort)
+      .limit(parsedArgs.limit)
+      .lean()
+      .exec(),
 
-  const result = generateConnectionObject(
-    paginationArgs,
-    posts,
-    getNodeFromResult,
-  );
-  return result.data;
+    Post.find({
+      creatorId: parent._id,
+    })
+      .countDocuments()
+      .exec(),
+  ]);
+
+  return transformToDefaultGraphQLConnection<
+    ParsedCursor,
+    InterfacePost,
+    InterfacePost
+  >({
+    objectList,
+    parsedArgs,
+    totalCount,
+  });
+};
+/*
+This is typescript type of the parsed cursor for this connection resolver.
+*/
+type ParsedCursor = string;
+
+/*
+This function is used to validate and transform the cursor passed to this connnection
+resolver.
+*/
+export const parseCursor = async ({
+  cursorValue,
+  cursorName,
+  cursorPath,
+  creatorId,
+}: ParseGraphQLConnectionCursorArguments & {
+  creatorId: string | Types.ObjectId;
+}): ParseGraphQLConnectionCursorResult<ParsedCursor> => {
+  const errors: DefaultGraphQLArgumentError[] = [];
+  const post = await Post.findOne({
+    _id: cursorValue,
+    creatorId,
+  });
+  if (!post) {
+    errors.push({
+      message: `Argument ${cursorName} is an invalid cursor.`,
+      path: cursorPath,
+    });
+    if (errors.length !== 0) {
+      return {
+        errors,
+        isSuccessful: false,
+      };
+    }
+  }
+
+  return {
+    isSuccessful: true,
+    parsedCursor: cursorValue,
+  };
 };
--- tests/resolvers/User/post.spec.ts
+++ tests/resolvers/User/post.spec.ts
@@ -1,23 +1,37 @@
 // Replace with the correct path
 import { GraphQLError } from "graphql";
 import type mongoose from "mongoose";
+import { nanoid } from "nanoid";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
-import type { InterfaceUser } from "../../../src/models";
+import { Post, type InterfaceUser } from "../../../src/models";
 import { posts as postResolver } from "../../../src/resolvers/User/posts";
-import type { ConnectionPageInfo } from "../../../src/types/generatedGraphQLTypes";
-import type { RelayConnectionArguments } from "../../../src/utilities/parseRelayConnectionArguments";
-import { parseRelayConnectionArguments } from "../../../src/utilities/parseRelayConnectionArguments";
+import type { PostEdge } from "../../../src/types/generatedGraphQLTypes";
+import type { DefaultGraphQLArgumentError } from "../../../src/utilities/graphQLConnection";
 import { connect, disconnect } from "../../helpers/db";
-import { createTestPost } from "../../helpers/posts";
-import type { TestUserType } from "../../helpers/userAndOrg";
+import { createTestPost, type TestPostType } from "../../helpers/posts";
+import type {
+  TestOrganizationType,
+  TestUserType,
+} from "../../helpers/userAndOrg";
 
 let MONGOOSE_INSTANCE: typeof mongoose;
 let testUser: TestUserType;
+let testPost: TestPostType;
+let testPost2: TestPostType;
+let testOrganization: TestOrganizationType;
 
 beforeAll(async () => {
   MONGOOSE_INSTANCE = await connect();
   const userOrgAndPost = await createTestPost();
   testUser = userOrgAndPost[0];
+  testOrganization = userOrgAndPost[1];
+  testPost = userOrgAndPost[2];
+  testPost2 = await Post.create({
+    text: `text${nanoid().toLowerCase()}`,
+    creatorId: testUser?._id,
+    organization: testOrganization?._id,
+    pinned: false,
+  });
 });
 
 afterAll(async () => {
@@ -26,154 +40,66 @@ afterAll(async () => {
 });
 
 describe("resolvers -> User -> post", () => {
-  it("returns posts created by the user", async () => {
-    const parent = testUser?.toObject() as InterfaceUser;
-    const args: RelayConnectionArguments = {
-      first: 1,
-      after: testUser?._id,
-    };
-    const postConnection = await postResolver?.(parent, args, {});
-
-    if (postConnection) {
-      console.log(postConnection);
-
-      expect(postConnection.edges).toHaveLength(0);
-    }
-  });
-
-  it("constructs query correctly with $gt operator when direction is BACKWARD", () => {
-    const args: RelayConnectionArguments = {
-      last: 1,
-      before: testUser?._id,
-    };
-    const result = parseRelayConnectionArguments(args, 10);
-    const query: Record<string, unknown> = {};
-    if (result.direction == "BACKWARD") {
-      query._id = { $gt: result.cursor };
-    }
-    expect(query).toEqual({ _id: { $gt: result.cursor } });
-  });
-  it("if first is greater than the maximum limit it should set limit to the maximum limit", () => {
-    const args: RelayConnectionArguments = {
-      first: 11,
-      after: "cursor",
-    };
-    const result = parseRelayConnectionArguments(args, 10);
-    expect(result.limit).toBe(10);
-  });
-  it("if last is greater than the maximum limit it should set limit to the maximum limit", () => {
-    const args: RelayConnectionArguments = {
-      last: 11,
-      before: "cursor",
-    };
-    const result = parseRelayConnectionArguments(args, 10);
-    expect(result.limit).toBe(10);
-  });
-  it("handle post beyond the limit correctly", () => {
-    const args: RelayConnectionArguments = {
-      first: 5,
-      after: "cursor",
-    };
-    const paginationArgs = parseRelayConnectionArguments(args, 10);
-    const posts: unknown[] = Array.from({ length: 6 }).map((_, index) => ({
-      _id: `post_${index}`,
-    }));
-    const pageInfo: ConnectionPageInfo = {
-      hasNextPage: false,
-      hasPreviousPage: false,
-      startCursor: null,
-      endCursor: null,
-    };
-    if (posts.length > paginationArgs.limit) {
-      if (paginationArgs.direction === "FORWARD") {
-        pageInfo.hasNextPage = true;
-      } else {
-        pageInfo.hasPreviousPage = true;
+  it(`throws GraphQLError if invalid arguments are provided to the resolver`, async () => {
+    try {
+      const parent = testUser?.toObject() as InterfaceUser;
+      await postResolver?.(parent, {}, {});
+    } catch (error) {
+      if (error instanceof GraphQLError) {
+        expect(error.extensions.code).toEqual("INVALID_ARGUMENTS");
+        expect(
+          (error.extensions.errors as DefaultGraphQLArgumentError[]).length,
+        ).toBeGreaterThan(0);
       }
-      posts.pop();
     }
-
-    expect(pageInfo.hasNextPage).toBe(true);
-    expect(posts.length).toBe(paginationArgs.limit);
   });
-  it("reverses posts array when fetching in backward direction", () => {
-    const args: RelayConnectionArguments = {
-      last: 5,
-      before: "cursor", // Example cursor
-    };
-    const paginationArgs = parseRelayConnectionArguments(args, 10);
-    const posts: unknown[] = [
-      { _id: "post_1" },
-      { _id: "post_2" },
-      { _id: "post_3" },
-    ];
-    if (paginationArgs.direction === "BACKWARD") {
-      posts.reverse();
-    }
-    expect(posts).toEqual([
-      { _id: "post_3" },
-      { _id: "post_2" },
-      { _id: "post_1" },
-    ]);
-  });
-});
-describe("parseConnectionArguments", () => {
-  it("should parse connection arguments correctly with 'first'", () => {
-    const args: RelayConnectionArguments = {
-      first: 5,
-      after: "cursor",
-    };
-    const result = parseRelayConnectionArguments(args, 10);
-    expect(result.direction).toBe("FORWARD");
-    expect(result.limit).toBe(5);
-    expect(result.cursor).toBe("cursor");
-  });
-
-  it("should parse connection arguments correctly with 'last'", () => {
-    const args: RelayConnectionArguments = {
-      last: 5,
-      before: "cursor",
-    };
-    const result = parseRelayConnectionArguments(args, 10);
-    expect(result.direction).toBe("BACKWARD");
-    expect(result.limit).toBe(5);
-    expect(result.cursor).toBe("cursor");
-  });
-
-  it("should throw an error when 'first' and 'last' are provided", () => {
-    const args: RelayConnectionArguments = {
-      first: 5,
-      last: 5,
-    };
-    expect(() => parseRelayConnectionArguments(args, 10)).toThrowError(
-      GraphQLError,
+  it(`returns the expected connection object`, async () => {
+    const parent = testUser as InterfaceUser;
+    const connection = await postResolver?.(
+      parent,
+      {
+        first: 2,
+      },
+      {},
     );
-  });
 
-  it("should throw an error when 'first' and 'before' are provided", () => {
-    const args: RelayConnectionArguments = {
-      first: 5,
-      before: "cursor",
-    };
-    expect(() => parseRelayConnectionArguments(args, 10)).toThrowError(
-      GraphQLError,
-    );
-  });
+    const totalCount = await Post.find({
+      creatorId: testUser?._id,
+    }).countDocuments();
 
-  it("should throw an error when 'last' and 'after' are provided", () => {
-    const args: RelayConnectionArguments = {
-      last: 5,
-      after: "cursor",
-    };
-    expect(() => parseRelayConnectionArguments(args, 10)).toThrowError(
-      GraphQLError,
-    );
-  });
+    // Log the specific properties of the connection object and the expected value
+    console.log("Connection:", connection);
+    console.log("Expected:", {
+      edges: [
+        {
+          cursor: testPost2?._id,
+          node: testPost2,
+        },
+        {
+          cursor: testPost?._id,
+          node: testPost,
+        },
+      ],
+      pageInfo: {
+        endCursor: testPost?._id,
+        hasNextPage: false,
+        hasPreviousPage: false,
+        startCursor: testPost2?._id,
+      },
+      totalCount,
+    });
 
-  it("should throw an error when neither 'first' nor 'last' are provided", () => {
-    const args: RelayConnectionArguments = {};
-    expect(() => parseRelayConnectionArguments(args, 10)).toThrowError(
-      GraphQLError,
+    // Check individual properties
+    expect((connection?.edges[0] as unknown as PostEdge).cursor).toEqual(
+      testPost2?._id,
     );
+    expect((connection?.edges[1] as unknown as PostEdge).cursor).toEqual(
+      testPost?._id,
+    );
+    expect(connection?.pageInfo.endCursor).toEqual(testPost?._id);
+    expect(connection?.pageInfo.hasNextPage).toBe(false);
+    expect(connection?.pageInfo.hasPreviousPage).toBe(false);
+    expect(connection?.pageInfo.startCursor).toEqual(testPost2?._id);
+    expect(connection?.totalCount).toEqual(totalCount);
   });
 });
