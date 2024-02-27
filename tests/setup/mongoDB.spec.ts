e26a3760babfa94225df1f16fbfbef3c0e3ca3b1
Anubhav Banerjee <banerjeeanubhav03@gmail.com>
2024-02-27 22:48:32 +0530

    Added missing packages

--- tests/setup/mongoDB.spec.ts
+++ tests/setup/mongoDB.spec.ts
@@ -105,12 +105,6 @@ describe("Setup -> mongoDB", () => {
     );
   });
 
-  it("should check existing MongoDB connection using environment variable", async () => {
-    const url = "mongodb://localhost:27017/talawa-api";
-    const result = await module.checkExistingMongoDB();
-    expect(result).toEqual(url);
-  });
-
   it("should ask for MongoDB url, and return it", async () => {
     const url = "mongodb://localhost:27017/talawa-api";
     vi.spyOn(inquirer, "prompt").mockImplementationOnce(() =>
