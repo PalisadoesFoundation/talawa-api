97e1affb9ee679f47f5dec811c6374e2567ca016
Ankit Varshney <lyfflame45@gmail.com>
2024-02-28 19:10:27 +0530

    fix formatting issue

--- tests/resolvers/Organization/posts.spec.ts
+++ tests/resolvers/Organization/posts.spec.ts
@@ -69,7 +69,6 @@ describe("resolvers -> Organization -> post", () => {
 
     const totalCount = await Post.find({
       organization: testOrganization?._id,
-
     }).countDocuments();
 
     expect((connection?.edges[0] as unknown as PostEdge).cursor).toEqual(
