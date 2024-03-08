5964946595da6613fe404c25a10c0c29b047accf
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2024-03-08 19:07:34 +0530

    Added exception in pull_req.yml

--- src/resolvers/AgendaSection/createdBy.ts
+++ src/resolvers/AgendaSection/createdBy.ts
@@ -1,8 +1,8 @@
-// import type { AgendaSectionResolvers } from "../../types/generatedGraphQLTypes";
-// import { User } from "../../models";
+import type { AgendaSectionResolvers } from "../../types/generatedGraphQLTypes";
+import { User } from "../../models";
 
-// export const createdBy: AgendaSectionResolvers["createdBy"] = async (
-//   parent,
-// ) => {
-//   return User.findOne(parent.createdBy).lean();
-// };
+export const createdBy: AgendaSectionResolvers["createdBy"] = async (
+  parent,
+) => {
+  return User.findOne(parent.createdBy).lean();
+};
