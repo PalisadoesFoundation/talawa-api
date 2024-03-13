38f1afa25b5600a5c2c83b1d578dea6819e4b48d
meetul <meetulrathore98@gmail.com>
2024-03-13 10:46:48 +0530

    add error for existing action item category in updateActionItemCategory

--- tests/resolvers/Mutation/createActionItem.spec.ts
+++ tests/resolvers/Mutation/createActionItem.spec.ts
@@ -71,7 +71,7 @@ beforeAll(async () => {
     description: `description${nanoid().toLowerCase()}`,
     allDay: true,
     startDate: new Date(),
-    recurring: true,
+    recurring: false,
     isPublic: true,
     isRegisterable: true,
     creatorId: randomUser?._id,
