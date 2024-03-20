6dd95f8b342102684748024eaa03f804122b0651
Aarish Shah Mohsin <aarishshah1@gmail.com>
2024-03-20 19:10:28 +0530

    added tests for setImageUploadSize.ts

--- tests/setup/setImageUploadSize.spec.ts
+++ tests/setup/setImageUploadSize.spec.ts
@@ -1,63 +1,83 @@
-import { it, expect, vi, describe, beforeEach } from "vitest";
-import fs from "fs";
-import dotenv from "dotenv";
+import { describe, it, expect, vi } from "vitest";
 import {
   setImageUploadSize,
   validateImageFileSize,
 } from "../../src/setup/setImageUploadSize";
+import fs from "fs";
+import dotenv from "dotenv";
 import { MAXIMUM_IMAGE_SIZE_LIMIT_KB } from "../../src/constants";
 
-/*
-  Test Case 1:
-  Description: setImageUploadSize updates .env_test for the test environment with the correct size.
-  Expected Behavior: When setImageUploadSize function is called with a size value, it should update the .env_test file with the provided size in kilobytes.
+describe("setImageUploadSize", () => {
+  const originalEnv = { ...process.env };
 
-  Test Case 2:
-  Description: setImageUploadSize truncates size if exceeding the maximum limit.
-  Expected Behavior: When setImageUploadSize function is called with a size value exceeding the maximum limit, it should truncate the size to the maximum limit defined by MAXIMUM_IMAGE_SIZE_LIMIT_KB constant and update the .env_test file accordingly.
+  it("should set the image upload size correctly in .env file", async () => {
+    vi.spyOn(dotenv, "parse").mockResolvedValue({
+      IMAGE_SIZE_LIMIT_KB: "500",
+    });
+    vi.spyOn(fs, "readFileSync").mockReturnValue("IMAGE_SIZE_LIMIT_KB=500");
 
-  Test Case 3:
-  Description: The validateImageFileSize function returns true for a size within the valid range.
-  Expected Behavior: When validateImageFileSize function is called with a size value within the valid range, it should return true indicating that the size is valid.
+    const writeFileSyncMock = vi.spyOn(fs, "writeFileSync");
+    const appendFileSyncMock = vi.spyOn(fs, "appendFileSync");
 
-  Test Case 4:
-  Description: The validateImageFileSize function returns false for a size exceeding the valid range.
-  Expected Behavior: When validateImageFileSize function is called with a size value exceeding the valid range, it should return false indicating that the size is not valid.
+    await setImageUploadSize(500);
 
-  Note: Each test case involves setting up the necessary environment, executing the functions, and asserting the expected behavior.
-*/
-describe("Setup -> setImageUploadSize", () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
-  });
-
-  it("setImageUploadSize updates .env_test for test environment with correct size.", async () => {
-    const size = 1300;
-    await setImageUploadSize(size);
-
-    const env = dotenv.parse(fs.readFileSync(".env_test"));
-    expect(env.IMAGE_SIZE_LIMIT_KB).toEqual(size.toString());
-  });
-
-  it("setImageUploadSize truncates size if exceeding limit", async () => {
-    const size = 2000000;
-    await setImageUploadSize(size);
-
-    const env = dotenv.parse(fs.readFileSync(".env_test"));
-    expect(env.IMAGE_SIZE_LIMIT_KB).toEqual(
-      MAXIMUM_IMAGE_SIZE_LIMIT_KB.toString(),
+    expect(writeFileSyncMock).toHaveBeenCalledWith(".env_test", "");
+    expect(appendFileSyncMock).toHaveBeenCalledWith(
+      ".env_test",
+      "IMAGE_SIZE_LIMIT_KB=500\n",
     );
   });
 
-  it("function validateImageFileSize returns true for size in range.", async () => {
-    const size = 13000;
-    const result = validateImageFileSize(size);
-    expect(result).toEqual(true);
+  it("should set the image upload size correctly in .env file when size is larger than the maximum limit", async () => {
+    vi.spyOn(dotenv, "parse").mockResolvedValue({
+      IMAGE_SIZE_LIMIT_KB: MAXIMUM_IMAGE_SIZE_LIMIT_KB.toString(),
+    });
+    vi.spyOn(fs, "readFileSync").mockReturnValue(
+      `IMAGE_SIZE_LIMIT_KB=${MAXIMUM_IMAGE_SIZE_LIMIT_KB}`,
+    );
+    const writeFileSyncMock = vi.spyOn(fs, "writeFileSync");
+    const appendFileSyncMock = vi.spyOn(fs, "appendFileSync");
+    await setImageUploadSize(MAXIMUM_IMAGE_SIZE_LIMIT_KB + 1);
+
+    expect(writeFileSyncMock).toHaveBeenCalledWith(".env_test", "");
+    expect(appendFileSyncMock).toHaveBeenCalledWith(
+      ".env_test",
+      `IMAGE_SIZE_LIMIT_KB=${MAXIMUM_IMAGE_SIZE_LIMIT_KB}\n`,
+    );
   });
 
-  it("function validateImageFileSize returns false for size exceeding range.", async () => {
-    const size = -1000;
-    const result = validateImageFileSize(size);
-    expect(result).toEqual(false);
+  it("should set the image upload size when node environment is not 'test'", async () => {
+    process.env.NODE_ENV = "development";
+
+    vi.spyOn(dotenv, "parse").mockResolvedValue({
+      IMAGE_SIZE_LIMIT_KB: "1000",
+    });
+
+    vi.spyOn(fs, "readFileSync").mockReturnValue("IMAGE_SIZE_LIMIT_KB=1000");
+    const writeFileSyncMock = vi.spyOn(fs, "writeFileSync").mockReturnThis();
+    const appendFileSyncMock = vi.spyOn(fs, "appendFileSync").mockReturnThis();
+
+    await setImageUploadSize(500);
+
+    expect(writeFileSyncMock).toHaveBeenCalledWith(".env", "");
+    expect(appendFileSyncMock).toHaveBeenCalledWith(
+      ".env",
+      `IMAGE_SIZE_LIMIT_KB=500\n`,
+    );
+    process.env = originalEnv;
+  });
+});
+
+describe("validateImageFileSize", () => {
+  it("should return true for a valid image file size", () => {
+    expect(validateImageFileSize(10)).toBe(true);
+  });
+
+  it("should return false for an invalid image file size of 0", () => {
+    expect(validateImageFileSize(0)).toBe(false);
+  });
+
+  it("should return false for a negative image file size", () => {
+    expect(validateImageFileSize(-5)).toBe(false);
   });
 });
