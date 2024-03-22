9035cebf1561e43317a381bb9e9752516c995dbe
Ankit varshney <ankit2210017@akgec.ac.in>
2024-03-22 19:52:38 +0530

    fixed linting error

--- setup.ts
+++ setup.ts
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 import fs from "fs";
 import inquirer from "inquirer";
 import path from "path";
-import { MAXIMUM_IMAGE_SIZE_LIMIT_KB } from "./src/constants";
 /* eslint-disable */
 import type { ExecException } from "child_process";
 import { exec } from "child_process";
 import mongodb from "mongodb";
+import { MAXIMUM_IMAGE_SIZE_LIMIT_KB } from "./src/constants";
 import {
   askForMongoDBUrl,
   checkConnection,
