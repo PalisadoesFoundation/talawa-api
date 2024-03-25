d83daabe93179fa9e492441fd6210db8849b9a18
meetul <meetulrathore98@gmail.com>
2024-03-25 18:26:05 +0530

    test

--- tests/resolvers/Event/recurrenceRule.spec.ts
+++ tests/resolvers/Event/recurrenceRule.spec.ts
@@ -9,8 +9,10 @@ import type mongoose from "mongoose";
 import { beforeAll, afterAll, describe, it, expect } from "vitest";
 import type { InterfaceEvent } from "../../../src/models";
 import { createTestUserAndOrganization } from "../../helpers/userAndOrg";
-import type { TestOrganizationType , TestUserType } from "../../helpers/userAndOrg";
-
+import type {
+  TestOrganizationType,
+  TestUserType,
+} from "../../helpers/userAndOrg";
 
 import { convertToUTCDate } from "../../../src/utilities/recurrenceDatesUtil";
 import type { MutationCreateEventArgs } from "../../../src/types/generatedGraphQLTypes";
