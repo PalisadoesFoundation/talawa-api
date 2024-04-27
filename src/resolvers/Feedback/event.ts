4e58a640e56597cfef3a31cf781df2064fa5ca86
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2024-04-27 11:23:54 -0700

    Deleted all files in the main branch in anticipation of merging develop into main cleanly

--- src/resolvers/Feedback/event.ts
+++
@@ -1,8 +0,0 @@
-import type { FeedbackResolvers } from "../../types/generatedGraphQLTypes";
-import { Event } from "../../models";
-
-export const event: FeedbackResolvers["event"] = async (parent) => {
-  return await Event.findOne({
-    _id: parent.eventId,
-  }).lean();
-};
--- src/resolvers/User/index.ts
+++
@@ -1,8 +0,0 @@
-import type { UserResolvers } from "../../types/generatedGraphQLTypes";
-import { assignedTasks } from "./assignedTasks";
-// import { tagsAssignedWith } from "./tagsAssignedWith";
-
-export const User: UserResolvers = {
-  assignedTasks,
-  // tagsAssignedWith,
-};
