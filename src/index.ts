fe6907d7a7836fe6b6dd5bdc45c7487e9108002c
Disha Talreja <dishatalreja1202@gmail.com>
2024-07-16 12:19:44 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -98,7 +98,7 @@ const wsServer = new WebSocketServer({
 // WebSocketServer start listening.
 const serverCleanup = useServer(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  { schema, context: (ctx, _msg, args) => ({ pubsub, ctx, args }) },
+  { schema, context: (_ctx, _msg, _args) => ({ pubsub }) },
   wsServer,
 );
 
