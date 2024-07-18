1997e965ac1ea7ab0cc3e7f749424d7a4e11fc34
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2024-07-18 14:32:45 +0530

    utilities done

--- src/utilities/encodedImageStorage/encodedImageExtensionCheck.ts
+++ src/utilities/encodedImageStorage/encodedImageExtensionCheck.ts
@@ -1,4 +1,10 @@
+/**
+ * Checks if the extension of an encoded image URL is valid (png, jpg, jpeg).
+ * @param encodedUrl - Encoded URL of the image.
+ * @returns `true` if the extension is valid, otherwise `false`.
+ */
 export const encodedImageExtentionCheck = (encodedUrl: string): boolean => {
+  // Extract the extension from the encodedUrl
   const extension = encodedUrl.substring(
     "data:".length,
     encodedUrl.indexOf(";base64"),
