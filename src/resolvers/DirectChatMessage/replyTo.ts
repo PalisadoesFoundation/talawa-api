f936adc59c90c682bef2722decf2912907d9c1a2
Disha <dishatalreja1202@gmail.com>
2024-07-28 17:30:15 +0530

    removed console logs

--- src/resolvers/DirectChatMessage/replyTo.ts
+++ src/resolvers/DirectChatMessage/replyTo.ts
@@ -3,14 +3,13 @@ import { DirectChatMessage } from "../../models";
 import { MESSAGE_NOT_FOUND_ERROR } from "../../constants";
 import { errors, requestContext } from "../../libraries";
 /**
- * This resolver function will fetch and return the receiver(user) of the Direct chat from the database.
+ * This resolver function will fetch and return the reply to message of the Direct chat from the database.
  * @param parent - An object that is the return value of the resolver for this field's parent.
  * @returns An `object` that contains User's data.
  */
 export const replyTo: DirectChatMessageResolvers["replyTo"] = async (
   parent,
 ) => {
-  console.log("PARENT ", parent);
   if (parent.replyTo) {
     const result = await DirectChatMessage.findOne({
       _id: parent.replyTo,
--- src/resolvers/GroupChatMessage/replyTo.ts
+++ src/resolvers/GroupChatMessage/replyTo.ts
@@ -3,12 +3,11 @@ import { GroupChatMessage } from "../../models";
 import { MESSAGE_NOT_FOUND_ERROR } from "../../constants";
 import { errors, requestContext } from "../../libraries";
 /**
- * This resolver function will fetch and return the receiver(user) of the Direct chat from the database.
+ * This resolver function will fetch and return the replied-to message in a group chat from the database.
  * @param parent - An object that is the return value of the resolver for this field's parent.
  * @returns An `object` that contains User's data.
  */
 export const replyTo: GroupChatMessageResolvers["replyTo"] = async (parent) => {
-  console.log("PARENT ", parent);
   if (parent.replyTo) {
     const result = await GroupChatMessage.findOne({
       _id: parent.replyTo,
