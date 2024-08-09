d8466744b64493479176736fe6b9f221293fe8a0
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2024-08-09 09:57:12 +0530

    done

--- src/resolvers/Mutation/updateNote.ts
+++ src/resolvers/Mutation/updateNote.ts
@@ -17,6 +17,30 @@ import { cacheUsers } from "../../services/UserCache/cacheUser";
 import { findUserInCache } from "../../services/UserCache/findUserInCache";
 import type { MutationResolvers } from "../../types/generatedGraphQLTypes";
 
+/**
+ * Updates an existing note in the system.
+ *
+ * This function updates a specific note in the database. It first checks if the current user
+ * exists and if they have the proper profile. Then it verifies if the note exists and whether
+ * the current user is authorized to update it. If all checks pass, the function updates the note
+ * with the provided data.
+ *
+ * The function performs the following steps:
+ * 1. Retrieves the current user from the cache or database.
+ * 2. Verifies if the current user exists.
+ * 3. Retrieves the current user's profile from the cache or database.
+ * 4. Checks if the user has the necessary authorization to update the note.
+ * 5. Finds the note to be updated in the database.
+ * 6. Verifies that the note belongs to the current user.
+ * 7. Updates the note with the new data provided.
+ *
+ * @param _parent - This parameter is not used in this resolver function.
+ * @param args - The arguments provided by the GraphQL query, including the ID of the note to be updated and the new data.
+ * @param context - The context of the request, containing information about the currently authenticated user.
+ *
+ * @returns  The updated note.
+ */
+
 export const updateNote: MutationResolvers["updateNote"] = async (
   _parent,
   args,
