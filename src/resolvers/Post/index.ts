018a00d983a4071e3e102817e3e36945ec1ae568
xoldd <xoldyckk@gmail.com>
2024-10-07 08:56:17 +0530

    wipe everything
    the old codebase is largely incompatible with the new changes that
    will be made and it is a huge hassle to do it incrementally while
    keeping the backwards compatibility
    
    as such the entire codebase is being wiped to start from scratch and the
    redundant stuff or new changes from the old codebase will be manually
    copied into the new codebase as needed

--- src/resolvers/Post/index.ts
+++
@@ -1,8 +0,0 @@
-import type { PostResolvers } from "../../types/generatedGraphQLTypes";
-import { comments } from "./comments";
-import { creator } from "./creator";
-
-export const Post: PostResolvers = {
-  comments,
-  creator,
-};
