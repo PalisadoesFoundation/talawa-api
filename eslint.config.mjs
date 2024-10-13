b625e8414c4a2155f832dfeb27d7affc11f1cd86
Disha <dishatalreja1202@gmail.com>
2024-10-13 13:02:55 +0530

    fix: eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,17 @@ import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsdoc from "eslint-plugin-tsdoc";
 import _import from "eslint-plugin-import";
 import { fixupPluginRules } from "@eslint/compat";
-import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
-import parser from "@graphql-eslint/eslint-plugin";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
-import graphqlEslint from "@graphql-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
 });
 
 export default [{
@@ -117,11 +115,11 @@ export default [{
     files: ["./src/typeDefs/**/*.graphql"],
 
     plugins: {
-        "@graphql-eslint": graphqlEslint,
+        "@typescript-eslint": typescriptEslint,
     },
 
     languageOptions: {
-        parser: parser,
+        parser: tsParser,
     },
 }, {
     files: ["tests/**/*"],
