70bdea9f6c7b0bb519231fff1f46c32d357bf970
Nitin Singh <nitintwt27@gmail.com>
2024-10-28 21:41:34 +0530

    Restrict user data access in User query

--- src/resolvers/Query/user.ts
+++ src/resolvers/Query/user.ts
@@ -5,12 +5,16 @@ import { AppUserProfile, User } from "../../models";
 import type { QueryResolvers } from "../../types/generatedGraphQLTypes";
 /**
  * This query fetch the user from the database.
+ *
+ * This function ensure that users can only query their own data and not access details of other users , protecting sensitive data.
+ *
  * @param _parent-
  * @param args - An object that contains `id` for the user.
  * @param context-
  * @returns An object that contains user data. If the user is not found then it throws a `NotFoundError` error.
  */
 export const user: QueryResolvers["user"] = async (_parent, args, context) => {
+  // Check if the current user exists in the system
   const currentUserExists = !!(await User.exists({
     _id: context.userId,
   }));
@@ -23,9 +27,18 @@ export const user: QueryResolvers["user"] = async (_parent, args, context) => {
     );
   }
 
-  const user: InterfaceUser = (await User.findOne({
-    _id: args.id,
-  }).lean()) as InterfaceUser;
+  // Check if the requesting user is trying to access their own data
+  if (context.userId !== args.id) {
+    throw new errors.UnauthorizedError(
+      "Access denied. You can only view your own profile.",
+    );
+  }
+
+  //Fetch the user data from the database based on the provided ID (args.id)
+  const user: InterfaceUser = (await User.findById(
+    args.id,
+  ).lean()) as InterfaceUser;
+
   const userAppProfile: InterfaceAppUserProfile = (await AppUserProfile.findOne(
     {
       userId: user._id,
