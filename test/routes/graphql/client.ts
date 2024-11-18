6eb2c8e5b485fafc97c50774744ab87b3e26a87e
xoldd <xoldyckk@gmail.com>
2024-11-19 01:30:17 +0530

    add tests for user associated graphql fields

--- test/routes/graphql/client.ts
+++ test/routes/graphql/client.ts
@@ -4,4 +4,6 @@ import { server } from "../../server";
 /**
  * Graphql client for triggering graphql operations against talawa api's graphql implementation during tests.
  */
-export const mercuriusClient = createMercuriusTestClient(server);
+export const mercuriusClient = createMercuriusTestClient(server, {
+	url: "/graphql",
+});
