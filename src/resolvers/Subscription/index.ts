c7e64b28dfb04e7287e9d0973255e0c5103da7f9
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-01-10 08:43:29 -0800

    20250110084329 Deleted all files in the main branch in anticipation of merging develop into main cleanly

--- src/resolvers/Subscription/index.ts
+++
@@ -1,7 +0,0 @@
-import type { SubscriptionResolvers } from "../../types/generatedGraphQLTypes";
-import { messageSentToChat } from "./messageSentToChat";
-import { onPluginUpdate } from "./onPluginUpdate";
-export const Subscription: SubscriptionResolvers = {
-  messageSentToChat,
-  onPluginUpdate,
-};
