e718f457eef72777a5d749dc4ccecc540457c8b6
Vanshika Sabharwal <vanshikasabharwal06@gmail.com>
2025-01-20 14:08:41 +0530

    updated code acc. to coderabbitai suggestions

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,12 +7,6 @@ export default defineConfig({
 		coverage: {
 			provider: "v8", // or 'istanbul' if you prefer
 			reporter: ["text", "lcov", "html"],
-			thresholds: {
-				lines: 95,
-				functions: 95,
-				branches: 95,
-				statements: 95,
-			}, // Specify 'lcov' to generate lcov.info
 		},
 		// https://vitest.dev/config/#fileparallelism
 		// fileParallelism: true,
