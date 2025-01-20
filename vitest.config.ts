852fc45e65a54b9ff7424f1bf1d914f80e50c180
Vanshika Sabharwal <vanshikasabharwal06@gmail.com>
2025-01-20 13:36:40 +0530

    updated code acc. to coderabbitai suggestions

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,28 +2,23 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [tsconfigPaths()],
-	test: {
-		coverage: {
-			provider: "v8", // or 'istanbul' if you prefer
-			reporter: ["text", "lcov", "html"],
-			thresholds: {
-				lines: 95,
-				functions: 95,
-				branches: 95,
-				statements: 95,
-			}, // Specify 'lcov' to generate lcov.info
-		},
-		// https://vitest.dev/config/#fileparallelism
-		// fileParallelism: true,
+  plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      provider: "v8", // or 'istanbul' if you prefer
+      reporter: ["text", "lcov", "html"],
+    },
+    // https://vitest.dev/config/#fileparallelism
+    // fileParallelism: true,
 
-		// https://vitest.dev/config/#globalsetup
-		globalSetup: ["./test/setup.ts"],
+    // https://vitest.dev/config/#globalsetup
+    globalSetup: ["./test/setup.ts"],
 
-		// https://vitest.dev/config/#passwithnotests
-		passWithNoTests: true,
+    // https://vitest.dev/config/#passwithnotests
+    passWithNoTests: true,
 
-		// // https://vitest.dev/config/#teardowntimeout,
-		// teardownTimeout: 10000
-	},
+    // // https://vitest.dev/config/#teardowntimeout,
+    // teardownTimeout: 10000
+  },
 });
+
