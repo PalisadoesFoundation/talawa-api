3bc3d4d082be5f42085601804f379e3b17be4872
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-01-25 18:09:49 -0800

    Added tutorial directories

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Head from "@docusaurus/Head";
 import Layout from "@theme/Layout";
-import useHomePageAnimations  from "../hooks/useHomePageAnimations";
-import  HeaderHero  from "../components/layout/HeaderHero";
-import  SecondPanel  from "../components/layout/SecondPanel";
-import  ThirdPanel  from "../components/layout/ThirdPanel";
-import  FourthPanel  from "../components/layout/FourthPanel";
-import  FifthPanel  from "../components/layout/FifthPanel";
-import  SixthPanel  from "../components/layout/SixthPanel";
-import  SeventhPanel  from "../components/layout/SeventhPanel";
-import  EighthPanel  from "../components/layout/EightPanel";
+import useHomePageAnimations from "../hooks/useHomePageAnimations";
+import HeaderHero from "../components/layout/HeaderHero";
+import SecondPanel from "../components/layout/SecondPanel";
+import ThirdPanel from "../components/layout/ThirdPanel";
+import FourthPanel from "../components/layout/FourthPanel";
+import FifthPanel from "../components/layout/FifthPanel";
+import SixthPanel from "../components/layout/SixthPanel";
+import SeventhPanel from "../components/layout/SeventhPanel";
+import EighthPanel from "../components/layout/EightPanel";
 
 const Index = () => {
   useHomePageAnimations();
@@ -17,14 +17,17 @@ const Index = () => {
   const pageTitle = "Talawa-Docs: Powered by The Palisadoes";
 
   return (
-    <Layout description="Powering Closer Communities" wrapperClassName="homepage">
+    <Layout
+      description="Powering Closer Communities"
+      wrapperClassName="homepage"
+    >
       <Head>
         <title>{pageTitle}</title>
         <meta property="og:title" content={pageTitle} />
         <meta property="twitter:title" content={pageTitle} />
       </Head>
       <HeaderHero />
-      <SecondPanel/>
+      <SecondPanel />
       <ThirdPanel />
       <FourthPanel />
       <FifthPanel />
