c77efcdcae7eafc556537404889a60d16b3cacd4
Nikhil Harisinghani <nikhilharisinghani26@gmail.com>
2025-01-28 18:37:28 +0000

    coderabbit.ai improvements

--- src/graphql/scalars/EmailAddress.ts
+++ src/graphql/scalars/EmailAddress.ts
@@ -2,6 +2,8 @@ import { EmailAddressResolver } from "graphql-scalars";
 import { builder } from "~/src/graphql/builder";
 
 /**
+ * A custom scalar type for validating email addresses according to RFC 5322.
+ * This ensures that all email fields in the schema are properly validated.
  * More information at this link: {@link https://the-guild.dev/graphql/scalars/docs/scalars/email-address}
  */
 export const EmailAddress = builder.addScalarType(
