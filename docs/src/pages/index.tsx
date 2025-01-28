332a9576e142433b8885f24d2c7c1a66eb272197
Abhi Murali <113867502+Abhi-MS@users.noreply.github.com>
2025-01-27 20:06:34 -0500

    Feature/ux improvements (#3091)
    * Modify the style of navbar to match Talawa docs
    
    * Change navbar links to ensure consistency across websites
    
    * Modify navbar and sidebar styles to match Talawa docs
    
    * Remove unnecessary css icon styling to remove external link icon in navbar
    
    * Fix styling bugs when deployed
    
    * Delete unnecessary sections from homepage
    
    * Fix footer text from My Project, Inc. to The Palisadoes Foundation, LLC.
    
    * Ensure responsiveness
    
    * Update homepage with minimal design
    
    * Delete unnecessary styles from custom.css
    
    * Delete unnecessary styles from index.css and update style for markdown pages in customs.css
    
    * Organize index.css for better readability
    
    * Organize custom.css for better readability
    
    * Modify navabar to have active nav link style
    
    * Resolve coderabbitai suggestions
    
    * Resolve coderabbitai suggestions, breakpoints
    
    * Resolve coderabbitai suggestions, breakpoints 2
    
    ---------
    
    Co-authored-by: Abhi-MS <ahiramims333@gmail.com>

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import Head from "@docusaurus/Head";
 import Layout from "@theme/Layout";
-import useHomePageAnimations  from "../hooks/useHomePageAnimations";
 import  HeaderHero  from "../components/layout/HeaderHero";
-import  SecondPanel  from "../components/layout/SecondPanel";
-import  ThirdPanel  from "../components/layout/ThirdPanel";
-import  FourthPanel  from "../components/layout/FourthPanel";
-import  FifthPanel  from "../components/layout/FifthPanel";
-import  SixthPanel  from "../components/layout/SixthPanel";
-import  SeventhPanel  from "../components/layout/SeventhPanel";
-import  EighthPanel  from "../components/layout/EightPanel";
+
 
 const Index = () => {
-  useHomePageAnimations();
-
   const pageTitle = "Talawa-Docs: Powered by The Palisadoes";
 
   return (
@@ -24,13 +15,6 @@ const Index = () => {
         <meta property="twitter:title" content={pageTitle} />
       </Head>
       <HeaderHero />
-      <SecondPanel/>
-      <ThirdPanel />
-      <FourthPanel />
-      <FifthPanel />
-      <SixthPanel />
-      <SeventhPanel />
-      <EighthPanel />
     </Layout>
   );
 };
