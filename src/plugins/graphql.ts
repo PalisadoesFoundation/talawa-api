675a1ca668d9968b96901ca8725392129ece8ddd
Sancheet <sancheet.p24@medhaviskillsuniversity.edu.in>
2025-02-06 02:43:56 +0530

    Create graphql.ts

---
+++ src/plugins/graphql.ts
@@ -0,0 +1,57 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import fastifyPlugin from 'fastify-plugin';
+import mercurius from 'mercurius';
+import { mercuriusUpload } from 'mercurius-upload';
+import { createContext } from '~/src/graphql/context';
+import { verifyClient } from '~/src/utilities/auth';
+import { schema } from '~/src/graphql/schema';
+import jwt from 'jsonwebtoken';
+
+
+export const graphql = fastifyPlugin(async (fastify: FastifyInstance) => {
+  fastify.register(mercuriusUpload, {
+    maxFieldSize: 1048576,
+    maxFiles: 20,
+    maxFileSize: 10485760,
+  });
+
+  fastify.register(mercurius, {
+    context: (request: FastifyRequest, reply: FastifyReply) =>
+      createContext({
+        fastify,
+        isSubscription: false,
+        request,
+        reply,
+      }),
+    graphiql: {
+      enabled: fastify.envConfig.API_IS_GRAPHIQL,
+    },
+    path: '/graphql',
+    schema,
+    subscription: {
+      context: async (socket, request) =>
+        await createContext({
+          fastify,
+          isSubscription: true,
+          request,
+          socket,
+        }),
+      keepAlive: 1000 * 30,
+      onConnect: (data: { type: "connection_init"; payload: any; }) => {
+        if (data.payload.authToken) {
+          try {
+            const user = jwt.verify(data.payload.authToken, 'your-secret-key');
+            return { user };
+          } catch (err) {
+            throw new Error('Invalid auth token');
+          }
+        }
+        throw new Error('Missing auth token');
+      },
+      onDisconnect: (ctx) => {},
+      verifyClient,
+    },
+  });
+});
+
+export default graphql;
