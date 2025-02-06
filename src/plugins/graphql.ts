47e62d2ed3d1b9349e6d3136a6c8b3d655c49c3d
Sancheet <sancheet.p24@medhaviskillsuniversity.edu.in>
2025-02-06 12:52:25 +0530

    Update graphql.ts

--- src/plugins/graphql.ts
+++ src/plugins/graphql.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import fastifyPlugin from "fastify-plugin";
 import mercurius from "mercurius";
 import { mercuriusUpload } from "mercurius-upload";
