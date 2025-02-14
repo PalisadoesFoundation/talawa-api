5dce47e8b44fdd02d3cef158792a63cdd0003881
prayansh_chhablani <prayanshchhablani@gmail.com>
2025-02-14 20:41:52 +0530

    db fix

--- src/utilities/loadSampleData.ts
+++ src/utilities/loadSampleData.ts
@@ -160,7 +160,7 @@ async function insertCollections(collections: string[]): Promise<void> {
 async function checkCountAfterImport(): Promise<void> {
   try {
     // Connect to MongoDB database
-    await connect("talawa-api");
+    await connect("talawa-dev");
 
     const collections = [
       { name: "users", model: User },
