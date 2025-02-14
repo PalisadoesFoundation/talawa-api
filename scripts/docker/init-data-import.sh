f2b07e9612b7bf777edb397565e5a69cb8a729dc
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2025-02-14 22:51:05 +0530

    updated code

--- scripts/docker/init-data-import.sh
+++ scripts/docker/init-data-import.sh
@@ -4,12 +4,17 @@ set -e
 # Generate new secrets
 ACCESS_TOKEN_SECRET=$(openssl rand -hex 32)
 REFRESH_TOKEN_SECRET=$(openssl rand -hex 32)
+MINIO_ROOT_USER="admin"
+MINIO_ROOT_PASSWORD=$(openssl rand -hex 16)  # Generate a strong random password
 
 # Export them as environment variables
 export ACCESS_TOKEN_SECRET
 export REFRESH_TOKEN_SECRET
+export MINIO_ROOT_USER
+export MINIO_ROOT_PASSWORD
 
 echo "Generated new ACCESS_TOKEN_SECRET and REFRESH_TOKEN_SECRET"
+echo "Generated new MINIO_ROOT_USER and MINIO_ROOT_PASSWORD"
 
 # Import sample data
 echo "Importing sample data..."
