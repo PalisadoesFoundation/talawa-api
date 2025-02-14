018f72258e81e9463c26f529ea0480968ffffacc
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2025-02-14 22:06:12 +0530

    updated files

--- scripts/docker/init-data-import.sh
+++ scripts/docker/init-data-import.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# Generate new secrets
+ACCESS_TOKEN_SECRET=$(openssl rand -hex 32)
+REFRESH_TOKEN_SECRET=$(openssl rand -hex 32)
+
+# Export them as environment variables
+export ACCESS_TOKEN_SECRET
+export REFRESH_TOKEN_SECRET
+
+echo "Generated new ACCESS_TOKEN_SECRET and REFRESH_TOKEN_SECRET"
+
 # Import sample data
 echo "Importing sample data..."
 npm run import:sample-data || {
