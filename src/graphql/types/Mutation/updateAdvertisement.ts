020a66705c900f2a02fd857e041750b79be27b22
udayempire <kuday0726@gmail.com>
2025-02-24 13:44:58 +0000

    fixed updateAdvertisement.ts

--- src/graphql/types/Mutation/updateAdvertisement.ts
+++ src/graphql/types/Mutation/updateAdvertisement.ts
@@ -90,6 +90,150 @@ builder.mutationField("updateAdvertisement", (t) =>
 			}
 
 			const currentUserId = ctx.currentClient.user.id;
+			const [currentUser, existingAdvertisement] = await Promise.all([
+				ctx.drizzleClient.query.usersTable.findFirst({
+					columns: {
+						role: true,
+					},
+					where: (fields, operators) => operators.eq(fields.id, currentUserId),
+				}),
+				ctx.drizzleClient.query.advertisementsTable.findFirst({
+					columns: {
+						endAt: true,
+						organizationId: true,
+						startAt: true,
+					},
+					with: {
+						attachmentsWhereAdvertisement: true,
+						organization: {
+							columns: {
+								countryCode: true,
+							},
+							with: {
+								membershipsWhereOrganization: {
+									columns: {
+										role: true,
+									},
+									where: (fields, operators) =>
+										operators.eq(fields.memberId, currentUserId),
+								},
+							},
+						},
+					},
+					where: (fields, operators) =>
+						operators.eq(fields.id, parsedArgs.input.id),
+				}),
+			]);
+
+			if (currentUser === undefined) {
+				throw new TalawaGraphQLError({
+					extensions: {
+						code: "unauthenticated",
+					},
+				});
+			}
+
+			if (existingAdvertisement === undefined) {
+				throw new TalawaGraphQLError({
+					extensions: {
+						code: "arguments_associated_resources_not_found",
+						issues: [
+							{
+								argumentPath: ["input", "id"],
+							},
+						],
+					},
+				});
+			}
+
+			if (
+				parsedArgs.input.endAt === undefined &&
+				parsedArgs.input.startAt !== undefined &&
+				existingAdvertisement.endAt <= parsedArgs.input.startAt
+			) {
+				throw new TalawaGraphQLError({
+					extensions: {
+						code: "invalid_arguments",
+						issues: [
+							{
+								argumentPath: ["input", "startAt"],
+								message: `Must be smaller than the value: ${parsedArgs.input.startAt.toISOString()}`,
+							},
+						],
+					},
+				});
+			}
+
+			if (
+				parsedArgs.input.endAt !== undefined &&
+				parsedArgs.input.startAt === undefined &&
+				parsedArgs.input.endAt <= existingAdvertisement.startAt
+			) {
+				throw new TalawaGraphQLError({
+					extensions: {
+						code: "invalid_arguments",
+						issues: [
+							{
+								argumentPath: ["input", "endAt"],
+								message: `Must be greater than the value: ${parsedArgs.input.endAt.toISOString()}`,
+							},
+						],
+					},
+				});
+			}
+
+			if (parsedArgs.input.name !== undefined) {
+				const name = parsedArgs.input.name;
+
+				const existingAdvertisementWithName =
+					await ctx.drizzleClient.query.advertisementsTable.findFirst({
+						columns: {
+							type: true,
+						},
+						where: (fields, operators) =>
+							operators.and(
+								operators.eq(fields.name, name),
+								operators.eq(
+									fields.organizationId,
+									existingAdvertisement.organizationId,
+								),
+							),
+					});
+
+				if (existingAdvertisementWithName !== undefined) {
+					throw new TalawaGraphQLError({
+						extensions: {
+							code: "forbidden_action_on_arguments_associated_resources",
+							issues: [
+								{
+									argumentPath: ["input", "name"],
+									message: "This name is not available.",
+								},
+							],
+						},
+					});
+				}
+			}
+
+			const currentUserOrganizationMembership =
+				existingAdvertisement.organization.membershipsWhereOrganization[0];
+
+			if (
+				currentUser.role !== "administrator" &&
+				(currentUserOrganizationMembership === undefined ||
+					currentUserOrganizationMembership.role !== "administrator")
+			) {
+				throw new TalawaGraphQLError({
+					extensions: {
+						code: "unauthorized_action_on_arguments_associated_resources",
+						issues: [
+							{
+								argumentPath: ["input", "id"],
+							},
+						],
+					},
+				});
+			}
 			const [updatedAdvertisement] = await ctx.drizzleClient
 				.update(advertisementsTable)
 				.set({
