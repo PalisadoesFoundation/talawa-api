c679479e75c803ff85d2f05b3c225cb9848399e2
Sancheet <sancheet.p24@medhaviskillsuniversity.edu.in>
2025-02-25 18:26:27 +0530

    Update organizationResolvers.test.ts

--- test/graphql/types/Organization/organizationResolvers.test.ts
+++ test/graphql/types/Organization/organizationResolvers.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect, vi } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 
 // Mock the required imports to ensure coverage
 vi.mock("~/src/graphql/builder", () => ({
