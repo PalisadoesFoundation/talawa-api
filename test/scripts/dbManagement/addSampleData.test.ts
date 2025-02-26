29d92beb91077c5ceb93a942b18534586b25e743
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2025-02-26 22:08:14 +0000

    Minor Update -> reset:data (#3304)
    * Preserved Admin
    
    * clean
    
    * final
    
    ---------
    
    Co-authored-by: JaiPannu-IITI <pannujai06iiti@gmail.com>

--- test/scripts/dbManagement/addSampleData.test.ts
+++ test/scripts/dbManagement/addSampleData.test.ts
@@ -10,9 +10,6 @@ suite("addSampleData main function tests", () => {
 	test("should execute all operations successfully", async () => {
 		// Arrange: simulate successful operations.
 		const pingDBSpy = vi.spyOn(helpers, "pingDB").mockResolvedValue(true);
-		const ensureAdminSpy = vi
-			.spyOn(helpers, "ensureAdministratorExists")
-			.mockResolvedValue(true);
 		const insertCollectionsSpy = vi
 			.spyOn(helpers, "insertCollections")
 			.mockResolvedValue(true);
@@ -25,7 +22,6 @@ suite("addSampleData main function tests", () => {
 
 		// Assert: verify that each helper was called as expected.
 		expect(pingDBSpy).toHaveBeenCalled();
-		expect(ensureAdminSpy).toHaveBeenCalled();
 		expect(insertCollectionsSpy).toHaveBeenCalledWith([
 			"users",
 			"organizations",
@@ -40,9 +36,6 @@ suite("addSampleData main function tests", () => {
 		// Verify that success messages are logged.
 		expect(consoleLogSpy).toHaveBeenCalledWith(
 			expect.stringContaining("Database connected successfully"),
-		);
-		expect(consoleLogSpy).toHaveBeenCalledWith(
-			expect.stringContaining("Administrator setup complete"),
 		);
 		expect(consoleLogSpy).toHaveBeenCalledWith(
 			expect.stringContaining("Sample Data added to the database"),
@@ -60,24 +53,9 @@ suite("addSampleData main function tests", () => {
 		);
 	});
 
-	test("should throw an error when ensureAdministratorExists fails", async () => {
-		// Arrange: pingDB succeeds, but ensureAdministratorExists fails.
-		vi.spyOn(helpers, "pingDB").mockResolvedValue(true);
-		const errorMsg = "admin error";
-		vi.spyOn(helpers, "ensureAdministratorExists").mockRejectedValue(
-			new Error(errorMsg),
-		);
-
-		// Act & Assert: main() should throw the specific error message.
-		await expect(mainModule.main()).rejects.toThrow(
-			"\n\x1b[31mAdministrator access may be lost, try reimporting sample DB to restore access\x1b[0m\n",
-		);
-	});
-
 	test("should throw an error when insertCollections fails", async () => {
-		// Arrange: pingDB and ensureAdministratorExists succeed, but insertCollections fails.
+		// Arrange: pingDB succeed, but insertCollections fails.
 		vi.spyOn(helpers, "pingDB").mockResolvedValue(true);
-		vi.spyOn(helpers, "ensureAdministratorExists").mockResolvedValue(true);
 		const errorMsg = "insert error";
 		vi.spyOn(helpers, "insertCollections").mockRejectedValue(
 			new Error(errorMsg),
