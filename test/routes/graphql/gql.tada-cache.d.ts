27e57665bca48b41d363e1379d3105d41cd0c62d
iamanishx <manishbiswal754@gmail.com>
2025-02-26 15:44:17 +0000

    authorization update

--- test/routes/graphql/gql.tada-cache.d.ts
+++ test/routes/graphql/gql.tada-cache.d.ts
@@ -75,6 +75,6 @@ declare module 'gql.tada' {
     "\n  mutation Mutation_createPost($input: MutationCreatePostInput!) {\n    createPost(input: $input) {\n      id\n      caption\n      pinnedAt\n      organization {\n        id\n      }\n      attachments {\n        mimeType\n        url\n      }\n    }\n  }\n":
       TadaDocumentNode<{ createPost: { id: string; caption: string | null; pinnedAt: string | null; organization: { id: string; } | null; attachments: { mimeType: string | null; url: string | null; }[] | null; } | null; }, { input: { organizationId: string; isPinned?: boolean | null | undefined; caption: string; attachments?: unknown[] | null | undefined; }; }, void>;
     "\n  mutation Mutation_createPresignedUrl($input: MutationCreatePresignedUrlInput!) {\n    createPresignedUrl(input: $input) {\n      fileUrl\n      presignedUrl\n    }\n  }\n":
-      TadaDocumentNode<{ createPresignedUrl: { fileUrl: string | null; presignedUrl: string | null; } | null; }, { input: { fileType: string; fileName: string; }; }, void>;
+      TadaDocumentNode<{ createPresignedUrl: { fileUrl: string | null; presignedUrl: string | null; } | null; }, { input: { organizationId: string; fileType: string; fileName: string; }; }, void>;
   }
 }
