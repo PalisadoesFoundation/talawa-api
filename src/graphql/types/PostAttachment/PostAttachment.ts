0e9c9e658612842a2d6d4ef828a2d19ce88ef309
imanishx <manishbiswal754@gmail.com>
2025-03-10 14:49:12 +0000

    done
    Signed-off-by: imanishx <manishbiswal754@gmail.com>

--- src/graphql/types/PostAttachment/PostAttachment.ts
+++ src/graphql/types/PostAttachment/PostAttachment.ts
@@ -4,13 +4,26 @@ import { builder } from "~/src/graphql/builder";
 export type PostAttachment = typeof postAttachmentsTable.$inferSelect;
 
 export const PostAttachment =
-	builder.objectRef<PostAttachment>("PostAttachment");
+  builder.objectRef<PostAttachment>("PostAttachment");
 
 PostAttachment.implement({
-	description: "Attachment of the post.",
-	fields: (t) => ({
-		mimeType: t.exposeString("mimeType", {
-			description: "Mime type of the attachment.",
-		}),
-	}),
+  description: "Attachment of the post.",
+  fields: (t) => ({
+    mimeType: t.exposeString("mimeType", {
+      description: "Mime type of the attachment.",
+    }),
+    // Add these fields:
+    objectName: t.exposeString("objectName", {
+      description: "Object name used when creating presigned URLs.",
+      nullable: true,
+    }),
+    fileHash: t.exposeString("fileHash", {
+      description: "File hash for deduplication purposes.",
+      nullable: true,
+    }),
+    name: t.exposeString("name", {
+      description: "Identifier name of the attachment.",
+    }),
+    // Any other fields you want to expose...
+  }),
 });
