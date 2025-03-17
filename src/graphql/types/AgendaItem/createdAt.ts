7b824c2481ca70adfc32f07c06a398dede484eb3
im-vedant <194vedantgupta@gmail.com>
2025-03-17 16:00:34 +0000

    add complexity configuration to AgendaItem fields

--- src/graphql/types/AgendaItem/createdAt.ts
+++ src/graphql/types/AgendaItem/createdAt.ts
@@ -1,10 +1,11 @@
 import { TalawaGraphQLError } from "~/src/utilities/TalawaGraphQLError";
+import envConfig from "~/src/utilities/graphqLimits";
 import { AgendaItem } from "./AgendaItem";
-
 AgendaItem.implement({
 	fields: (t) => ({
 		createdAt: t.field({
 			description: "Date time at the time the agenda item was last created.",
+			complexity: envConfig.API_GRAPHQL_SCALAR_RESOLVER_FIELD_COST,
 			resolve: async (parent, _args, ctx) => {
 				if (!ctx.currentClient.isAuthenticated) {
 					throw new TalawaGraphQLError({
