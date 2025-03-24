8c73d42bc151786d2cf896e566bf136df5c560b6
athaulrehman0304 <146646777+athaulrehman0304@users.noreply.github.com>
2025-03-25 00:00:46 +0530

    fixed org query for register page #3389

--- src/resolvers/Query/organizations.ts
+++ src/resolvers/Query/organizations.ts
@@ -1,53 +1,30 @@
-import type { QueryResolvers } from "../../types/generatedGraphQLTypes";
+import { QueryResolvers } from "../../types/generatedGraphQLTypes";
 import { Organization } from "../../models";
 import { errors } from "../../libraries";
 import { ORGANIZATION_NOT_FOUND_ERROR } from "../../constants";
 import { getSort } from "./helperFunctions/getSort";
 import { cacheOrganizations } from "../../services/OrganizationCache/cacheOrganizations";
 import { findOrganizationsInCache } from "../../services/OrganizationCache/findOrganizationsInCache";
-/**
- * If a 'id' is specified, this query will return an organisation;
- * otherwise, it will return all organisations with a size of limit 100.
- * @param _parent-
- * @param args - An object containing `orderBy` and `id` of the Organization.
- * @returns The organization if valid `id` is provided else return organizations with size limit 100.
- * @remarks `id` in the args is optional.
- */
-export const organizations: QueryResolvers["organizations"] = async (
-  _parent,
-  args,
-) => {
+
+export const organizations = async (_parent, args) => {
   const sort = getSort(args.orderBy);
-
-  let organizationFound;
   if (args.id) {
-    const organizationFoundInCache = await findOrganizationsInCache([args.id]);
-
-    if (!organizationFoundInCache.includes(null)) {
-      return organizationFoundInCache;
-    }
-
-    organizationFound = await Organization.find({
-      _id: args.id,
-    })
+    const cached = await findOrganizationsInCache([args.id]);
+    if (!cached.includes(null)) return cached;
+    const org = await Organization.find({ _id: args.id })
       .sort(sort)
+      .select("name description image _id")
       .lean();
-
-    await cacheOrganizations(organizationFound);
-
-    if (!organizationFound[0]) {
-      throw new errors.NotFoundError(
-        ORGANIZATION_NOT_FOUND_ERROR.DESC,
-        ORGANIZATION_NOT_FOUND_ERROR.CODE,
-        ORGANIZATION_NOT_FOUND_ERROR.PARAM,
-      );
-    }
-
-    return organizationFound;
+    await cacheOrganizations(org);
+    if (!org[0]) throw new errors.NotFoundError(ORGANIZATION_NOT_FOUND_ERROR.DESC, ORGANIZATION_NOT_FOUND_ERROR.CODE, ORGANIZATION_NOT_FOUND_ERROR.PARAM);
+    return org;
   } else {
-    organizationFound = await Organization.find().sort(sort).limit(100).lean();
-    await cacheOrganizations(organizationFound);
+    const filter = args.filter || "";
+    const orgs = await Organization.find({ name: { $regex: filter, $options: "i" } })
+      .sort(sort)
+      .select("name description image _id")
+      .lean();
+    await cacheOrganizations(orgs);
+    return orgs;
   }
-
-  return organizationFound;
 };
