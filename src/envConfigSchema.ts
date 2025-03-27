7be21da00e05440daa09590cba7a3c2cb5d7bc21
NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>
2025-03-27 19:40:16 +0530

    checks and tests
    Signed-off-by: NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>

--- src/envConfigSchema.ts
+++ src/envConfigSchema.ts
@@ -240,10 +240,10 @@ export const envConfigSchema = Type.Object({
 		maximum: 65535,
 		minimum: 0,
 	}),
-	// API_REDIS_URI: Type.String({
-	// 	format: "uri",
-	// 	pattern: "^redis://.*",
-	// }),
+	API_REDIS_URI: Type.String({
+		format: "uri",
+		pattern: "^redis://.*",
+	}),
 	//  cost of scalar field
 	API_GRAPHQL_SCALAR_FIELD_COST: Type.Number({
 		minimum: 0,
@@ -291,3 +291,5 @@ export const envSchemaAjv: EnvSchemaOpt["ajv"] = {
 		return ajvInstance;
 	},
 };
+
+
