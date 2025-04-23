ae9ce871cacd265287555a82ce297f8a4c136bac
Bittu kumar <kumarbittuu26@gmail.com>
2025-04-24 02:07:51 +0530

    fix: allow all org admins to block users

--- src/graphql/types/Mutation/blockUser.ts
+++ src/graphql/types/Mutation/blockUser.ts
@@ -44,56 +44,83 @@ builder.mutationField("blockUser", (t) =>
 
 			const currentUserId = ctx.currentClient.user.id;
 
-			const [
-				organization,
-				targetUser,
-				currentUserMembership,
-				targetUserMembership,
-				existingBlock,
-			] = await Promise.all([
+			const [currentUser, existingOrganization] = await Promise.all([
+				ctx.drizzleClient.query.usersTable.findFirst({
+					columns: {
+						role: true,
+					},
+					where: (fields, operators) => operators.eq(fields.id, currentUserId),
+				}),
 				ctx.drizzleClient.query.organizationsTable.findFirst({
+					with: {
+						membershipsWhereOrganization: {
+							columns: {
+								role: true,
+							},
+							where: (fields, operators) =>
+								operators.eq(fields.memberId, currentUserId),
+						},
+					},
 					where: (fields, operators) =>
 						operators.eq(fields.id, parsedArgs.organizationId),
 				}),
-				ctx.drizzleClient.query.usersTable.findFirst({
-					where: (fields, operators) =>
-						operators.eq(fields.id, parsedArgs.userId),
-				}),
-				ctx.drizzleClient.query.organizationMembershipsTable.findFirst({
-					where: (fields, operators) =>
-						operators.and(
-							operators.eq(fields.memberId, currentUserId),
-							operators.eq(fields.organizationId, parsedArgs.organizationId),
-						),
-				}),
-				ctx.drizzleClient.query.organizationMembershipsTable.findFirst({
-					where: (fields, operators) =>
-						operators.and(
-							operators.eq(fields.memberId, parsedArgs.userId),
-							operators.eq(fields.organizationId, parsedArgs.organizationId),
-						),
-				}),
-				ctx.drizzleClient.query.blockedUsersTable.findFirst({
-					where: (fields, operators) =>
-						operators.and(
-							operators.eq(fields.userId, parsedArgs.userId),
-							operators.eq(fields.organizationId, parsedArgs.organizationId),
-						),
-				}),
 			]);
 
-			if (!organization) {
+			if (!existingOrganization) {
 				throw new TalawaGraphQLError({
 					extensions: {
 						code: "arguments_associated_resources_not_found",
 						issues: [
 							{
-								argumentPath: ["input", "organizationId"],
+								argumentPath: ["organizationId"],
 							},
 						],
 					},
 				});
 			}
+
+			const currentUserOrganizationMembership =
+				existingOrganization.membershipsWhereOrganization[0];
+
+			if (
+				currentUser?.role !== "administrator" &&
+				(currentUserOrganizationMembership === undefined ||
+					currentUserOrganizationMembership.role !== "administrator")
+			) {
+				throw new TalawaGraphQLError({
+					extensions: {
+						code: "unauthorized_action",
+						message:
+							"You must be an admin of this organization to block users.",
+					},
+				});
+			}
+
+			const [targetUser, targetUserMembership, existingBlock] =
+				await Promise.all([
+					ctx.drizzleClient.query.usersTable.findFirst({
+						columns: {
+							id: true,
+							role: true,
+						},
+						where: (fields, operators) =>
+							operators.eq(fields.id, parsedArgs.userId),
+					}),
+					ctx.drizzleClient.query.organizationMembershipsTable.findFirst({
+						where: (fields, operators) =>
+							operators.and(
+								operators.eq(fields.memberId, parsedArgs.userId),
+								operators.eq(fields.organizationId, parsedArgs.organizationId),
+							),
+					}),
+					ctx.drizzleClient.query.blockedUsersTable.findFirst({
+						where: (fields, operators) =>
+							operators.and(
+								operators.eq(fields.userId, parsedArgs.userId),
+								operators.eq(fields.organizationId, parsedArgs.organizationId),
+							),
+					}),
+				]);
 
 			if (!targetUser) {
 				throw new TalawaGraphQLError({
@@ -104,19 +131,6 @@ builder.mutationField("blockUser", (t) =>
 								argumentPath: ["input", "userId"],
 							},
 						],
-					},
-				});
-			}
-
-			if (
-				!currentUserMembership ||
-				currentUserMembership.role !== "administrator"
-			) {
-				throw new TalawaGraphQLError({
-					extensions: {
-						code: "unauthorized_action",
-						message:
-							"You must be an admin of this organization to block users.",
 					},
 				});
 			}
