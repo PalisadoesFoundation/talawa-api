e02606ebd1328d2d8db376a711ba5c7338398bcf
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2025-04-28 11:52:45 +0530

    type errors and code quality fixed

--- src/graphql/types/Event/Event.ts
+++ src/graphql/types/Event/Event.ts
@@ -1,54 +1,54 @@
 import type { eventsTable } from "~/src/drizzle/tables/events";
 import { builder } from "~/src/graphql/builder";
 import {
-  EventAttachment,
-  type EventAttachment as EventAttachmentType,
+	EventAttachment,
+	type EventAttachment as EventAttachmentType,
 } from "~/src/graphql/types/EventAttachment/EventAttachment";
 
 export type Event = typeof eventsTable.$inferSelect & {
-  attachments: EventAttachmentType[] | null;
+	attachments: EventAttachmentType[] | null;
 };
 
 export const Event = builder.objectRef<Event>("Event");
 
 Event.implement({
-  description:
-    "Events are occurrences that take place for specific purposes at specific times.",
-  fields: (t) => ({
-    attachments: t.expose("attachments", {
-      description: "Array of attachments.",
-      type: t.listRef(EventAttachment),
-    }),
-    description: t.exposeString("description", {
-      description: "Custom information about the event.",
-    }),
-    endAt: t.expose("endAt", {
-      description: "Date time at the time the event ends at.",
-      type: "DateTime",
-    }),
-    id: t.exposeID("id", {
-      description: "Global identifier of the event.",
-      nullable: false,
-    }),
-    name: t.exposeString("name", {
-      description: "Name of the event.",
-    }),
-    startAt: t.expose("startAt", {
-      description: "Date time at the time the event starts at.",
-      type: "DateTime",
-      nullable: false,
-    }),
-    allDay: t.exposeBoolean("allDay", {
-      description: "Indicates if the event spans the entire day.",
-    }),
-    isPublic: t.exposeBoolean("isPublic", {
-      description: "Indicates if the event is publicly visible.",
-    }),
-    isRegisterable: t.exposeBoolean("isRegisterable", {
-      description: "Indicates if users can register for this event.",
-    }),
-    location: t.exposeString("location", {
-      description: "Physical or virtual location of the event.",
-    }),
-  }),
+	description:
+		"Events are occurrences that take place for specific purposes at specific times.",
+	fields: (t) => ({
+		attachments: t.expose("attachments", {
+			description: "Array of attachments.",
+			type: t.listRef(EventAttachment),
+		}),
+		description: t.exposeString("description", {
+			description: "Custom information about the event.",
+		}),
+		endAt: t.expose("endAt", {
+			description: "Date time at the time the event ends at.",
+			type: "DateTime",
+		}),
+		id: t.exposeID("id", {
+			description: "Global identifier of the event.",
+			nullable: false,
+		}),
+		name: t.exposeString("name", {
+			description: "Name of the event.",
+		}),
+		startAt: t.expose("startAt", {
+			description: "Date time at the time the event starts at.",
+			type: "DateTime",
+			nullable: false,
+		}),
+		allDay: t.exposeBoolean("allDay", {
+			description: "Indicates if the event spans the entire day.",
+		}),
+		isPublic: t.exposeBoolean("isPublic", {
+			description: "Indicates if the event is publicly visible.",
+		}),
+		isRegisterable: t.exposeBoolean("isRegisterable", {
+			description: "Indicates if users can register for this event.",
+		}),
+		location: t.exposeString("location", {
+			description: "Physical or virtual location of the event.",
+		}),
+	}),
 });
