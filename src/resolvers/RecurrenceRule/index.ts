1807445486ec5ce608478964cb838a79b7bbc63f
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-05-18 18:30:14 -0700

    Saving before reset 05/18/25 18:30:14

--- src/resolvers/RecurrenceRule/index.ts
+++
@@ -1,8 +0,0 @@
-import type { RecurrenceRuleResolvers } from "../../types/generatedGraphQLTypes";
-import { organization } from "./organization";
-import { baseRecurringEvent } from "./baseRecurringEvent";
-
-export const RecurrenceRule: RecurrenceRuleResolvers = {
-  organization,
-  baseRecurringEvent,
-};
