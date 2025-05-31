17c8822a3f9144e26bf9260c577613d930d7acd7
Bittu kumar <kumarbittuu26@gmail.com>
2025-06-01 03:32:59 +0530

    feat: Add mutation for assign and unassign user tag

---
+++ src/graphql/types/AssignUserTag/AssignUserTag.ts
@@ -0,0 +1,21 @@
+import { builder } from "~/src/graphql/builder";
+
+/**
+ * Represents a user assigned to a tag by an organization.
+ */
+export const AssignUserTag = builder.objectRef<{
+  assigneeId: string;
+  tagId: string;
+}>("AssignUserTag");
+
+AssignUserTag.implement({
+  description: "Represents a tag assigned to a user.",
+  fields: (t) => ({
+    assigneeId: t.exposeID("assigneeId", {
+      description: "Unique identifier of the assigned user.",
+    }),
+    tagId: t.exposeID("tagId", {
+      description: "Global identifier of the tag.",
+    })
+  }),
+});
