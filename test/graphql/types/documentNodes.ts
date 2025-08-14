ce398ef2f082be74201c5097ac9c9ccbe9d1d0e8
Harsh <harshk1729@gmail.com>
2025-08-14 21:43:22 +0530

    fixed query in documentNodes.ts

--- test/graphql/types/documentNodes.ts
+++ test/graphql/types/documentNodes.ts
@@ -386,13 +386,27 @@ export const Query_fundCampaignPledge =
 }`);
 
 export const Query_getPledgesByUserId =
-	gql(`query Query_getPledgesByUserId($userId: QueryUserInput! , $where: QueryPledgeWhereInput, $orderBy: QueryPledgeOrderByInput) {
-  getPledgesByUserId(userId: $userId , where: $where , orderBy: $orderBy) {
+	gql(`query Query_getPledgesByUserId($input: QueryFundCampaignPledgesByUserInput! , $where: QueryPledgeWhereInput, $orderBy: QueryPledgeOrderByInput) {
+  getPledgesByUserId(input: $input , where: $where , orderBy: $orderBy) {
     id
-    note
     amount
+    note
+    updatedAt
     campaign {
-    endAt
+      id
+      name
+      startAt
+      endAt
+      currencyCode
+      goalAmount
+    }
+    pledger {
+      id
+      name
+      avatarURL
+    }
+    updater {
+      id
     }
   }
 }`);
