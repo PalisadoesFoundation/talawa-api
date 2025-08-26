ba7470e351372c31777f09c895653feb88e2504c
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2025-08-27 00:17:42 +0530

    added time for tests in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
 		// teardownTimeout: 10000
 
 		hookTimeout: 30000, // 30 seconds for hooks
+		testTimeout: 20000, // 20 seconds per test
 		pool: "threads", // for faster test execution and to avoid postgres max-limit error
 	},
 });
