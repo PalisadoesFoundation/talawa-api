70f769d381cb8ec5eb4f208592b4c09d360ca7a1
Divyanshu gautam <gautamdivyanshu034@gmail.com>
2025-08-28 23:09:11 +0530

    .

--- test/setup.ts
+++ test/setup.ts
@@ -1,25 +1,19 @@
-import { sql } from "drizzle-orm";
+import { reset } from "drizzle-seed";
 import type { GlobalSetupContext } from "vitest/node";
+import * as schema from "~/src/drizzle/schema";
 import { server } from "./server";
 
 /**
  * Function that runs before all tests are ran. It re-runs each time one or more tests or javascript modules used within them are mutated in watch mode. More information at this link: {@link https://vitest.dev/config/#globalsetup}
  */
 export const setup = async (_ctx: GlobalSetupContext) => {
-	await server.ready();
+  await server.ready();
 };
 
 /**
  * Function that runs after all tests are ran. It re-runs each time one or more tests or javascript modules used within them are mutated in watch mode. More information at this link: {@link https://vitest.dev/config/#globalsetup}
  */
-import * as schema from "~/src/drizzle/schema";
 export const teardown = async () => {
-	const tableNames = Object.keys(schema);
-	const truncatePromises = tableNames.map((tableName) => {
-		return server.drizzleClient.execute(
-			sql.raw(`TRUNCATE TABLE ${tableName} RESTART IDENTITY CASCADE`),
-		);
-	});
-	await Promise.all(truncatePromises);
-	await server.close();
+  await reset(server.drizzleClient, schema);
+  await server.close();
 };
