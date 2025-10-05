abf7d45ca264385645b009d1643ba62a78ca4bf1
BharathC0 <bharatha52452@gmail.com>
2025-10-05 21:14:48 +0530

    fix:changes

--- test/minioMockSetup.ts
+++ test/minioMockSetup.ts
@@ -1,8 +1,8 @@
 import { vi } from "vitest";
 vi.mock("minio", () => ({
-    Client: vi.fn().mockImplementation(() => ({
-        makeRequestStreamAsync: vi.fn(),
-        getBucketRegionAsync: vi.fn(),
-        bucketExists: vi.fn(),
-    })),
+	Client: vi.fn().mockImplementation(() => ({
+		makeRequestStreamAsync: vi.fn(),
+		getBucketRegionAsync: vi.fn(),
+		bucketExists: vi.fn(),
+	})),
 }));
