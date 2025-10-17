2321b7f29196437407347e0a89e239501d353e91
Eyepatch5263 <pratyushpragyey@gmail.com>
2025-10-18 05:01:08 +0530

    chore: performed changes suggested by coderabbit

--- docs/src/utils/Heading.tsx
+++ docs/src/utils/Heading.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
   text: string;
 }
-
 const Heading: React.FC<HeadingProps> = ({ text, className, ...rest }) => (
   <h2 className={['Heading', className].filter(Boolean).join(' ')} {...rest}>
     {text}
