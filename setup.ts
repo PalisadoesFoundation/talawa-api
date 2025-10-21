7862ecbcd8224b5ba149bffb4e2016f68dd5725d
yashgoyal0110 <yg364550@gmail.com>
2025-10-22 01:32:31 +0530

    env-backup

--- setup.ts
+++ setup.ts
@@ -1,1 +1,11 @@
-s;
+import { setup } from "scripts/setup/setup";
+
+setup().catch((err) => {
+	console.error(`Setup failed: ${err.message}`);
+	console.error("Error details:", {
+		type: err.name,
+		code: err.code,
+		stack: err.stack,
+	});
+	process.exit(1);
+});
