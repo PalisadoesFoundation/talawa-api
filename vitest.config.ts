604b79fc431f2fac6cb703a53414cdc2d9a67613
Sourya07singh <144201791+Sourya07@users.noreply.github.com>
2025-10-22 03:41:06 +0530

    chore: add test.exclude array as PR objective

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,11 +4,27 @@ import { configDefaults, defineConfig } from "vitest/config";
 export default defineConfig({
 	plugins: [tsconfigPaths()],
 	test: {
+		exclude: [
+			...configDefaults.exclude,
+			"dist/**",
+			"coverage/**",
+			"docs/**",
+			"**/*.d.ts",
+			"data/**",
+			"docker/**",
+			"drizzle_migrations/**",
+			"envFiles/**",
+			"scripts/**",
+		],
 		coverage: {
 			provider: "v8", // or 'istanbul' if you prefer
 			reporter: ["text", "lcov", "html"],
 			exclude: [
 				...(configDefaults.coverage?.exclude ?? []),
+				"dist/**",
+				"coverage/**",
+				"docs/**",
+				"**/*.d.ts",
 				"data/**",
 				"docker/**",
 				"drizzle_migrations/**",
@@ -29,6 +45,7 @@ export default defineConfig({
 		// teardownTimeout: 10000
 
 		hookTimeout: 30000, // 30 seconds for hooks
+		testTimeout: 60000, // 60 seconds per test
 		pool: "threads", // for faster test execution and to avoid postgres max-limit error
 	},
 });
