439ffe53d54daaec5561a160dd18bbb2512426b2
Eyepatch5263 <pratyushpragyey@gmail.com>
2025-11-13 15:19:02 +0530

    tests: added Setup Script  Backup Feature for the .env file.

--- test/scripts/setup/apiSetup.test.ts
+++ test/scripts/setup/apiSetup.test.ts
@@ -31,7 +31,9 @@ describe("Setup -> apiSetup", () => {
 		process.env.MINIO_ROOT_PASSWORD = "password";
 		process.env.POSTGRES_PASSWORD = "password";
 		const mockResponses = [
-			...(isEnvConfigured ? [{ envReconfigure: true }, { backupEnv: false }] : []),
+			...(isEnvConfigured
+				? [{ envReconfigure: true }, { backupEnv: false }]
+				: []),
 			{ CI: "true" },
 			{ useDefaultMinio: true },
 			{ useDefaultPostgres: true },
@@ -277,7 +279,7 @@ describe("generateJwtSecret", () => {
 			});
 		const consoleErrorSpy = vi
 			.spyOn(console, "error")
-			.mockImplementation(() => { });
+			.mockImplementation(() => {});
 
 		expect(() => generateJwtSecret()).toThrow("Failed to generate JWT secret");
 		expect(consoleErrorSpy).toHaveBeenCalledWith(
