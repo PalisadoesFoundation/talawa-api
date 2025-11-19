846468ce363827b1afc4efce6a68a1a4a34af766
blourvim <blourvim@gmail.com>
2025-11-19 09:18:45 +0300

    remove redundant R flag from entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 # Fix docker.sock permissions, I am not sure if this is the best way, since the docker.sock on the host is also modified. A developer should also change the docker.sock's permissions back to normal. With simply running docker_sock_cleanup.sh
-sudo chown -R talawa /var/run/docker.sock
+sudo chown talawa /var/run/docker.sock
 # Execute the original command
 if [ $# -gt 0 ]; then
     exec "$@"
