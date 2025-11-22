c3e84807ec195009b7ab2bb36defed6d9b2dad58
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-11-21 21:23:54 -0500

    Streamlined the install, config and operations guides (#3758)
    * Streamlined the install, config and operations guides
    
    * Fixed document ID
    
    * Fixed comment
    
    * Fixed route

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -19,25 +19,25 @@ const config: Config = {
   onBrokenLinks: "throw",
   markdown: {
     hooks: {
-      onBrokenMarkdownLinks: 'warn', // Or 'throw', 'ignore'
+      onBrokenMarkdownLinks: "warn", // Or 'throw', 'ignore'
     },
   },
-  
+
   plugins: [
-  [
-    "@graphql-markdown/docusaurus",
-    {
-      schema: "../schema.graphql",        
-      rootPath: "docs",     
-      baseURL: "auto-schema",
-      loaders: {
-        GraphQLFileLoader: "@graphql-tools/graphql-file-loader",
+    [
+      "@graphql-markdown/docusaurus",
+      {
+        schema: "../schema.graphql",
+        rootPath: "docs",
+        baseURL: "auto-schema",
+        loaders: {
+          GraphQLFileLoader: "@graphql-tools/graphql-file-loader",
+        },
+        homepage: false,
+        pretty: true,
       },
-      homepage: false,
-      pretty : true,
-    },
+    ],
   ],
-],
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
@@ -46,6 +46,7 @@ const config: Config = {
     locales: ["en"],
   },
 
+  // Remote css file fetched from talawa-docs
   stylesheets: ["https://docs.talawa.io/css/styles-latest.css"],
 
   presets: [
@@ -58,7 +59,9 @@ const config: Config = {
           editUrl: ({ docPath }: { docPath: string }) => {
             return `https://github.com/PalisadoesFoundation/talawa-api/edit/develop/docs/docs/${docPath}`;
           },
-          exclude : ["**/auto-docs/graphql/types/Plugin/inputs/variables/queryPluginInputSchema.md"],
+          exclude: [
+            "**/auto-docs/graphql/types/Plugin/inputs/variables/queryPluginInputSchema.md",
+          ],
         },
         blog: {
           showReadingTime: true,
@@ -66,7 +69,8 @@ const config: Config = {
             "https://github.com/PalisadoesFoundation/talawa-api/tree/develop/docs/docs",
         },
         theme: {
-          customCss: undefined,
+          // custom css file with project-specific styling
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
@@ -199,7 +203,7 @@ const config: Config = {
       defaultMode: "light",
       disableSwitch: false,
       respectPrefersColorScheme: true,
-    },         
+    },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
