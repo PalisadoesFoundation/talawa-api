780fcdecb7629fff0d3031c9befce31b3098864e
Digvijay Singh Rawat <hackerearthx1@gmail.com>
2025-12-18 21:13:54 +0000

    format

--- src/graphql/types/FundCampaign/pledgedAmount.ts
+++ src/graphql/types/FundCampaign/pledgedAmount.ts
@@ -1,8 +1,8 @@
 import { eq, sum } from "drizzle-orm";
 import { fundCampaignPledgesTable } from "~/src/drizzle/tables/fundCampaignPledges";
 import type { GraphQLContext } from "~/src/graphql/context";
-import { TalawaGraphQLError } from "~/src/utilities/TalawaGraphQLError";
 import envConfig from "~/src/utilities/graphqLimits";
+import { TalawaGraphQLError } from "~/src/utilities/TalawaGraphQLError";
 import type { FundCampaign as FundCampaignType } from "./FundCampaign";
 import { FundCampaign } from "./FundCampaign";
 
