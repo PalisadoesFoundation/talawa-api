67d87a167626e0b3686f62fbea32e25df9e2ab12
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-12-22 15:01:54 -0500

    Revert "Revert "Revert "Fix sample post attachments upload to MinIO (#4047)" …"
    This reverts commit f98efd8c95051879530500b26386b540daa0ad31.

--- scripts/dbManagement/helpers.ts
+++ scripts/dbManagement/helpers.ts
@@ -428,7 +428,7 @@ export async function insertCollections(
 								const fileExtension = attachment.mimeType.split("/").pop();
 								const filePath = path.resolve(
 									dirname,
-									`./sample_data/images/${attachment.objectName}.${fileExtension}`,
+									`./sample_data/images/${attachment.name}.${fileExtension}`,
 								);
 								const fileData = await fs.readFile(filePath);
 								await minioClient.putObject(
@@ -442,7 +442,7 @@ export async function insertCollections(
 								);
 							} catch (error) {
 								console.error(
-									`Failed to upload attachment ${attachment.objectName}:`,
+									`Failed to upload attachment ${attachment.name}:`,
 									error,
 								);
 								throw error;
