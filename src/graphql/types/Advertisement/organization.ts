adecf71519b5cd2cafa96871cc8314f0cc95ec90
Aadithya Senthil Kumaran <aadithyacps@gmail.com>
2025-12-27 23:38:37 +0530

    fix: correct typo in comment (Organziation -> Organization)

--- src/graphql/types/Advertisement/organization.ts
+++ src/graphql/types/Advertisement/organization.ts
@@ -15,7 +15,7 @@ export const resolveOrganization = async (
 				operators.eq(fields.id, parent.organizationId),
 		});
 
-	// Organziation id existing but the associated organization not existing is a business logic error and probably means that the corresponding data in the database is in a corrupted state. It must be investigated and fixed as soon as possible to prevent additional data corruption.
+	// Organization id existing but the associated organization not existing is a business logic error and probably means that the corresponding data in the database is in a corrupted state. It must be investigated and fixed as soon as possible to prevent additional data corruption.
 	if (existingOrganization === undefined) {
 		ctx.log.error(
 			"Postgres select operation returned an empty array for an advertisement's organization id that isn't null.",
