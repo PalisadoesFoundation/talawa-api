15c8d2ff8298c9d53343e7a9003ced0402d25366
Ansingh0305 <ansingh0305@gmail.com>
2026-01-21 20:58:43 +0530

    chore: re-add verification files with correct config

---
+++ test/drizzle/integration_heavy.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from "vitest";
+import { integrationHeavy } from "../../src/integration_heavy";
+
+test("integrationHeavy works", () => {
+	expect(integrationHeavy(0)).toBe(50);
+});
