dccb9ff651bc1b0adb47f3f15f440ec5a736101e
Ansingh0305 <ansingh0305@gmail.com>
2026-01-21 21:09:29 +0530

    fix: correct test expectation

--- test/drizzle/integration_heavy.test.ts
+++ test/drizzle/integration_heavy.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from "vitest";
 import { integrationHeavy } from "../../src/integration_heavy";
 
 test("integrationHeavy works", () => {
-	expect(integrationHeavy(0)).toBe(50);
+	expect(integrationHeavy(0)).toBe(49);
 });
