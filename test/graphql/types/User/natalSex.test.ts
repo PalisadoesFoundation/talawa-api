f50f4a6f0303331ca8a38b0dd55e5ef696773c17
Himanshu Soni <h30s.soni@gmail.com>
2026-02-07 12:03:20 +0530

    Add tests for User natalSex field resolver

---
+++ test/graphql/types/User/natalSex.test.ts
@@ -0,0 +1,520 @@
+import { faker } from "@faker-js/faker";
+import { expect, suite, test } from "vitest";
+import type {
+	TalawaGraphQLFormattedError,
+	UnauthenticatedExtensions,
+	UnauthorizedActionExtensions,
+} from "~/src/utilities/TalawaGraphQLError";
+import { assertToBeNonNullish } from "../../../helpers";
+import { server } from "../../../server";
+import { mercuriusClient } from "../client";
+import {
+	Mutation_createUser,
+	Mutation_deleteUser,
+	Mutation_updateUser,
+	Query_signIn,
+	Query_user_natalSex,
+} from "../documentNodes";
+
+suite("User field natalSex", () => {
+	suite(
+		`results in a graphql error with "unauthenticated" extensions code in the "errors" field and "null" as the value of "data.user.natalSex" field if`,
+		() => {
+			test("client triggering the graphql operation is not authenticated.", async () => {
+				const administratorUserSignInResult = await mercuriusClient.query(
+					Query_signIn,
+					{
+						variables: {
+							input: {
+								emailAddress:
+									server.envConfig.API_ADMINISTRATOR_USER_EMAIL_ADDRESS,
+								password: server.envConfig.API_ADMINISTRATOR_USER_PASSWORD,
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(
+					administratorUserSignInResult.data.signIn?.user?.id,
+				);
+
+				const userNatalSexResult = await mercuriusClient.query(
+					Query_user_natalSex,
+					{
+						variables: {
+							input: {
+								id: administratorUserSignInResult.data.signIn.user.id,
+							},
+						},
+					},
+				);
+
+				expect(userNatalSexResult.data.user?.natalSex).toEqual(null);
+				expect(userNatalSexResult.errors).toEqual(
+					expect.arrayContaining<TalawaGraphQLFormattedError>([
+						expect.objectContaining<TalawaGraphQLFormattedError>({
+							extensions: expect.objectContaining<UnauthenticatedExtensions>({
+								code: "unauthenticated",
+							}),
+							message: expect.any(String),
+							path: ["user", "natalSex"],
+						}),
+					]),
+				);
+			});
+
+			test("client triggering the graphql operation has no existing user associated to their authentication context.", async () => {
+				const administratorUserSignInResult = await mercuriusClient.query(
+					Query_signIn,
+					{
+						variables: {
+							input: {
+								emailAddress:
+									server.envConfig.API_ADMINISTRATOR_USER_EMAIL_ADDRESS,
+								password: server.envConfig.API_ADMINISTRATOR_USER_PASSWORD,
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(
+					administratorUserSignInResult.data.signIn?.authenticationToken,
+				);
+
+				const createUserResult = await mercuriusClient.mutate(
+					Mutation_createUser,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								emailAddress: `email${faker.string.ulid()}@email.com`,
+								isEmailAddressVerified: false,
+								name: "name",
+								password: "password",
+								role: "regular",
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(createUserResult.data.createUser?.user?.id);
+
+				await mercuriusClient.mutate(Mutation_deleteUser, {
+					headers: {
+						authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+					},
+					variables: {
+						input: {
+							id: createUserResult.data.createUser.user.id,
+						},
+					},
+				});
+
+				assertToBeNonNullish(
+					createUserResult.data.createUser.authenticationToken,
+				);
+				assertToBeNonNullish(
+					administratorUserSignInResult.data.signIn.user?.id,
+				);
+
+				const userNatalSexResult = await mercuriusClient.query(
+					Query_user_natalSex,
+					{
+						headers: {
+							authorization: `bearer ${createUserResult.data.createUser.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								id: administratorUserSignInResult.data.signIn.user.id,
+							},
+						},
+					},
+				);
+
+				expect(userNatalSexResult.data.user?.natalSex).toEqual(null);
+				expect(userNatalSexResult.errors).toEqual(
+					expect.arrayContaining<TalawaGraphQLFormattedError>([
+						expect.objectContaining<TalawaGraphQLFormattedError>({
+							extensions: expect.objectContaining<UnauthenticatedExtensions>({
+								code: "unauthenticated",
+							}),
+							message: expect.any(String),
+							path: ["user", "natalSex"],
+						}),
+					]),
+				);
+			});
+		},
+	);
+
+	suite(
+		`results in a graphql error with "unauthorized_action" extensions code in the "errors" field and "null" as the value of "data.user.natalSex" field if`,
+		() => {
+			test(`client triggering the graphql operation is not associated to an administrator user.
+	            argument "input.id" is not equal to the id of the existing user associated to the client triggering the graphql operation.`, async () => {
+				const administratorUserSignInResult = await mercuriusClient.query(
+					Query_signIn,
+					{
+						variables: {
+							input: {
+								emailAddress:
+									server.envConfig.API_ADMINISTRATOR_USER_EMAIL_ADDRESS,
+								password: server.envConfig.API_ADMINISTRATOR_USER_PASSWORD,
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(
+					administratorUserSignInResult.data.signIn?.authenticationToken,
+				);
+
+				const createUserResult = await mercuriusClient.mutate(
+					Mutation_createUser,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								emailAddress: `email${faker.string.ulid()}@email.com`,
+								isEmailAddressVerified: false,
+								name: "name",
+								password: "password",
+								role: "regular",
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(
+					createUserResult.data.createUser?.authenticationToken,
+				);
+
+				assertToBeNonNullish(
+					administratorUserSignInResult.data.signIn.user?.id,
+				);
+
+				const userNatalSexResult = await mercuriusClient.query(
+					Query_user_natalSex,
+					{
+						headers: {
+							authorization: `bearer ${createUserResult.data.createUser.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								id: administratorUserSignInResult.data.signIn.user.id,
+							},
+						},
+					},
+				);
+
+				expect(userNatalSexResult.data.user?.natalSex).toEqual(null);
+				expect(userNatalSexResult.errors).toEqual(
+					expect.arrayContaining<TalawaGraphQLFormattedError>([
+						expect.objectContaining<TalawaGraphQLFormattedError>({
+							extensions: expect.objectContaining<UnauthorizedActionExtensions>(
+								{
+									code: "unauthorized_action",
+								},
+							),
+							message: expect.any(String),
+							path: ["user", "natalSex"],
+						}),
+					]),
+				);
+			});
+		},
+	);
+
+	suite(
+		`results in an empty "errors" field and the expected value for the "data.user.natalSex" field where`,
+		() => {
+			test(`"data.user.natalSex" returns the natalSex value when user accesses their own data.`, async () => {
+				const administratorUserSignInResult = await mercuriusClient.query(
+					Query_signIn,
+					{
+						variables: {
+							input: {
+								emailAddress:
+									server.envConfig.API_ADMINISTRATOR_USER_EMAIL_ADDRESS,
+								password: server.envConfig.API_ADMINISTRATOR_USER_PASSWORD,
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(
+					administratorUserSignInResult.data.signIn?.authenticationToken,
+				);
+				assertToBeNonNullish(
+					administratorUserSignInResult.data.signIn.user?.id,
+				);
+
+				const userNatalSexResult = await mercuriusClient.query(
+					Query_user_natalSex,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								id: administratorUserSignInResult.data.signIn.user.id,
+							},
+						},
+					},
+				);
+
+				expect(userNatalSexResult.errors).toBeUndefined();
+				// natalSex can be null for admin user that was just created
+				expect(userNatalSexResult.data.user).toBeDefined();
+			});
+
+			test(`"data.user.natalSex" returns the correct natalSex value when admin accesses another user's data.`, async () => {
+				const administratorUserSignInResult = await mercuriusClient.query(
+					Query_signIn,
+					{
+						variables: {
+							input: {
+								emailAddress:
+									server.envConfig.API_ADMINISTRATOR_USER_EMAIL_ADDRESS,
+								password: server.envConfig.API_ADMINISTRATOR_USER_PASSWORD,
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(
+					administratorUserSignInResult.data.signIn?.authenticationToken,
+				);
+
+				const createUserResult = await mercuriusClient.mutate(
+					Mutation_createUser,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								emailAddress: `email${faker.string.ulid()}@email.com`,
+								isEmailAddressVerified: false,
+								name: "name",
+								password: "password",
+								role: "regular",
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(createUserResult.data.createUser?.user?.id);
+
+				// Update the user with a natalSex
+				const updateUserResult = await mercuriusClient.mutate(
+					Mutation_updateUser,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								id: createUserResult.data.createUser.user.id,
+								natalSex: "female",
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(updateUserResult.data.updateUser?.id);
+
+				const userNatalSexResult = await mercuriusClient.query(
+					Query_user_natalSex,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								id: createUserResult.data.createUser.user.id,
+							},
+						},
+					},
+				);
+
+				expect(userNatalSexResult.errors).toBeUndefined();
+				expect(userNatalSexResult.data.user?.natalSex).toEqual("female");
+			});
+
+			test(`"data.user.natalSex" returns null when natalSex is not set.`, async () => {
+				const administratorUserSignInResult = await mercuriusClient.query(
+					Query_signIn,
+					{
+						variables: {
+							input: {
+								emailAddress:
+									server.envConfig.API_ADMINISTRATOR_USER_EMAIL_ADDRESS,
+								password: server.envConfig.API_ADMINISTRATOR_USER_PASSWORD,
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(
+					administratorUserSignInResult.data.signIn?.authenticationToken,
+				);
+
+				const createUserResult = await mercuriusClient.mutate(
+					Mutation_createUser,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								emailAddress: `email${faker.string.ulid()}@email.com`,
+								isEmailAddressVerified: false,
+								name: "name",
+								password: "password",
+								role: "regular",
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(createUserResult.data.createUser?.user?.id);
+
+				const userNatalSexResult = await mercuriusClient.query(
+					Query_user_natalSex,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								id: createUserResult.data.createUser.user.id,
+							},
+						},
+					},
+				);
+
+				expect(userNatalSexResult.errors).toBeUndefined();
+				expect(userNatalSexResult.data.user?.natalSex).toBeNull();
+			});
+
+			test(`"data.user.natalSex" returns correct value for male natalSex.`, async () => {
+				const administratorUserSignInResult = await mercuriusClient.query(
+					Query_signIn,
+					{
+						variables: {
+							input: {
+								emailAddress:
+									server.envConfig.API_ADMINISTRATOR_USER_EMAIL_ADDRESS,
+								password: server.envConfig.API_ADMINISTRATOR_USER_PASSWORD,
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(
+					administratorUserSignInResult.data.signIn?.authenticationToken,
+				);
+
+				const createUserResult = await mercuriusClient.mutate(
+					Mutation_createUser,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								emailAddress: `email${faker.string.ulid()}@email.com`,
+								isEmailAddressVerified: false,
+								name: "name",
+								password: "password",
+								role: "regular",
+								natalSex: "male",
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(createUserResult.data.createUser?.user?.id);
+
+				const userNatalSexResult = await mercuriusClient.query(
+					Query_user_natalSex,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								id: createUserResult.data.createUser.user.id,
+							},
+						},
+					},
+				);
+
+				expect(userNatalSexResult.errors).toBeUndefined();
+				expect(userNatalSexResult.data.user?.natalSex).toEqual("male");
+			});
+
+			test(`"data.user.natalSex" returns correct value for other natalSex.`, async () => {
+				const administratorUserSignInResult = await mercuriusClient.query(
+					Query_signIn,
+					{
+						variables: {
+							input: {
+								emailAddress:
+									server.envConfig.API_ADMINISTRATOR_USER_EMAIL_ADDRESS,
+								password: server.envConfig.API_ADMINISTRATOR_USER_PASSWORD,
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(
+					administratorUserSignInResult.data.signIn?.authenticationToken,
+				);
+
+				const createUserResult = await mercuriusClient.mutate(
+					Mutation_createUser,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								emailAddress: `email${faker.string.ulid()}@email.com`,
+								isEmailAddressVerified: false,
+								name: "name",
+								password: "password",
+								role: "regular",
+								natalSex: "other",
+							},
+						},
+					},
+				);
+
+				assertToBeNonNullish(createUserResult.data.createUser?.user?.id);
+
+				const userNatalSexResult = await mercuriusClient.query(
+					Query_user_natalSex,
+					{
+						headers: {
+							authorization: `bearer ${administratorUserSignInResult.data.signIn.authenticationToken}`,
+						},
+						variables: {
+							input: {
+								id: createUserResult.data.createUser.user.id,
+							},
+						},
+					},
+				);
+
+				expect(userNatalSexResult.errors).toBeUndefined();
+				expect(userNatalSexResult.data.user?.natalSex).toEqual("other");
+			});
+		},
+	);
+});
